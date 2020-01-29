7acc5a8125ab2239845904918983f2264052d88d
Ju Liu <liujuy@gmail.com>
2020-01-29 13:23:26 -0800

    clean unused data

--- examples/tet4_vascular/FSI/preprocess_sv_fsi_tets.cpp
+++ examples/tet4_vascular/FSI/preprocess_sv_fsi_tets.cpp
@@ -65,12 +65,9 @@ int main( int argc, char * argv[] )
   int in_ncommon = 2;
   const bool isDualGraph = true;
 
-  PetscMPIInt rank, size;
-
+  PetscMPIInt size;
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
-
   MPI_Comm_size(PETSC_COMM_WORLD, &size);
-  MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 
   if(size != 1) SYS_T::print_fatal("ERROR: preprocessor is a serial program! \n");
 
