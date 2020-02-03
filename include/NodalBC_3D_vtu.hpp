08d7a72095071fff9ad19d0da6ef6f49ff48f344
Ju Liu <liujuy@gmail.com>
2020-02-02 21:16:36 -0800

    in

--- include/NodalBC_3D_vtu.hpp
+++ include/NodalBC_3D_vtu.hpp
@@ -3,12 +3,13 @@
 // ==================================================================
 // NodalBC_3D_vtu.hpp
 //
-// This is an instantiation of INodalBC for 3D problems by reading a
-// vtu file.
+// This is an instantiation of INodalBC for 3D problems by reading vtu
+// files.
 //
-// This class is designed for fixing a group of nodes in a volumetric
+// This class is designed for (1) fixing a group of nodes in a volumetric
 // subdomain, e.g. the solid subdomain displacement in the FSI problem
-// when solving the fluid mesh motion.
+// when solving the fluid mesh motion (2) enforce essential bc that
+// are from .vtu files, such as the quadratic triangle mesh.
 //
 // Date: July 26 2017
 // Author: Ju Liu
@@ -20,11 +21,25 @@ class NodalBC_3D_vtu : public INodalBC
 {
   public:
     // --------------------------------------------------------------
+    // Default constructor: clear the dir_nodes, per_slave_nodes,
+    // per_master_nodes; set num_dir_nodes = num_per_nodes = 0;
+    // Set ID array based on the above `no-nodal' bc setting.
+    // --------------------------------------------------------------
+    NodalBC_3D_vtu( const int &nFunc );
+
+    // --------------------------------------------------------------
     // Read in the vtu file that all the nodes in this file will be
     // enforced as essential boundary conditions.
     // --------------------------------------------------------------
     NodalBC_3D_vtu( const std::string &vtufilename, const int &nFunc );
-   
+  
+    // --------------------------------------------------------------
+    // Read a list of vtu files to specify the Dirichlet nodes.
+    // No periodic BC nodes.
+    // --------------------------------------------------------------
+    NodalBC_3D_vtu( const std::vector<std::string> &vtufileList,
+        const int &nFunc );
+
     // --------------------------------------------------------------
     // Read in the vtu file and a list of vtp file that all nodes in these
     // files will be enforced as essential boundary conditions.
