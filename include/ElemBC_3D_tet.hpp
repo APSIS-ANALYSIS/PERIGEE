c89af4679b5e0d1137e814fe34145db258b87fa5
Ju Liu <liujuy@gmail.com>
2020-02-06 14:13:36 -0800

    header file in

---
+++ include/ElemBC_3D_tet.hpp
@@ -0,0 +1,42 @@
+#ifndef ELEMBC_3D_TET_HPP
+#define ELEMBC_3D_TET_HPP
+// ==================================================================
+// ElemBC_3D_tet.hpp
+//
+// This is an instantiation of ElemBC for 3D problems. It records the
+// elemental bc information for separate surface files.
+//
+// Date: Feb. 6 2020
+// ==================================================================
+#include "ElemBC.hpp"
+#include "Tet_Tools.hpp"
+
+class ElemBC_3D_tet : public ElemBC
+{
+  public:
+    ElemBC_3D_tet( const std::vector<std::string> &vtkfileList );
+
+    virtual ~ElemBC_3D_tet();
+
+  protected:
+    ElemBC_3D_tet4() {}; // Disallow default constructor
+    
+    int num_ebc;
+    int * num_node;     // length num_ebc
+    int * num_cell;     // length num_ebc
+    int * cell_nLocBas; // length num_ebc
+
+    // num_ebc times 3 x num_node[ii] in size
+    std::vector< std::vector<double> > pt_xyz;
+
+    // num_ebc times cell_nLocBas[ii] x num_cell[ii] in size
+    std::vector< std::vector<int> > tri_ien;
+
+    // num_ebc times num_node[ii] in size
+    std::vector< std::vector<int> > global_node;
+
+    // num_ebc times num_cell[ii] in size
+    std::vector< std::vector<int> > global_cell;
+};
+
+#endif
