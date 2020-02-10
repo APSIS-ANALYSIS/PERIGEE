0a98d3178ce6a827337af324941dc461cef0d9fe
Liu, Ju <liujuy@gmail.com>
2020-02-10 01:03:02 -0800

    more info from cmake config

--- conf/basic_variable_log.cmake
+++ conf/basic_variable_log.cmake
@@ -144,24 +144,24 @@ MESSAGE( STATUS "APPLE: " ${APPLE} )
 # But there are also two variables exactly for this purpose: 
 
 # the compiler flags for compiling C sources 
-#MESSAGE( STATUS "CMAKE_C_FLAGS: " ${CMAKE_C_FLAGS} )
+MESSAGE( STATUS "CMAKE_C_FLAGS: " ${CMAKE_C_FLAGS} )
 
 # the compiler flags for compiling C++ sources 
-#MESSAGE( STATUS "CMAKE_CXX_FLAGS: " ${CMAKE_CXX_FLAGS} )
+MESSAGE( STATUS "CMAKE_CXX_FLAGS: " ${CMAKE_CXX_FLAGS} )
 
 
 # Choose the type of build.  Example: SET(CMAKE_BUILD_TYPE Debug) 
-#MESSAGE( STATUS "CMAKE_BUILD_TYPE: " ${CMAKE_BUILD_TYPE} )
+MESSAGE( STATUS "CMAKE_BUILD_TYPE: " ${CMAKE_BUILD_TYPE} )
 
 # if this is set to ON, then all libraries are built as shared libraries by
 # default.
 MESSAGE( STATUS "BUILD_SHARED_LIBS: " ${BUILD_SHARED_LIBS} )
 
 # the compiler used for C files 
-#MESSAGE( STATUS "CMAKE_C_COMPILER: " ${CMAKE_C_COMPILER} )
+MESSAGE( STATUS "CMAKE_C_COMPILER: " ${CMAKE_C_COMPILER} )
 
 # the compiler used for C++ files 
-#MESSAGE( STATUS "CMAKE_CXX_COMPILER: " ${CMAKE_CXX_COMPILER} )
+MESSAGE( STATUS "CMAKE_CXX_COMPILER: " ${CMAKE_CXX_COMPILER} )
 
 # if the compiler is a variant of gcc, this should be set to 1 
 #MESSAGE( STATUS "CMAKE_COMPILER_IS_GNUCC: " ${CMAKE_COMPILER_IS_GNUCC} )
