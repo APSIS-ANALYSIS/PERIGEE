78b0bab7c5b70205a4dabb872495639cf136214a
Ju Liu <liujuy@gmail.com>
2020-02-11 14:24:25 -0800

    Residual_BackflowStab added

--- examples/tet_ns_3d/src/PLocAssem_Tet_VMS_NS_GenAlpha.cpp
+++ examples/tet_ns_3d/src/PLocAssem_Tet_VMS_NS_GenAlpha.cpp
@@ -1019,14 +1019,55 @@ void PLocAssem_Tet_VMS_NS_GenAlpha::Assem_Residual_EBC_Resistance(
 
 
 void PLocAssem_Tet_VMS_NS_GenAlpha::Assem_Residual_BackFlowStab(
-        const double * const &vec_a,
-        const double * const &vec_b,
+        const double * const &dot_sol,
+        const double * const &sol,
         FEAElement * const &element,
         const double * const &eleCtrlPts_x,
         const double * const &eleCtrlPts_y,
         const double * const &eleCtrlPts_z,
         const IQuadPts * const &quad )
-{}
+{
+  element->buildBasis( quad, eleCtrlPts_x, eleCtrlPts_y, eleCtrlPts_z );
+
+  const int face_nqp = quad -> get_num_quadPts();
+
+  double gwts, nx, ny, nz, surface_area;
+  double u, v, w;
+  double factor;
+
+  Zero_sur_Residual();
+
+  for(int qua = 0; qua < face_nqp; ++qua)
+  {
+    element->get_R(qua, &R[0]);
+    element->get_2d_normal_out(qua, nx, ny, nz, surface_area);
+
+    u = 0.0; v = 0.0; w = 0.0;;
+    for(int ii=0; ii<snLocBas; ++ii)
+    {
+      const int ii4 = ii * 4;
+      u += sol[ii4+1] * R[ii];
+      v += sol[ii4+2] * R[ii];
+      w += sol[ii4+3] * R[ii];
+    }
+
+    const double temp = u * nx + v * ny + w * nz;
+
+    if(temp < 0.0) factor = temp * rho0 * beta;
+    else factor = 0.0;
+
+    gwts = surface_area * quad -> get_qw(qua);
+
+    for(int A=0; A<snLocBas; ++A)
+    {
+      sur_Residual[4*A+1] -= gwts * R[A] * factor * u;
+      sur_Residual[4*A+2] -= gwts * R[A] * factor * v;
+      sur_Residual[4*A+3] -= gwts * R[A] * factor * w;
+    }
+  }
+
+}
+
 
 void PLocAssem_Tet_VMS_NS_GenAlpha::Assem_Tangent_Residual_BackFlowStab(
         const double &dt,
