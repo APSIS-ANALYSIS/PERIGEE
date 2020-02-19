7448410af3b37d34e1b91f24beac34b82b06a070
Ju Liu <liujuy@gmail.com>
2020-02-19 15:21:32 -0800

    generate form gmsh

--- examples/tet4_vascular/FSI/gmsh_process.cpp
+++ examples/tet4_vascular/FSI/gmsh_process.cpp
@@ -1,6 +1,8 @@
 // ==================================================================
 // gmsh_process.cpp
 //
+// Code that handles the gmsh file.
+//
 // Date Created: July 1 2017
 // Author: Ju Liu
 // ==================================================================
@@ -8,7 +10,7 @@
 
 int main( int argc, char * argv[] )
 {
-  std::string gmshFile = "fsi_cylinder.msh";
+  std::string gmshFile = "fsi_wall.msh";
 
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
   SYS_T::GetOptionString("-gmsh_file", gmshFile);
@@ -16,26 +18,13 @@ int main( int argc, char * argv[] )
 
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
-  // In FSI problems, we take domain tag 0 be the fluid domain;
-  // domain tag 1 be the solid domain.
-  GIO -> check_FSI_ordering();
-
   GIO -> print_info();
 
-  // Write the fluid surface meshes associated with the fluid volume mesh
   GIO -> write_vtp(0,0,true);
   GIO -> write_vtp(1,0,true);
   GIO -> write_vtp(2,0);
+  GIO -> write_vtp(3,0);
 
-  // Write the solid surface meshes associated with the solid volume mesh
-  GIO -> write_vtp(3,1);
-  GIO -> write_vtp(4,1);
-  GIO -> write_vtp(5,1);
-
-  // Write each 3D volumetric mesh separately
-  GIO -> write_each_vtu();
-
-  // Write the whole volumetric mesh with each sub-domain with a physical tag
   const std::string wmname("whole_vol");
   const bool isXML = true;
   GIO -> write_vtu( wmname, isXML );
