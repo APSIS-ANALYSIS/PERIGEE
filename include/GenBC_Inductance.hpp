50ddbc17ff983e85f54d39294e481e80582327ba
Ju Liu <liujuy@gmail.com>
2020-02-20 17:45:31 -0800

    try a different geometry for inductor

--- include/GenBC_Inductance.hpp
+++ include/GenBC_Inductance.hpp
@@ -30,7 +30,8 @@ class GenBC_Inductance : public IGenBC
     virtual double get_m( const int &ii, const double &dot_Q,
        const double &Q ) const
     {
-      return 0.0;
+      return induct[ii] / 1.0e-3;
+      //return 0.0;
     }
 
     // We do not perform boundary check. Users are responsible to
@@ -38,7 +39,8 @@ class GenBC_Inductance : public IGenBC
     virtual double get_n( const int &ii, const double &dot_Q,
        const double &Q ) const
     {
-      return induct[ii];
+      return 0.0;
+      //return induct[ii];
     }
 
 
@@ -47,7 +49,8 @@ class GenBC_Inductance : public IGenBC
     virtual double get_P( const int &ii, const double &dot_Q,
        const double &Q ) const
     {
-      return induct[ii] * dot_Q + pres_offset[ii];
+      return induct[ii] * (Q - Q0[ii]) / 1.0e-3 + pres_offset[ii];
+      //return induct[ii] * dot_Q + pres_offset[ii];
     }
 
     virtual double get_P0( const int &ii ) const
