ae31e0863c51af4abde316c4f45ba564a606374e
Ju Liu <liujuy@gmail.com>
2020-02-21 15:23:43 -0800

    gmsh file converter in

--- examples/tet_ns_3d/gmsh_process.cpp
+++ examples/tet_ns_3d/gmsh_process.cpp
@@ -11,18 +11,23 @@
 int main( int argc, char * argv[] )
 {
   std::string gmshFile = "cylinder.msh";
+  int num_outlet = 1;
 
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
   SYS_T::GetOptionString("-gmsh_file", gmshFile);
+  SYS_T::GetOptionInt("-num_outlet", num_outlet);
   std::cout<<" -gmsh_file: "<<gmshFile<<std::endl;
+  std::cout<<" -num_outlet: "<<num_outlet<<std::endl;
 
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
   GIO -> print_info();
 
-  GIO -> write_vtp(0,0,true);
-  GIO -> write_vtp(1,0,true);
-  GIO -> write_vtp(2,0,true);
+  GIO -> write_vtp(0,0,false); // assumed to be wall vtp
+  GIO -> write_vtp(1,0,false); // assumed to be inlet vtp
+  
+  for(int ii=0; ii<num_outlet; ++ii)
+    GIO -> write_vtp(2+ii,0,true);
 
   const std::string wmname("whole_vol");
   const bool isXML = true;
