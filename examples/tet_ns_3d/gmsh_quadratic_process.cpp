a8c7b3827d0f9889f6917687394610e68af1be1f
Ju Liu <liujuy@gmail.com>
2020-03-03 16:01:55 -0800

    some comments

--- examples/tet_ns_3d/gmsh_quadratic_process.cpp
+++ examples/tet_ns_3d/gmsh_quadratic_process.cpp
@@ -3,7 +3,7 @@
 //
 // Code that handles the gmsh file.
 //
-// Date Created: July 1 2017
+// Date Created: March 3 2020
 // Author: Ju Liu
 // ==================================================================
 #include "Gmsh_FileIO.hpp"
@@ -24,14 +24,20 @@ int main( int argc, char * argv[] )
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
   GIO -> print_info();
+ 
+  // Gmsh file should contain only one 3D domain 
+  const int vol_id = 0;
 
-  GIO -> update_quadratic_tet_IEN( 0 );
+  // Gmsh IEN needs to be updated by swapping local node 8 and 9 to be
+  // compatible with the VTK IEN node ordering
+  GIO -> update_quadratic_tet_IEN( vol_id );
 
-  GIO -> write_quadratic_sur_vtu( 0, 0, false); // assumed to be wall vtp
-  GIO -> write_quadratic_sur_vtu( 1, 0, true); // assumed to be wall vtp
+  // Write the surface mesh into vtu files
+  GIO -> write_quadratic_sur_vtu( 0, vol_id, false); // assumed to be wall vtp
+  GIO -> write_quadratic_sur_vtu( 1, vol_id, true); // assumed to be wall vtp
   
   for(int ii=0; ii<num_outlet; ++ii)
-    GIO -> write_quadratic_sur_vtu( 2+ii, 0, true );
+    GIO -> write_quadratic_sur_vtu( 2+ii, vol_id, true );
 
   const std::string wmname("whole_vol");
   const bool isXML = true;
