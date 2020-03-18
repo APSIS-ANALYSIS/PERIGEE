7df5de576797467d4c9a9e6a7443685c04636811
Ju Liu@Stampede2 <liujuy@gmail.com>
2020-03-18 03:27:39 -0500

    in

--- conf/stampede2_PETSc_v36p0_shared_VTK.cmake
+++ conf/stampede2_PETSc_v36p0_shared_VTK.cmake
@@ -11,8 +11,6 @@
 # This one is for stampede2 build.
 # ===================================================================
 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
-
 # ===================================================================
 # 1. VTK VARIABLES
 # ===================================================================
@@ -28,11 +26,27 @@ MESSAGE(STATUS ${VTK_DIR})
 #SET(PETSC_DIR /home1/apps/intel18/impi18_0/petsc/3.10)
 #SET(PETSC_ARCH skylake)
 
-set(PETSC_DIR /work/01346/liujuy/stampede2/lib/petsc-3.6.4-opt)
+SET(PETSC_DIR /work/01346/liujuy/stampede2/lib/petsc-3.6.4-opt)
+SET(PETSC_ARCH .)
 
-set(PETSC_ARCH .)
+SET(PETSC_LIBRARY_DIRS ${PETSC_DIR}/${PETSC_ARCH}/lib )
 
-find_package(PETSc)
+find_library (PETSC_LIBRARIES NAMES petsc HINTS "${PETSC_DIR}/${PETSC_ARCH}" 
+  PATH_SUFFIXES "lib" NO_DEFAULT_PATH)
+
+find_path (PETSC_CONF_DIR petscrules HINTS "${PETSC_DIR}/${PETSC_ARCH}"
+  PATH_SUFFIXES "lib/petsc/conf" "conf" NO_DEFAULT_PATH)
+
+include(${PETSC_CONF_DIR}/PETScConfig.cmake)
+#include(${PETSC_CONF_DIR}/PETScBuildInternal.cmake)
+
+# Load libpetsc and the external libs
+SET(PETSC_link_lib ${PETSC_LIBRARIES} ${PETSC_PACKAGE_LIBS})
+
+# ===================================================================
+# 3. METIS VARIABLES
+# ===================================================================
+SET(METIS_DIR ${PETSC_DIR}/${PETSC_ARCH}/lib)
 
 # ===================================================================
 # 4. HDF5 VARIABLES
@@ -40,13 +54,22 @@ find_package(PETSc)
 SET(HDF5_DIR /opt/apps/intel17/impi17_0/phdf5/1.8.16/x86_64 ) 
 
 # ===================================================================
-# 5. Compiler options
+# 5. Eigen VARIABLES
+# ===================================================================
+#INCLUDE_DIRECTORIES(/work/01346/liujuy/stampede2/lib/eigen-3.3.4/include/eigen3)
+
+# ===================================================================
+# 6. SLEPc VARIABLES
+# ===================================================================
+#SET(SLEPC_DIR /work/01346/liujuy/stampede2/lib/slepc-3.8.3-opt)
+
+# ===================================================================
+# 7. Compiler options
 # ===================================================================
 SET(CMAKE_C_COMPILER  /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/bin/mpicc)
 SET(CMAKE_CXX_COMPILER /opt/intel/compilers_and_libraries_2017.4.196/linux/mpi/intel64/bin/mpicxx)
 SET(CMAKE_CXX_FLAGS "-xCORE-AVX512 -O3 -xhost -Wall")
 SET(CMAKE_BUILD_TYPE RELEASE)
-SET(CMAKE_CXX_STANDARD 11)
 
 # ===================================================================
 # END OF FILE
