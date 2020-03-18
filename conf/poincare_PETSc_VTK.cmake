61e9817b24a385d41d0cbdf48ffce098375ab65b
Liu, Ju <liujuy@gmail.com>
2020-03-17 23:29:17 -0700

    poincare cmake update

--- conf/poincare_PETSc_VTK.cmake
+++ conf/poincare_PETSc_VTK.cmake
@@ -7,6 +7,8 @@
 # IN THE CMAKELISTS.TXT FILE, YOU ONLY NEED TO INCLUDE
 # THIS FILE TO HAVE THESE VARIABLES DEFINED IN YOUR CMAKE.
 
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
 # This one is for poincare, my laptop.
 # =========================================================
 # 1. VTK VARIABLES
@@ -21,23 +23,26 @@ SET(VTK_link_lib vtkCommonCore-7.1 vtkCommonSystem-7.1 vtkCommonDataModel-7.1
 # ========================================================
 # 2. PETSc VARIABLES
 # ========================================================
-SET(PETSC_DIR /Users/juliu/lib/petsc-3.11.3-debug)
-SET(PETSC_ARCH .)
+set(PETSC_DIR /Users/juliu/lib/petsc-3.11.3-debug)
 
-SET(PETSC_LIBRARY_DIRS ${PETSC_DIR}/${PETSC_ARCH}/lib )
-find_library (PETSC_LIBRARIES NAMES petsc HINTS "${PETSC_DIR}" 
-  PATH_SUFFIXES "${PETSC_ARCH}/lib" "lib" NO_DEFAULT_PATH)
-find_path (PETSC_CONF_DIR petscrules HINTS "${PETSC_DIR}/${PETSC_ARCH}"
-  PATH_SUFFIXES "lib/petsc/conf" "conf" NO_DEFAULT_PATH)
-include(${PETSC_CONF_DIR}/PETScBuildInternal.cmake)
+set(PETSC_ARCH .)
+
+find_package(PETSc)
+
+#SET(PETSC_LIBRARY_DIRS ${PETSC_DIR}/${PETSC_ARCH}/lib )
+#find_library (PETSC_LIBRARIES NAMES petsc HINTS "${PETSC_DIR}" 
+#  PATH_SUFFIXES "${PETSC_ARCH}/lib" "lib" NO_DEFAULT_PATH)
+#find_path (PETSC_CONF_DIR petscrules HINTS "${PETSC_DIR}/${PETSC_ARCH}"
+#  PATH_SUFFIXES "lib/petsc/conf" "conf" NO_DEFAULT_PATH)
+#include(${PETSC_CONF_DIR}/PETScBuildInternal.cmake)
 #include(${PETSC_CONF_DIR}/PETScConfig.cmake)
-SET(PETSC_link_lib ${PETSC_LIBRARIES} ${PETSC_PACKAGE_LIBS})
+#SET(PETSC_link_lib ${PETSC_LIBRARIES} ${PETSC_PACKAGE_LIBS})
 
 
 # ========================================================
 # 3. METIS VARIABLES
 # ========================================================
-SET(METIS_DIR ${PETSC_DIR})
+#SET(METIS_DIR ${PETSC_DIR})
 
 # ========================================================
 # 4. HDF5 VARIABLES
