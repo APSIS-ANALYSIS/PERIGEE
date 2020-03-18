4d01fdf2d3a761705cb2eaf65e13b8304804c3cf
Ju Liu <liujuy@gmail.com>
2020-03-18 10:00:39 -0700

    handle hdf5 nest

--- conf/setup_external_libs.cmake
+++ conf/setup_external_libs.cmake
@@ -5,27 +5,8 @@ MESSAGE(STATUS "=================== External library setup ===================")
 # ===============================================
 # SET VTK
 # ===============================================
-IF(DEFINED VTK_DIR)
-  MESSAGE(STATUS "VTK_DIR = ${VTK_DIR}")
-ELSE(DEFINED VTK_DIR)
-  MESSAGE(FATAL_ERROR "VTK_DIR is NOT defined! Check ~/PETSc_VTK.cmake")
-ENDIF(DEFINED VTK_DIR)
-
-If(DEFINED VTK_VERSION)
-  MESSAGE(STATUS "VTK_VERSION = ${VTK_VERSION}")
-ELSE(DEFINED VTK_VERSION)
-  MESSAGE(FATAL_ERROR "VTK_VERSION is NOT defined! Check ~/PETSc_VTK.cmake")
-ENDIF(DEFINED VTK_VERSION)
-
-IF(DEFINED VTK_link_lib)
-  MESSAGE(STATUS "VTK_link_lib=${VTK_link_lib}")
-ELSE(DEFINED VTK_link_lib)
-  MESSAGE(FATAL_ERROR "VTK_link_lib is NOT defined! Check ~/PETSc_VTK.cmake")
-ENDIF(DEFINED VTK_link_lib)
-
 SET(VTK_INCLUDE_DIRS ${VTK_INCLUDE_DIRS}
   ${VTK_DIR}/include/${VTK_VERSION})
-
 
 IF(${VTK_VERSION} MATCHES "vtk-5.10")
   SET(VTK_LINK_DIRS ${VTK_LINK_DIRS} ${VTK_DIR}/lib/${VTK_VERSION})
@@ -34,10 +15,6 @@ ELSE(${VTK_VERSION} MATCHES "vtk-5.10")
 ENDIF(${VTK_VERSION} MATCHES "vtk-5.10")
 
 SET(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${VTK_link_lib} )
-
-# ===============================================
-# FINISH VTK SETUP
-# ===============================================
 
 # ===============================================
 # SET HDF5 library
@@ -53,13 +30,7 @@ SET(HDF5_INCLUDE_DIRS ${HDF5_DIR}/include)
 SET(HDF5_LINK_LIBS hdf5)
 
 SET(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${HDF5_LINK_LIBS})
-# ===============================================
-# FINISH HDF5 setup
-# ===============================================
 
-# ===============================================
-# Finish Slepc setup
-# ===============================================
 # INCLUDE LIBRARY HEADER FILES
 INCLUDE_DIRECTORIES( ${VTK_INCLUDE_DIRS} )
 #INCLUDE_DIRECTORIES( ${METIS_INCLUDE_DIRS} )
