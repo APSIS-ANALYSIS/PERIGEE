9045af0537c15eaeffda934695172f9d7b9e7c11
liuju <liujuy@gmail.com>
2020-03-17 23:43:31 -0700

    my sherlock conf file update

--- conf/stanford_sherlock.cmake
+++ conf/stanford_sherlock.cmake
@@ -1,3 +1,5 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+
 # =========================================================
 # 1. VTK VARIABLES
 # =========================================================
@@ -14,32 +16,13 @@ SET(VTK_link_lib vtkCommonCore-7.1 vtkCommonSystem-7.1 vtkCommonDataModel-7.1
 # ========================================================
 # 2. PETSc VARIABLES
 # ========================================================
-SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.11.3)
-SET(PETSC_ARCH arch-linux2-c-opt)
+#SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.11.3)
+#SET(PETSC_ARCH arch-linux2-c-opt)
 
-#SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt)
-#SET(PETSC_ARCH .)
+SET(PETSC_DIR /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt)
+SET(PETSC_ARCH .)
 
-#----------------------------------------------------------
-# You do not need to modify the rest of PETSc variables.
-SET(PETSC_LIBRARY_DIRS ${PETSC_DIR}/${PETSC_ARCH}/lib )
-
-find_library (PETSC_LIBRARIES NAMES petsc HINTS "${PETSC_DIR}/${PETSC_ARCH}" 
-  PATH_SUFFIXES "lib" NO_DEFAULT_PATH)
-
-find_path (PETSC_CONF_DIR petscrules HINTS "${PETSC_DIR}/${PETSC_ARCH}"
-  PATH_SUFFIXES "lib/petsc/conf" "conf" NO_DEFAULT_PATH)
-
-include(${PETSC_CONF_DIR}/PETScBuildInternal.cmake)
-#include(${PETSC_CONF_DIR}/PETScConfig.cmake)
-
-SET(PETSC_link_lib ${PETSC_LIBRARIES} ${PETSC_PACKAGE_LIBS})
-#----------------------------------------------------------
-
-# ========================================================
-# 3. METIS VARIABLES
-# ========================================================
-SET(METIS_DIR /home/groups/amarsden/lib-perigee/metis-5.0.3)
+find_package(PETSc)
 
 # ========================================================
 # 4. HDF5 VARIABLES
@@ -50,13 +33,15 @@ SET(HDF5_DIR /home/groups/amarsden/lib-perigee/hdf5-1.8.16)
 # ========================================================
 # 5. Compiler options 
 # ========================================================
-SET(CMAKE_C_COMPILER  /home/groups/amarsden/lib-perigee/petsc-3.11.3/arch-linux2-c-opt/bin/mpicc)
-SET(CMAKE_CXX_COMPILER /home/groups/amarsden/lib-perigee/petsc-3.11.3/arch-linux2-c-opt/bin/mpicxx)
+SET(CMAKE_C_COMPILER  /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt/bin/mpicc)
+SET(CMAKE_CXX_COMPILER /home/groups/amarsden/lib-perigee/petsc-3.8.4-opt/bin/mpicxx)
 
 IF( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
   SET(CMAKE_CXX_FLAGS "-O3 -Wall")
 ELSE( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
   SET(CMAKE_CXX_FLAGS "-O0 -Wall")
 ENDIF( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
+
+SET(CMAKE_CXX_STANDARD 11)
 
 # EOF
