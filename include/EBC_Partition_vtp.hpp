133f2a567a162a858fb52a68e20e67d3f7beaaff
Ju Liu <liujuy@gmail.com>
2020-07-09 21:10:34 -0700

    clean the old code

--- include/EBC_Partition_vtp.hpp
+++ include/EBC_Partition_vtp.hpp
@@ -24,9 +24,6 @@ class EBC_Partition_vtp : public IEBC_Partition
 
     virtual ~EBC_Partition_vtp();
 
-    // Write the data to hdf5 file in group /ebc
-    //virtual void write_hdf5( const char * FileName ) const;
-    
     // Write the data to hdf5 file in group /GroupName
     virtual void write_hdf5( const char * const &FileName,
        const char * const &GroupName = "/ebc" ) const;
