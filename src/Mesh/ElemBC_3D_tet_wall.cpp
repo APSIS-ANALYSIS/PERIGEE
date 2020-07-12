f05f0df7a8f3b84c532747de60d1b96b03bcdea9
Ju Liu <liujuy@gmail.com>
2020-07-12 12:42:17 -0700

    have the data structure settup

--- src/Mesh/ElemBC_3D_tet_wall.cpp
+++ src/Mesh/ElemBC_3D_tet_wall.cpp
@@ -4,7 +4,16 @@ ElemBC_3D_tet_wall::ElemBC_3D_tet_wall(
     const std::vector<std::string> &vtkfileList,
     const int &elemtype )
 : ElemBC_3D_tet( vtkfileList, elemtype )
-{}
+{
+  int total_num_nodes = 0;
+  for(int ii=0; ii<num_ebc; ++ii) total_num_nodes += num_node[ii];
+
+  radius.resize( total_num_nodes );
+  radial.resize( total_num_nodes );
+  tangential.resize( total_num_nodes );
+  circumferential.resize( total_num_nodes );
+
+}
 
 
 ElemBC_3D_tet_wall::~ElemBC_3D_tet_wall()
