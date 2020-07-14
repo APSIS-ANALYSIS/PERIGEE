c8cf225ca049c972d59bffe45f5a0e887f74a491
Ju Liu <liujuy@gmail.com>
2020-07-14 01:00:59 -0700

    i implemented the search algorithm in the constructor by copying. have NOT done any test yet.

--- src/Mesh/ElemBC_3D_tet_wall.cpp
+++ src/Mesh/ElemBC_3D_tet_wall.cpp
@@ -15,9 +15,37 @@ ElemBC_3D_tet_wall::ElemBC_3D_tet_wall(
   reader -> SetFileName( centerlineFile.c_str() );
   reader -> Update();
 
-  line_polydata = reader -> GetOutput();
-  reader -> Delete();
+  vtkPolyData * centerlineData = reader -> GetOutput();
+  
+  vtkPointLocator * locator = vtkPointLocator::New();
+  locator -> Initialize();
+  locator -> SetDataSet( centerlineData );
+  locator -> BuildLocator();
 
+  for(int ebc_id = 0; ebc_id < num_ebc; ++ebc_id)
+  {
+    for(int ii=0; ii<num_node[ebc_id]; ++ii)
+    {
+      const double coor_x = pt_xyz[ebc_id][3*ii];
+      const double coor_y = pt_xyz[ebc_id][3*ii+1];
+      const double coor_z = pt_xyz[ebc_id][3*ii+2];
+
+      const double pt [] = {coor_x, coor_y, coor_z};
+
+      const int closest_id = locator -> FindClosestPoint(&pt[0]);
+
+      const double * closest_cl_pt = centerlineData -> GetPoints() -> GetPoint(closest_id);
+      const double line_pt_x = closest_cl_pt[0];
+      const double line_pt_y = closest_cl_pt[1];
+      const double line_pt_z = closest_cl_pt[2];
+
+      radius[ebc_id][ii] = MATH_T::norm2(line_pt_x - coor_x, line_pt_y - coor_y, line_pt_z - coor_z);
+    }
+  }
+
+  // clean memory
+  locator -> Delete();
+  reader -> Delete();
 }
 
 
