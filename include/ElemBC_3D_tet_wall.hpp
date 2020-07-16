c7b23ad876002606830cfd39d0ae54e7cca2312b
Ingrid Lan <ingridl@stanford.edu>
2020-07-15 17:20:46 -0700

    pass thickness_to_radius vec by reference

--- include/ElemBC_3D_tet_wall.hpp
+++ include/ElemBC_3D_tet_wall.hpp
@@ -16,7 +16,7 @@ class ElemBC_3D_tet_wall : public ElemBC_3D_tet
 {
   public:
     ElemBC_3D_tet_wall( const std::vector<std::string> &vtkfileList,
-        const std::vector<double> thickness_to_radius,
+        const std::vector<double> &thickness_to_radius,
         const std::string &centerlineFile = "centerlines.vtp",
         const int &elemtype = 501 );
 
