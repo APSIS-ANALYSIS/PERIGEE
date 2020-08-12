1796a9e34461e376e9a734d0dec4185c23c2a85d
Ju Liu <liujuy@gmail.com>
2020-08-12 16:51:12 -0700

    natural bc assembly in

--- examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
+++ examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
@@ -188,6 +188,14 @@ class PGAssem_2x2Block_NS_FEM
 
     void EssBC_G( const ALocal_NodalBC * const &nbc_part );
 
+    void NatBC_G( const double &curr_time, const double &dt,
+        IPLocAssem_2x2Block * const &lassem_ptr,
+        FEAElement * const &element_s,
+        const IQuadPts * const &quad_s,
+        const ALocal_NodalBC * const &nbc_part,
+        const ALocal_EBC * const &ebc_part );
+
+
     void GetLocal(const double * const &array, const int * const &IEN,
         double * const &local_array) const
     {
