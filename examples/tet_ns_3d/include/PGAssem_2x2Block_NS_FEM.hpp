db6dc97ab199ae40fe081402b628eeb429328d79
Ju Liu <liujuy@gmail.com>
2020-08-12 11:14:28 -0700

    remove some code

--- examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
+++ examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
@@ -122,42 +122,6 @@ class PGAssem_2x2Block_NS_FEM
 
 
 
-   
-    
-    virtual double Assem_surface_flowrate(
-        const PDNSolution * const &sol,
-        IPLocAssem_2x2Block * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const APart_Node * const &pnode_ptr,
-        const ALocal_EBC * const &ebc_part,
-        const int &ebc_id );
-
-    virtual double Assem_surface_flowrate(
-        const PDNSolution * const &sol,
-        IPLocAssem_2x2Block * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const APart_Node * const &pnode_ptr,
-        const ALocal_Inflow_NodalBC * const &infbc_part );
-
-    virtual double Assem_surface_ave_pressure(
-        const PDNSolution * const &sol,
-        IPLocAssem_2x2Block * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const APart_Node * const &pnode_ptr,
-        const ALocal_EBC * const &ebc_part,
-        const int &ebc_id );
-
-    virtual double Assem_surface_ave_pressure(
-        const PDNSolution * const &sol,
-        IPLocAssem_2x2Block * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const APart_Node * const &pnode_ptr,
-        const ALocal_Inflow_NodalBC * const &infbc_part );
-
 
   private:
     // --------------------------------------------------------------
@@ -185,17 +149,6 @@ class PGAssem_2x2Block_NS_FEM
     void EssBC_KG( const ALocal_NodalBC * const &nbc_part );
 
     void EssBC_G( const ALocal_NodalBC * const &nbc_part );
-
-    void NatBC_Resis_KG( const double &dt,
-        const PDNSolution * const &dot_sol,
-        const PDNSolution * const &sol,
-        IPLocAssem_2x2Block * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const APart_Node * const &node_ptr,
-        const ALocal_NodalBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc );
 
     void GetLocal(const double * const &array, const int * const &IEN,
         double * const &local_array) const
