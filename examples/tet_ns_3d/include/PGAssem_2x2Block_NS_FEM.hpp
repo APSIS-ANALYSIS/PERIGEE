912858005f5cd6bdc0a93d5821976351da095aad
Ju Liu <liujuy@gmail.com>
2020-08-13 00:37:00 -0700

    one more in

--- examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
+++ examples/tet_ns_3d/include/PGAssem_2x2Block_NS_FEM.hpp
@@ -213,6 +213,16 @@ class PGAssem_2x2Block_NS_FEM
         const ALocal_EBC * const &ebc_part,
         const IGenBC * const &gbc );
 
+    void NatBC_Resis_KG( const double &dt,
+        const PDNSolution * const &dot_sol,
+        const PDNSolution * const &sol,
+        IPLocAssem_2x2Block * const &lassem_ptr,
+        FEAElement * const &element_s,
+        const IQuadPts * const &quad_s,
+        const ALocal_NodalBC * const &nbc_part,
+        const ALocal_EBC * const &ebc_part,
+        const IGenBC * const &gbc );
+
 
     void GetLocal(const double * const &array, const int * const &IEN,
         double * const &local_array) const
