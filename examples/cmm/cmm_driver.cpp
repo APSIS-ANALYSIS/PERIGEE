45ce613c7b3d2014290331cf9abc08d2863232e0
Ingrid Lan <ingridl@stanford.edu>
2020-11-20 17:25:32 -0800

    CMM driver: until local assembly initialization

--- examples/cmm/cmm_driver.cpp
+++ examples/cmm/cmm_driver.cpp
@@ -2,7 +2,7 @@
 // cmm_driver.cpp
 //
 // Tetrahedral element based finite element code for the 3D Coupled-Momentum
-// Method using the Variational Multiscale Formulation and Generalize-
+// Method using the Variational Multiscale Formulation and Generalized-
 // alpha time stepping.
 //
 // ==================================================================
@@ -11,18 +11,18 @@
 #include "ALocal_EBC_outflow.hpp"
 #include "ALocal_EBC_wall.hpp"
 #include "ALocal_Inflow_NodalBC.hpp"
-// #include "QuadPts_Gauss_Triangle.hpp"
-// #include "QuadPts_Gauss_Tet.hpp"
-// #include "FEAElement_Tet4.hpp"
-// #include "FEAElement_Tet10_v2.hpp"
-// #include "FEAElement_Triangle3_3D_der0.hpp"
-// #include "FEAElement_Triangle6_3D_der0.hpp"
-// #include "CVFlowRate_Unsteady.hpp"
-// #include "CVFlowRate_Linear2Steady.hpp"
+#include "QuadPts_Gauss_Triangle.hpp"
+#include "QuadPts_Gauss_Tet.hpp"
+#include "FEAElement_Tet4.hpp"
+#include "FEAElement_Tet10_v2.hpp"
+#include "FEAElement_Triangle3_3D_der0.hpp"
+#include "FEAElement_Triangle6_3D_der0.hpp"
+#include "CVFlowRate_Unsteady.hpp"
+#include "CVFlowRate_Linear2Steady.hpp"
 // #include "GenBC_Resistance.hpp"
 // #include "GenBC_RCR.hpp"
 // #include "GenBC_Inductance.hpp"
-// #include "PLocAssem_Tet_VMS_NS_GenAlpha.hpp"
+#include "PLocAssem_Tet_VMS_NS_GenAlpha.hpp"
 // #include "PGAssem_NS_FEM.hpp"
 #include "PTime_NS_Solver.hpp"
 
@@ -48,8 +48,8 @@ int main( int argc, char *argv[] )
   // Inflow file
   std::string inflow_file("inflow_fourier_series.txt");
 
-  double inflow_thd_time = 1.0;      // prescribed time for inflow to reach steadness
-  double inflow_tgt_rate = 1.0;      // prescribed flow rate at steady state
+  double inflow_thd_time = 1.0;      // time for linearly increasing inflow to reach steady state
+  double inflow_tgt_rate = 1.0;      // inflow upon reaching steady state
 
   // LPN file
   std::string lpn_file("lpn_rcr_input.txt");
@@ -75,10 +75,10 @@ int main( int argc, char *argv[] )
   double initial_time = 0.0;         // time of initial condition
   double initial_step = 0.1;         // time step size
   int    initial_index = 0;          // index of initial condition
-  double final_time = 1.0;           // final time
+  double final_time = 1.0;           // end time of simulation
   std::string sol_bName("SOL_");     // base name of the solution file
   int    ttan_renew_freq = 1;        // frequency of tangent matrix renewal
-  int    sol_record_freq = 1;        // frequency of recording the solution
+  int    sol_record_freq = 1;        // frequency for recording the solution
 
   // Restart options
   bool   is_restart = false;
@@ -146,7 +146,7 @@ int main( int argc, char *argv[] )
   SYS_T::cmdPrint(      "-wall_kappa:",      wall_kappa);
 
   // If the inflow file exists, print its filename.
-  // Otherwise, print parameters for linear2steady inflow setting 
+  // Otherwise, print parameters for linear2steady inflow setting. 
   if( SYS_T::file_exist( inflow_file ) )
     SYS_T::cmdPrint(    "-inflow_file:",     inflow_file);
   else
@@ -209,7 +209,7 @@ int main( int argc, char *argv[] )
 
   MPI_Barrier(PETSC_COMM_WORLD);
 
-  // ===== Data from Files =====
+  // ===== Read data from partition files =====
   // Control points' xyz coordinates
   FEANode * fNode = new FEANode(part_file, rank);
 
@@ -225,33 +225,97 @@ int main( int argc, char *argv[] )
   // Local sub-domain's element indices
   ALocal_Elem * locElem = new ALocal_Elem(part_file, rank);
 
-  // Local sub-domain's nodal bc
+  // Local sub-domain's nodal (Dirichlet) BC
   ALocal_NodalBC * locnbc = new ALocal_NodalBC(part_file, rank);
 
-  // Local sub-domain's inflow bc
+  // Local sub-domain's inflow (Dirichlet) BC
   ALocal_Inflow_NodalBC * locinfnbc = new ALocal_Inflow_NodalBC(part_file, rank);
 
-  // Local sub-domain's outflow elemental bc
+  // Local sub-domain's outflow elemental (Neumann) BC
   ALocal_EBC * locebc = new ALocal_EBC_outflow(part_file, rank);
 
-  // Local sub-domain's wall elemental bc for CMM
+  // Local sub-domain's wall elemental (Neumann) BC for CMM
   ALocal_EBC * locebc_wall = new ALocal_EBC_wall(part_file, rank, "ebc_wall");
 
   // Local sub-domain's nodal indices
   APart_Node * pNode = new APart_Node(part_file, rank);
 
-  SYS_T::commPrint("===> Data from HDF5 files are read from disk.\n");
+  SYS_T::commPrint("===> Data from HDF5 files read from disk.\n");
 
   SYS_T::print_fatal_if( size!= PartBasic->get_cpu_size(),
       "Error: Assigned CPU number does not match the partition. \n");
 
-  SYS_T::commPrint("===> %d processor(s) are assigned for FEM analysis. \n", size);
+  SYS_T::commPrint("===> %d processor(s) assigned for FEM analysis. \n", size);
+
+  // ===== Prescribed inflow =====
+  SYS_T::commPrint("===> Set up inflow. \n");
+
+  ICVFlowRate * inflow_rate_ptr = nullptr;
+
+  // If inflow file exists, prescribe it. Otherwise, prescribe an inflow that 
+  // linearly increases until a steady flow rate.
+  if( SYS_T::file_exist( inflow_file ) )
+    inflow_rate_ptr = new CVFlowRate_Unsteady( inflow_file.c_str() );
+  else
+    inflow_rate_ptr = new CVFlowRate_Linear2Steady( inflow_thd_time, inflow_tgt_rate );
+
+  inflow_rate_ptr->print_info();
+
+  // ===== Quadrature rules =====
+  SYS_T::commPrint("===> Build quadrature rules. \n");
+  IQuadPts * quadv = new QuadPts_Gauss_Tet( nqp_tet );
+  IQuadPts * quads = new QuadPts_Gauss_Triangle( nqp_tri );
+
+  // ===== Finite element containers =====
+  SYS_T::commPrint("===> Set up volumetric and surface element containers. \n");
+  FEAElement * elementv = nullptr;
+  FEAElement * elements = nullptr;
+
+  if( GMIptr->get_elemType() == 501 )          // linear tet
+  {
+    if( nqp_tet > 5 ) SYS_T::commPrint("Warning: Requested > 5 volumetric quadrature points for a linear tet element.\n");
+    if( nqp_tri > 4 ) SYS_T::commPrint("Warning: Reqiested > 4 surface quadrature points for a linear tri element.\n");
+
+    elementv = new FEAElement_Tet4( nqp_tet );
+    elements = new FEAElement_Triangle3_3D_der0( nqp_tri );
+  }
+  else if( GMIptr->get_elemType() == 502 )     // quadratic tet
+  {
+    SYS_T::print_fatal_if( nqp_tet < 29, "Error: not enough quadrature points for quadratic tet element.\n" );
+    SYS_T::print_fatal_if( nqp_tri < 13, "Error: not enough quadrature points for quadratic tri element.\n" );
+
+    elementv = new FEAElement_Tet10_v2( nqp_tet );
+    elements = new FEAElement_Triangle6_3D_der0( nqp_tri );
+  }
+  else SYS_T::print_fatal("Error: Element type not supported.\n");
+
+  // ===== Generate a sparse matrix for enforcing nodal BCs ====
+  Matrix_PETSc * pmat = new Matrix_PETSc(pNode, locnbc);
+
+  pmat->gen_perm_bc(pNode, locnbc);
+
+  // ===== Generalized-alpha =====
+  SYS_T::commPrint("===> Set up the generalized-alpha time integration scheme.\n");
+
+  TimeMethod_GenAlpha * tm_galpha_ptr = new TimeMethod_GenAlpha(
+      genA_rho_inf, false );
+
+  tm_galpha_ptr->print_info();
+
+  // ===== Local assembly routine =====
+  IPLocAssem * locAssem_ptr = new PLocAssem_Tet_VMS_NS_GenAlpha(
+      tm_galpha_ptr, GMIptr->get_nLocBas(),
+      quadv->get_num_quadPts(), elements->get_nLocBas(),
+      fluid_density, fluid_mu, bs_beta, c_tauc, GMIptr->get_elemType() );
 
 
-  // ===== Clean Memory =====
+  // ===== Deallocate memory =====
   delete fNode; delete locIEN; delete GMIptr; delete PartBasic;
   delete locElem; delete locnbc; delete locinfnbc;
   delete locebc; delete locebc_wall; delete pNode;
+  delete inflow_rate_ptr; delete quadv; delete quads;
+  delete elementv; delete elements; delete pmat;
+  delete tm_galpha_ptr; delete locAssem_ptr; 
 
   PetscFinalize();
   return EXIT_SUCCESS;
