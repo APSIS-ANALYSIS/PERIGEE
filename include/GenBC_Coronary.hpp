3ab6528b450fe48d79361d1d57a8816b3262f010
Ju Liu <liujuy@gmail.com>
2021-01-24 14:25:36 +0800

    initial commit for the genbc branch. head file for GenBC_Coronary setted up

---
+++ include/GenBC_Coronary.hpp
@@ -0,0 +1,8 @@
+#ifndef GENBC_CORONARY_HPP
+#define GENBC_CORONARY_HPP
+// ==================================================================
+// GenBC_Coronary.hpp
+// ==================================================================
+#include "IGenBC.hpp"
+
+#endif
