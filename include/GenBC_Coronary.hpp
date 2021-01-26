031ecb115f1328fd410438ed2c1ff931772ac376
Weiguang Yang <wgyang@stanford.edu>
2021-01-26 00:12:34 -0800

    Add memeber functions and variables to GenBC_Coronary.hpp. Add the class constructor to read coronary BC inputs.

--- include/GenBC_Coronary.hpp
+++ include/GenBC_Coronary.hpp
@@ -4,5 +4,100 @@
 // GenBC_Coronary.hpp
 // ==================================================================
 #include "IGenBC.hpp"
+class GenBC_Coronary : public IGenBC
+{
+  public:
+    GenBC_Coronary( const char * const &lpn_filename, const int &in_N,
+       const double &dt3d );
+
+    virtual ~GenBC_Coronary();
+
+    virtual void print_info() const;
+
+    virtual int get_num_ebc() const {return num_ebc;}
+
+    virtual double get_m( const int &ii, const double &in_dot_Q,
+       const double &in_Q ) const ;
+
+    virtual double get_n( const int &ii, const double &in_dot_Q,
+       const double &in_Q ) const
+    {
+      return 0.0;
+    }
+
+    // Obtain P for the ii-th outlet surface (coronary or RCR).
+    virtual double get_P( const int &ii, const double &in_dot_Q,
+       const double &in_Q ) const;
+
+    // Get initial P for the ii-th outlet face at the begining of LPM ODE integration.
+    virtual double get_P0( const int &ii ) const;
+
+    // Set initial values for the LPM ODE integration.
+    virtual void reset_initial_sol( const int &ii, const double &in_Q_0,
+       const double &in_P_0, const double &curr_time );
+
+  private:
+    const int N;
+
+    const double h; // delta t = Nh
+
+    // Parameters used to define difference quotient for get_m.
+    const double absTol, relTol;
+
+    int num_ebc;
+
+    // starting time for integrating the coronary LPM
+    double tstart;
+
+    // ending time for integrating the coronary LPM
+    double tend;
+
+    // Vectors storing the Ra, Ca, Ra_micro, Cim, Rv, Pd, Pim scaling values for all coronary outlet faces
+    // the length of the vectors is num_ebc
+    std::vector<double> Ra,Ca,Ra_micro,Cim,Rv,Pd,alpha_Pim;
+
+    // number of intramyocardial pressure Pim data points for each outlet face
+    // The vector length is num_ebc, num_Pimdata=0 indicates an RCR outlet.
+    std::vector<int> num_Pimdata;
+
+    // tdata and Pimdata for user-provided intramyocardial pressure waveform (time-pressure) for each coronary outlet face
+    // Pimderdata for the corresponding dPim/dt for each coronary outlet face.
+    std::vector<std::vector<double> > tdata,Pimdata,Pimderdata;
+
+    // precomputed dPim/dt needed by RK4
+    std::vector<std::vector<double>> dPimdt_k1,dPimdt_k2,dPimdt_k3;
+
+    // prev_0D_sol records solutions when each ODE integration is completed.
+    mutable std::vector<std::vector<double> > prev_0D_sol;
+
+    // Vectors for outlet initial flow and capacitor pressures (2 capacitors)
+    std::vector<double> Q0;
+
+    std::vector<std::vector<double>> Pi0;
+
+    // Evaluate the coronary LPM (2 first order ODEs) for the ii-th outlet face (which is a coronary outlet) and output the ODE derivatives to K.
+    void F( const int &ii, const double * const &pi, const double &q, const double &dPimdt, double * const &K) const;
+
+    // Evaluate the RCR ODE for the ii-th outlet face and return the result.
+    double F( const int &ii, const double &pi, const double &q) const;
+
+    // Pre-compute dPim/dt at the begining of the ODE integration for the ii-th outlet face
+    void get_dPimdt( const int &ii);
+
+    // Evaluate the derivatives of a piecewise cubic hermite interpolating polynomial (PCHIP) between points x1 and x2 with values f1, f2 and derivatives d1 and d2
+    // for points xe with a size ne. Outputs: fe
+    void  cubic_hermite_derivative( const double &x1, const double &x2, const double &f1, const double &f2,
+       const double &d1, const double &d2, const int &ne, const std::vector<double> &xe, std::vector<double> &fe );
+
+    // Set PCHIP for Pim data for the ii-th outlet face.
+    void set_phcip( const int &ii);
+
+    // Set derivatives for a PCHIP with user provided points and values.
+    void spline_pchip_set( const int &n, const std::vector<double> &x, const std::vector<double> &f, std::vector<double> &d);
+
+    // PCHIP sign-testing routine.
+    double pchst( const double &arg1, const double &arg2 ) const;
+
+};
 
 #endif
---
+++ src/Model/GenBC_Coronary.cpp
@@ -0,0 +1,163 @@
+#include "GenBC_Coronary.hpp"
+
+GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_N,
+    const double &dt3d )
+: N( in_N ), h( dt3d/static_cast<double>(N) ),
+  absTol( 1.0e-8 ), relTol( 1.0e-5 )
+{
+  // Now read the lpn input file for num_ebc and coronary model parameters (Ra, Ca, Ra_micro, Cim, Rv, Pd and Pim)
+  std::string temp_name( lpn_filename );
+  SYS_T::file_check( temp_name ); // make sure the file is on the disk
+
+  std::ifstream reader;
+  reader.open( lpn_filename, std::ifstream::in );
+
+  std::istringstream sstrm;
+  std::string sline;
+  std::string bc_type;
+  tstart=0.0;
+  tend=N*h;
+  // The first non-commented lien should be
+  // Coronary num_ebc
+  while( std::getline(reader, sline) )
+  {
+    if( sline[0] != '#' && !sline.empty() )
+    {
+      sstrm.str(sline);
+      sstrm >> bc_type;
+      sstrm >> num_ebc;
+      sstrm.clear();
+      break;
+    }
+  }
+
+  // Check the file's bc_type matches RCR
+  if( bc_type.compare("Coronary") ==0
+      || bc_type.compare("CORONARY") == 0
+      || bc_type.compare("coronary") == 0 )
+  {
+    Ra.resize( num_ebc ); Ca.resize( num_ebc );
+    Ra_micro.resize( num_ebc ); Cim.resize( num_ebc );
+    Rv.resize( num_ebc ); Pd.resize( num_ebc );
+    alpha_Pim.resize(num_ebc);Q0.resize(num_ebc);
+    Pi0.resize(num_ebc); num_Pimdata.resize(num_ebc);
+    tdata.resize(num_ebc);Pimdata.resize(num_ebc);
+    Pimderdata.resize(num_ebc);prev_0D_sol.resize(num_ebc);
+    dPimdt_k1.resize(num_ebc);dPimdt_k2.resize(num_ebc);
+    dPimdt_k3.resize(num_ebc);
+
+    for(int ii =0;ii<num_ebc;++ii)
+    {
+      prev_0D_sol[ii].resize(2);
+      Pi0[ii].resize(2);
+    }
+
+    for(int ii=0;ii<num_ebc;++ii)
+    {
+      dPimdt_k1[ii].resize(N+1);
+      dPimdt_k2[ii].resize(N);
+      dPimdt_k3[ii].resize(N);
+    }
+  }
+  else SYS_T::print_fatal("Error: the outflow model in %s does not match GenBC_Coronary.\n", lpn_filename);
+
+  // Read files for each ebc to set the values of Ra, Ca, Ra_micro, Cim,Rv,Pd Pim and alpha_Pim
+  int counter = 0;
+  int data_size=1;
+  while( std::getline(reader, sline) )
+  {
+    if( sline[0] != '#' && !sline.empty() )
+    {
+      sstrm.str( sline );
+      int face_id;
+      sstrm >> face_id;
+
+      // Make sure the face_id, the first column in the file are listed
+      // from 0 to ebc_id - 1
+      if(face_id != counter) SYS_T::print_fatal("Error: GenBC_Coronary the input file %s has wrong format in the face id column (the first column). \n", lpn_filename);
+
+      sstrm >> Ra[ counter ];
+      sstrm >> Ca[ counter ];
+      sstrm >> Ra_micro[ counter ];
+      sstrm >> Cim[ counter ];
+      sstrm >> Rv[ counter ];
+      sstrm >> Pd[ counter ];
+      sstrm >> num_Pimdata[ counter ];
+      sstrm >> alpha_Pim[ counter ];
+
+      SYS_T::print_fatal_if(num_Pimdata[counter]<=2 && num_Pimdata[counter]!=0, "Error: num of Pim data needs to be 0 for RCR or >2 for coronary  \n");
+
+      if(num_Pimdata[counter]>0)
+      {
+       data_size=num_Pimdata[counter];
+      }
+      else
+      {
+       data_size=1;
+      }
+
+      tdata[counter].resize(data_size);
+      Pimdata[counter].resize(data_size);
+      Pimderdata[counter].resize(data_size);
+
+      sstrm.clear();
+
+      for (int ii =0;ii<num_Pimdata[counter];++ii)
+      {
+        getline(reader, sline);
+        sstrm.str( sline );
+        sstrm>>tdata[counter][ii];
+
+        sstrm>>Pimdata[counter][ii];
+        Pimdata[counter][ii]=Pimdata[counter][ii]*alpha_Pim[counter];
+        sstrm.clear();
+      }
+
+      if(num_Pimdata[counter]>0)
+      {
+        set_phcip(counter);
+        get_dPimdt(counter);
+      }
+
+      SYS_T::print_fatal_if(tdata[counter][0]>0.0, "Error: Pim data do not start from 0.\n");
+      counter += 1;
+    }
+  }
+
+  if(counter != num_ebc ) SYS_T::print_fatal("Error: GenBC_Coronary the input file %s does not contain complete data for outlet faces. \n", lpn_filename);
+
+  reader.close();
+
+  SYS_T::commPrint( "===> GenBC_Coronary data are read in from %s.\n", lpn_filename );
+
+  // Set a zero initial value. They should be reset based on the initial
+  // 3D solutions.
+  for(int ii=0; ii<num_ebc; ++ii)
+  {
+    Q0[ii] = 0.0;
+    Pi0[ii][0] = 0.0;
+    Pi0[ii][1]=0.0;
+    prev_0D_sol[ii][0]=0.0;
+    prev_0D_sol[ii][1]=0.0;
+
+    // Make sure C and R are nonzero
+    SYS_T::print_fatal_if(Ca[ii]==0.0, "Error: GenBC_Coronary Ca cannot be zero.\n");
+    SYS_T::print_fatal_if(Cim[ii]==0.0, "Error: GenBC_Coronary Cim cannot be zero.\n");
+    SYS_T::print_fatal_if(Ra_micro[ii]==0.0, "Error: GenBC_Coronary Ra_micro cannot be zero.\n");
+    SYS_T::print_fatal_if(Rv[ii]==0.0, "Error: GenBC_Coronary Rv cannot be zero.\n");
+  }
+}
+
+GenBC_Coronary::~GenBC_Coronary()
+{}
+
+void GenBC_Coronary::print_info() const
+{
+  SYS_T::commPrint( "     Coronary model: N = %d, h = %e, num_ebc = %d \n", N, h, num_ebc );
+
+  for(int ii=0; ii<num_ebc; ++ii)
+    SYS_T::commPrint( "     ebcid = %d, Ra = %e, Ca = %e, Ra_micro = %e,Rim = %e, Rv = %e, Pd = %e \n", ii, Ra[ii], Ca[ii],Ra_micro[ii],Cim[ii], Rv[ii], Pd[ii] );
+}
+
+
+// EOF
