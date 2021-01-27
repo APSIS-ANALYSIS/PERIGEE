75954e5a6c8a4d2b6ca51b397bff11552a88382a
Ju Liu <liujuy@gmail.com>
2021-01-27 10:28:45 +0800

    formatted the code

--- src/Model/GenBC_Coronary.cpp
+++ src/Model/GenBC_Coronary.cpp
@@ -1,11 +1,12 @@
 #include "GenBC_Coronary.hpp"
 
-GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_N,
-    const double &dt3d )
+GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, 
+    const int &in_N, const double &dt3d )
 : N( in_N ), h( dt3d/static_cast<double>(N) ),
   absTol( 1.0e-8 ), relTol( 1.0e-5 )
 {
-  // Now read the lpn input file for num_ebc and coronary model parameters (Ra, Ca, Ra_micro, Cim, Rv, Pd and Pim)
+  // Now read the lpn input file for num_ebc and coronary model 
+  // parameters (Ra, Ca, Ra_micro, Cim, Rv, Pd and Pim)
   std::string temp_name( lpn_filename );
   SYS_T::file_check( temp_name ); // make sure the file is on the disk
 
@@ -17,7 +18,8 @@ GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_
   std::string bc_type;
   tstart=0.0;
   tend=N*h;
-  // The first non-commented lien should be
+  
+  // The first non-commented line should be
   // Coronary num_ebc
   while( std::getline(reader, sline) )
   {
@@ -31,20 +33,28 @@ GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_
     }
   }
 
-  // Check the file's bc_type matches RCR
+  // Check the file's bc_type matches Coronary
   if( bc_type.compare("Coronary") ==0
       || bc_type.compare("CORONARY") == 0
       || bc_type.compare("coronary") == 0 )
   {
-    Ra.resize( num_ebc ); Ca.resize( num_ebc );
-    Ra_micro.resize( num_ebc ); Cim.resize( num_ebc );
-    Rv.resize( num_ebc ); Pd.resize( num_ebc );
-    alpha_Pim.resize(num_ebc);Q0.resize(num_ebc);
-    Pi0.resize(num_ebc); num_Pimdata.resize(num_ebc);
-    tdata.resize(num_ebc);Pimdata.resize(num_ebc);
-    Pimderdata.resize(num_ebc);prev_0D_sol.resize(num_ebc);
-    dPimdt_k1.resize(num_ebc);dPimdt_k2.resize(num_ebc);
-    dPimdt_k3.resize(num_ebc);
+    Ra.resize( num_ebc ); 
+    Ca.resize( num_ebc );
+    Ra_micro.resize( num_ebc ); 
+    Cim.resize( num_ebc );
+    Rv.resize( num_ebc ); 
+    Pd.resize( num_ebc );
+    alpha_Pim.resize( num_ebc );
+    Q0.resize( num_ebc );
+    Pi0.resize( num_ebc ); 
+    num_Pimdata.resize( num_ebc );
+    tdata.resize( num_ebc );
+    Pimdata.resize( num_ebc );
+    Pimderdata.resize( num_ebc );
+    prev_0D_sol.resize( num_ebc );
+    dPimdt_k1.resize( num_ebc );
+    dPimdt_k2.resize( num_ebc );
+    dPimdt_k3.resize( num_ebc );
 
     for(int ii =0;ii<num_ebc;++ii)
     {
@@ -61,7 +71,8 @@ GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_
   }
   else SYS_T::print_fatal("Error: the outflow model in %s does not match GenBC_Coronary.\n", lpn_filename);
 
-  // Read files for each ebc to set the values of Ra, Ca, Ra_micro, Cim,Rv,Pd Pim and alpha_Pim
+  // Read files for each ebc to set the values of Ra, Ca, Ra_micro, 
+  // Cim,Rv, Pd Pim and alpha_Pim
   int counter = 0;
   int data_size=1;
   while( std::getline(reader, sline) )
@@ -74,7 +85,7 @@ GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_
 
       // Make sure the face_id, the first column in the file are listed
       // from 0 to ebc_id - 1
-      if(face_id != counter) SYS_T::print_fatal("Error: GenBC_Coronary the input file %s has wrong format in the face id column (the first column). \n", lpn_filename);
+      SYS_T::print_fatal_if( face_id != counter, "Error: GenBC_Coronary the input file %s has wrong format in the face id column (the first column). \n", lpn_filename);
 
       sstrm >> Ra[ counter ];
       sstrm >> Ca[ counter ];
@@ -85,7 +96,8 @@ GenBC_Coronary::GenBC_Coronary( const char * const &lpn_filename, const int &in_
       sstrm >> num_Pimdata[ counter ];
       sstrm >> alpha_Pim[ counter ];
 
-      SYS_T::print_fatal_if(num_Pimdata[counter]<=2 && num_Pimdata[counter]!=0, "Error: num of Pim data needs to be 0 for RCR or >2 for coronary  \n");
+      SYS_T::print_fatal_if(num_Pimdata[counter]<=2 && num_Pimdata[counter]!=0, 
+          "Error: num of Pim data needs to be 0 for RCR or >2 for coronary  \n");
 
       if(num_Pimdata[counter]>0)
       {
