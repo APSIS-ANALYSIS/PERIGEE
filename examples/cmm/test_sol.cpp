a865978e3ec287f9d10c1bddcc755421d6d42732
Ju Liu <liujuy@gmail.com>
2021-02-19 23:26:05 +0800

    in

--- examples/cmm/test_sol.cpp
+++ examples/cmm/test_sol.cpp
@@ -6,6 +6,17 @@ int main( int argc, char *argv[] )
 {
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
 
+  const PetscMPIInt rank = SYS_T::get_MPI_rank();
+
+  std::string part_file("part");
+
+  APart_Node * pNode = new APart_Node(part_file, rank);
+
+  PDNSolution * sol_a = new PDNSolution( pNode );
+  
+  delete sol_a;
+  delete pNode; 
+
   PetscFinalize();
   return 0;
 }
