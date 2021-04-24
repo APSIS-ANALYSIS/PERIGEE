547b5d4e53cfcb05ea8b65116b02a45477ffc306
Ju Liu <liujuy@gmail.com>
2021-04-24 13:58:27 +0800

    compile on linux box

--- conf/ladyzhenskaya.cmake
+++ conf/ladyzhenskaya.cmake
@@ -4,17 +4,18 @@ set(HOME_DIR /home/juliu)
 # ========================================================
 # Specify the library locations
 # ========================================================
-set(VTK_DIR ${HOME_DIR}/lib/VTK-8.2.0-shared/lib/cmake/vtk-8.2)
+set(VTK_DIR ${HOME_DIR}/lib/VTK-8.2.0-SHARED/lib/cmake/vtk-8.2)
 
 if( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
-  set(PETSC_DIR ${HOME_DIR}/lib/petsc-3.14.3-opt)
+  set(PETSC_DIR ${HOME_DIR}/lib/petsc-3.14.5-opt)
 else( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
-  set(PETSC_DIR ${HOME_DIR}/lib/petsc-3.14.3-debug)
+  set(PETSC_DIR ${HOME_DIR}/lib/petsc-3.14.5-debug)
 endif( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
 
 set(PETSC_ARCH .)
 
-set(HDF5_ROOT ${HOME_DIR}/lib/hdf5-1.8.16)
+set(HDF5_ROOT ${HOME_DIR}/lib/hdf5-1.12.0)
+set(BESSEL_DIR ${HOME_DIR}/lib/complex_bessel)
 
 # ========================================================
 # Setup the libraries
@@ -29,6 +30,12 @@ find_package(PETSc REQUIRED)
 include_directories(${VTK_INCLUDE_DIRS})
 include_directories(${HDF5_INCLUDE_DIRS})
 include_directories(${PETSC_INC})
+include_directories(${BESSEL_DIR}/include/complex_bessel_bits)
+include_directories(${BESSEL_DIR}/include)
+
+LINK_DIRECTORIES( ${BESSEL_DIR}/lib )
+set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} complex_bessel)
+
 
 set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${VTK_LIBRARIES})
 set(EXTRA_LINK_LIBS ${EXTRA_LINK_LIBS} ${HDF5_LIBRARIES})
@@ -41,9 +48,9 @@ message(STATUS "External Libraries: " ${EXTRA_LINK_LIBS})
 # ========================================================
 # Compiler options 
 # ========================================================
-set(CMAKE_C_COMPILER  ${HOME_DIR}/lib/mpich-3.3.2/bin/mpicc)
-set(CMAKE_CXX_COMPILER ${HOME_DIR}/lib/mpich-3.3.2/bin/mpicxx)
-set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_C_COMPILER  ${HOME_DIR}/lib/mpich-3.3/bin/mpicc)
+set(CMAKE_CXX_COMPILER ${HOME_DIR}/lib/mpich-3.3/bin/mpicxx)
+set(CMAKE_CXX_STANDARD 14)
 
 if( ${CMAKE_BUILD_TYPE} MATCHES "Release" )
   set(CMAKE_CXX_FLAGS "-O3 -Wall")
