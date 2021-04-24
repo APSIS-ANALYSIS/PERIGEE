b4e9bbdb0faa41f39ae73ccdaaf473fcdeda9064
Ju Liu <liujuy@gmail.com>
2021-04-24 20:31:46 +0800

    added a few more comments for alocal ring nodalbc

--- include/ALocal_Ring_NodalBC.hpp
+++ include/ALocal_Ring_NodalBC.hpp
@@ -5,21 +5,20 @@
 //
 // Analysis-use, ring nodal indices.
 //
-// Date Created: July 8 2017
-// Author: Ju Liu
+// Author: Ingrid S. Lan
+// Date Created: April 15 2021
 // ==================================================================
 #include "HDF5_Reader.hpp"
 
 class ALocal_Ring_NodalBC
 {
   public:
-    ALocal_Ring_NodalBC( const std::string &fileBaseName,
-        const int &cpu_rank );
+    ALocal_Ring_NodalBC( const std::string &fileBaseName, const int &cpu_rank );
 
     virtual ~ALocal_Ring_NodalBC();
 
     // Get global index of a Dirichlet node in the local partition
-    // \para node ranges [ 0 , Num_LD ).
+    // para node ranges [ 0 , Num_LD ).
     virtual int get_LDN( const int &node ) const { return LDN[node]; }
 
     // Get the number of Dirichlet nodes in the local partition
@@ -28,15 +27,15 @@ class ALocal_Ring_NodalBC
     // get the Dirichlet node's dominant component index, where the
     // dominant component of the unit normal vector is the largest in
     // absolute value
-    // \para node ranges [ 0, Num_LD )
+    // parameter node ranges [ 0, Num_LD )
     virtual double get_dominant_comp( const int &node ) const
     { return dominant_comp[ cap_id[node] ]; }
 
     // get the Dirichlet node's outward normal vector components.
-    // \para node ranges [ 0, Num_LD )
+    // parameter node ranges [ 0, Num_LD )
     // comp=0 : x-component; comp=1 : y-component; comp=2 : z-component
     virtual double get_outvec( const int &node, const int &comp ) const
-    { return outnormal[ 3*(cap_id[node]) + comp ]; }
+    { return outnormal[ 3 * cap_id[node] + comp ]; }
 
     // determine whether a given index belongs to the LDN vector
     virtual bool is_inLDN( const int &ii) const
@@ -46,23 +45,23 @@ class ALocal_Ring_NodalBC
     // Number of local Dirichlet nodes
     int Num_LD;
 
-    // Global indices of the local Dirichlet nodes (ring BC)
+    // Global indices of the local Dirichlet nodes (of the ring BC type)
     std::vector<int> LDN;
 
-    // Number of caps (inlets, outlets)
+    // Number of caps (equals the number of inlets + the number of outlets)
     int num_caps;
 
-    // If Num_LD > 0, store corresponding cap ID: [0, num_caps)
-    // length Num_LD
+    // If Num_LD > 0, store corresponding cap ID, which ranges [0, num_caps)
+    // vector length is Num_LD
     std::vector<int> cap_id;
 
     // Dominant component index of each cap's unit normal vector: 0, 1, or 2
-    // length num_caps
+    // vector length is num_caps
     std::vector<int> dominant_comp;
 
-    // Each cap's unit normal vector, length 3 x num_caps
+    // Each cap's unit normal vector,
+    // vector length 3 x num_caps
     std::vector<double> outnormal;
-
 };
 
 #endif
