ab57111b8fd32db97b26fdccfe3cf66f13b85453
Ju Liu <liujuy@gmail.com>
2021-05-01 15:29:52 +0800

    set initialized container to have zero values

--- src/Analysis_Tool/ALocal_EBC_wall.cpp
+++ src/Analysis_Tool/ALocal_EBC_wall.cpp
@@ -40,7 +40,10 @@ ALocal_EBC_wall::ALocal_EBC_wall( const std::string &fileBaseName,
        "ALocal_EBC_wall: size of qua_prestress is inconsistent with face_nqp. \n");
    }
    else
+   {
      qua_prestress.resize( 6 * face_nqp * num_local_cell[0] );
+     for(int ii=0; ii< 6 * face_nqp * num_local_cell[0]; ++ii) qua_prestress[ii] = 0.0;
+   }
   }
 
   delete h5r; H5Fclose( file_id );
