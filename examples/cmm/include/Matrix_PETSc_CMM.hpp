84a699ec9af11da8dfb1d9f74bdc66b3a886a57d
Ingrid Lan <ingridl@stanford.edu>
2021-05-09 14:19:12 -0700

    Matrix_PETSc_CMM: gen_outlet_ring_radial_motion_bc() for type 4

--- examples/cmm/include/Matrix_PETSc_CMM.hpp
+++ examples/cmm/include/Matrix_PETSc_CMM.hpp
@@ -94,6 +94,33 @@ class Matrix_PETSc_CMM : public Matrix_PETSc
         const ALocal_NodalBC * const &bc_part,
         const ALocal_Ring_NodalBC * const &ring_bc_part );
 
+    // ------------------------------------------------------------------------
+    // Type 4: Generate a matrix accounting for the essential boundary conditions
+    // that describe inlet clamping and outlet radial motion for a 4-dof system.
+    // Assumption: the matrix system has 4 degrees-of-freedom per node and the
+    //             1st dof is pressure, the next 3 dofs are velocity.
+    // The constraint equations are
+    //             nx ux + ny uy + nz uz = 0
+    //             tx ux + ty uy + tz uz = 0
+    // Let a and b denote the dominant comp of n and t vectors respectively.
+    //             na ua + nb ub = -nc uc
+    //             ta ua + tb ub = -tc uc
+    // Solve that equation gives
+    //             ua = val1 uc
+    //             ub = val2 uc
+    //             val1 = (nb tc - nc tb) / (na tb - nb ta)
+    //             val2 = (ta nc - na tc) / (na tb - nb ta)         
+    // 1. For outlet ring nodes that belong to the ALocal_Ring_NodalBC class, the rows
+    //    for ua and ub with columns at uc are inserted with value val1 & val2,
+    //    respectively.
+    // 2. For the remaining essential BC nodes, we assign 0 to all entries in
+    //    all components' rows.
+    // 3. For all remaining nodes, we assign 1 to the diagonal entries. 
+    // ------------------------------------------------------------------------
+    void gen_outlet_ring_radial_motion_bc( const APart_Node * const &pnode_ptr,
+        const ALocal_NodalBC * const &bc_part,
+        const ALocal_Ring_NodalBC * const &ring_bc_part );
+
 };
 
 #endif
