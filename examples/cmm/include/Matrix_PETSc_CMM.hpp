6c94732412afde66741edc865616ee26b564f659
Ingrid Lan <ingridl@stanford.edu>
2021-05-09 14:06:32 -0700

    Matrix_PETSc_CMM: gen_outlet_ring_inplane_bc() for type 3

--- examples/cmm/include/Matrix_PETSc_CMM.hpp
+++ examples/cmm/include/Matrix_PETSc_CMM.hpp
@@ -73,6 +73,27 @@ class Matrix_PETSc_CMM : public Matrix_PETSc
         const ALocal_NodalBC * const &bc_part,
         const ALocal_Ring_NodalBC * const &ring_bc_part );
 
+    // ------------------------------------------------------------------------
+    // Type 3 : Generate a matrix accounting for the essential
+    // boundary conditions that describe inlet clamping and outlet in plane motion
+    // for a 4-dof system like the NS equations.
+    // Assumption: the matrix system has 4 degrees-of-freedom per node and the
+    //             1st dof is pressure, the next 3 dofs are velocity.
+    // 1. For outlet ring nodes that belong to the ALocal_Ring_NodalBC class, the
+    // dominant component's row will be modified. Let the dominant component's
+    // corresponding entry in the outward normal be Nd, and the remaining two
+    // non-dominant entries be Na, Nb. Then in the dominant component's row, the
+    // diagonal entry will be 0, and entries in the non-dominant components'
+    // columns will be -Na / Nd and - Nb / Nd. For the two non-dominant
+    // components' rows, diagonal entries will be 1.
+    // 2. For the remaining essential BC nodes, we assign 0 to all entries in all
+    // components' rows.
+    // 3. For all remaining nodes, we assign 1 to the diagonal entries.
+    // ------------------------------------------------------------------------
+    void gen_outlet_ring_inplane_bc( const APart_Node * const &pnode_ptr,
+        const ALocal_NodalBC * const &bc_part,
+        const ALocal_Ring_NodalBC * const &ring_bc_part );
+
 };
 
 #endif
--- examples/cmm/src/Matrix_PETSc_CMM.cpp
+++ examples/cmm/src/Matrix_PETSc_CMM.cpp
@@ -19,6 +19,10 @@ Matrix_PETSc_CMM::Matrix_PETSc_CMM( const APart_Node * const &pnode_ptr,
       gen_ring_radial_motion_bc( pnode_ptr, bc_part, ring_bc_part );
       SYS_T::commPrint("     Matrix_PETSc_CMM : matrix for radial motion.\n");
       break;
+    case 3:
+      gen_outlet_ring_inplane_bc( pnode_ptr, bc_part, ring_bc_part );
+      SYS_T::commPrint("     Matrix_PETSc_CMM : matrix for inlet clamping & outlet in-plane motion.\n");
+      break;
     default:
       SYS_T::print_fatal("Error: Matrix_PETSc_CMM has no such matrix type.\n");
       break;
@@ -129,6 +133,60 @@ void Matrix_PETSc_CMM::gen_ring_radial_motion_bc( const APart_Node * const &pnod
         
     MatSetValue(K, nrow, rrow, val1, INSERT_VALUES);
     MatSetValue(K, trow, rrow, val2, INSERT_VALUES);
+  }
+
+  MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);
+  MatAssemblyEnd(K, MAT_FINAL_ASSEMBLY);
+
+  is_set = true;
+}
+
+void Matrix_PETSc_CMM::gen_outlet_ring_inplane_bc( const APart_Node * const &pnode_ptr,
+    const ALocal_NodalBC * const &bc_part,
+    const ALocal_Ring_NodalBC * const &ring_bc_part )
+{
+  if(is_set) Clear();
+  SYS_T::print_fatal_if(m != n, "Error: This is not a square matrix. \n");
+
+  const int nnode = pnode_ptr->get_nlocalnode();
+  const int dof   = bc_part->get_dofMat();
+
+  // We enforce 4 dofs per node
+  SYS_T::print_fatal_if(dof != 4, "Error: Matrix_PETSc::gen_outlet_ring_inplane_bc assumes 4 dofs per node.\n");
+
+  for(int ii=0; ii<nnode; ++ii)
+  {
+    for(int jj=0; jj<dof; ++jj)
+    {
+      const int row = pnode_ptr->get_node_loc(ii) * dof + jj;
+      const int col = bc_part->get_LID(jj, ii) * dof + jj;
+      // For non-essential bc nodes, its diagonal entry will be assigned 1.0
+      MatSetValue(K, row, col, 1.0, INSERT_VALUES);
+    }
+  }
+
+  const int num_ring_node = ring_bc_part -> get_Num_LD();
+
+  for(int ii=0; ii<num_ring_node; ++ii)
+  {
+    const int cap_id = ring_bc_part -> get_cap_id( ii );
+
+    if( cap_id != 0 )
+    {
+      const int dnode = ring_bc_part -> get_LDN( ii ); // The ring node's nodal index
+      const int dcomp = ring_bc_part -> get_dominant_n_comp( ii );
+      const int row = dnode * dof + dcomp + 1;
+      const double bot = -1.0 * ring_bc_part -> get_outvec(ii, dcomp);
+      for(int jj=0; jj<3; ++jj)
+      {
+        if( jj != dcomp )
+        {
+          const int col = dnode * dof + jj + 1;
+          const double top = ring_bc_part -> get_outvec(ii, jj);
+          MatSetValue(K, row, col, top/bot, INSERT_VALUES);
+        }
+      }
+    }
   }
 
   MatAssemblyBegin(K, MAT_FINAL_ASSEMBLY);
