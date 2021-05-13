51a42aed59e31bdce22ad16c1d0f18620d068bcf
Ju Liu <liujuy@gmail.com>
2021-05-13 22:56:34 +0800

    need new cs

---
+++ include/NodalBC_3D_wall.hpp
@@ -0,0 +1,24 @@
+#ifndef NODALBC_3D_WALL_HPP
+#define NODALBC_3D_WALL_HPP
+// ============================================================================
+// NodalBC_3D_wall.hpp
+//
+// This is a class that stores the wall nodes with the ring nodes excluded.
+//
+// Author: Ju Liu
+// Datei Created: May 13 2021
+// ============================================================================
+#include "INodalBC.hpp"
+
+class NodalBC_3D_wall : public INodalBC
+{
+  public:
+    NodalBC_3D_wall( const std::string &inflow_file,
+        const std::string &wall_file,
+        const std::vector<std::string> &outflow_files,
+        const int &nFunc, const int &elemtype = 501 );
+
+    virtual ~NodalBC_3D_wall();
+};
+
+#endif
