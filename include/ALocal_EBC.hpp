732c3f9b9468c1cacf79f58f89a8305cf349334f
Ju Liu <liujuy@gmail.com>
2021-05-23 13:55:32 +0800

    in

--- include/ALocal_EBC.hpp
+++ include/ALocal_EBC.hpp
@@ -1,13 +1,13 @@
 #ifndef ALOCAL_EBC_HPP
 #define ALOCAL_EBC_HPP
-// ==================================================================
+// ============================================================================
 // ALocal_EBC.hpp
 //
 // Analysis-use local subdomain's elemental boundary conditions.
 //
 // Author: Ju Liu
 // Date: Jan. 16 2017
-// ==================================================================
+// ============================================================================
 #include "HDF5_Reader.hpp"
 #include "QuadPts_Gauss_Triangle.hpp"
 
@@ -37,11 +37,11 @@ class ALocal_EBC
     {return num_local_node[ii];}
 
     // 0 <= ii < num_ebc
-    virtual int get_num_local_cell(const int &ii) const
+    virtual int get_num_local_cell(const int &ii) const 
     {return num_local_cell[ii];}
 
     // 0 <= ii < num_ebc
-    virtual int get_cell_nLocBas(const int &ii) const
+    virtual int get_cell_nLocBas(const int &ii) const 
     {return cell_nLocBas[ii];}
 
     // 0 <= ii < num_ebc
@@ -69,7 +69,7 @@ class ALocal_EBC
     virtual int get_local_global_cell(const int &ii, const int &jj) const
     {return local_global_cell[ii][jj];}
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_ctrlPts_xyz: given the ebc_id ii, the element index eindex,
     // return the control points' geometry.
     // Users are responsible for allocating & deleting the ctrl_xyz
@@ -78,29 +78,27 @@ class ALocal_EBC
     // surface element id: 0 <= eindex < num_local_cell[ii];
     // ctrl_x/y/z : output geometry array, length is 
     //              cell_nLocBas[ii].
-    // --------------------------------------------------------------
-    virtual void get_ctrlPts_xyz(const int &ii, 
-        const int &eindex, double * const &ctrl_x, 
-        double * const &ctrl_y, double * const &ctrl_z ) const;
+    // ------------------------------------------------------------------------
+    virtual void get_ctrlPts_xyz(const int &ii, const int &eindex, 
+        double * const &ctrl_x, double * const &ctrl_y, double * const &ctrl_z ) const;
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_SIEN: returns the surface element's IEN.
     // The users are responsible for allocating and deleting the sien
     // array.
     // ebc_id : 0 <= ii < num_ebc;
     // eindex : 0 <= eindex < num_local_cell[ii]
     // sien : length cell_nLocBas[ii].
-    // --------------------------------------------------------------
-    virtual void get_SIEN( const int &ii,
-        const int &eindex, int * const &sien ) const;
+    // ------------------------------------------------------------------------
+    virtual void get_SIEN( const int &ii, const int &eindex, int * const &sien ) const;
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_intPts_xyz: returns the surface element's interior point
     // coordinates.
     // ebc_id : 0 <= ii < num_ebc;
     // eindex : 0 <= eindex < num_local_cell[ii];
     // coor_x/y/z : output interior point coordinates
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_intPts_xyz(const int &ii,
         const int &eindex, double &coor_x, double &coor_y,
         double &coor_z ) const
@@ -108,10 +106,9 @@ class ALocal_EBC
       SYS_T::print_fatal("Error: ALocal_EBC::get_intPts_xyz is not implemented. \n");
     }
 
-    // --------------------------------------------------------------
-    // Generate a file name for outlet face ii
-    // Outlet_xx_flowrate.txt
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // Generate a file name for outlet face ii as Outlet_xx_flowrate.txt
+    // ------------------------------------------------------------------------
     virtual std::string gen_flowfile_name(const int &ii) const
     {
       std::ostringstream ss;
@@ -124,88 +121,88 @@ class ALocal_EBC
       return ss.str();
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // The following functions are in the derived _outflow classes
     // for the Dirichlet-to-Neumann type outflow boundary conditions.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_num_face_nodes : return the number of nodes on the surface
     //                      if this partition owns any cell on this
     //                      surface; otherwise return 0.
     //                      ii : face id ranging from 0 <= ii < num_ebc.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual int get_num_face_nodes(const int &ii) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_num_face_nodes is not implemented. \n");
       return -1;
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_intNA : returns the integral of N_A basis of the face,
     //             if this partition owns any cell on this surface.
     //             ii : face_id ranging 0 <= ii < num_ebc.
     //             out : length is get_num_face_nodes(ii).
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_intNA(const int &ii, std::vector<double> &out) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_intNA is not implemented. \n");
     }
     
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_LID : returns the LID for the nodes associated with intNA,
     //           if this partition owns any cell on this surface.
     //           ii : face_id ranging from 0 <= ii < num_ebc,
     //           out : length is 3 x get_num_face_nodes(ii),
     //           and is the LID of node 0 x, y, z, node1 x, y, z, ...
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_LID(const int &ii, std::vector<int> &out ) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_LID is not implemented. \n");
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_outvec : return the outward normal vector for face ii, if
     //              this partition owns any cell on this surface.
     //              ii : face_id ranging from 0 <= ii < num_ebc,
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_outvec( const int &ii, double &nx, double &ny, double &nz ) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_outvec is not implemented. \n");
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_thickness : return the wall thickness, if this partition
     //                 owns any cell on the wall.
     // Users are responsible for allocating & deleting the e_thickness array.
     // Only one surface per the assumption in wall ebc.
     // surface element id: 0 <= eindex < num_local_cell[0]
     // e_thickness : output thickness array, length is cell_nLocBas[0].
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_thickness( const int &eindex, double * const &e_thickness ) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_thickness is not implemented. \n");
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_youngsmod : return the wall young's modulus, if this partition
     //                 owns any cell on the wall.
     // Users are responsible for allocating & deleting the e_youngsmod array.
     // Only one surface per the assumption in wall ebc.
     // surface element id: 0 <= eindex < num_local_cell[0]
     // e_youngsmod : output youngsmod array, length is cell_nLocBas[0].
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_youngsmod( const int &eindex, double * const &e_youngsmod ) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_youngsmod is not implemented. \n");
     }
 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_prestress, set_prestress : return / set the wall prestress, if this partition
     //                                owns any cell on the wall.
     // Users are responsible for allocating & deleting the e_quaprestress array.
     // Only one surface per the assumption in wall ebc.
     // surface element id: 0 <= eindex < num_local_cell[0]
     // e_quaprestress : output/input prestress array, length is 6 x face_nqp.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual void get_prestress( const int &eindex, double * const &e_quaprestress ) const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_prestress is not implemented. \n");
@@ -223,10 +220,9 @@ class ALocal_EBC
       SYS_T::print_fatal("Error: ALocal_EBC::write_prestress_hdf5 is not implemented. \n");
     }
 
-
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // get_fluid_density : return the fluid density used to compute young's modulus 
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     virtual double get_fluid_density() const
     {
       SYS_T::print_fatal("Error: ALocal_EBC::get_fluid_density is not implemented. \n");
