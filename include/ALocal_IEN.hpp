1b0b4e899d287951301ed4e66b7daa9655594c4c
Ju Liu <liujuy@gmail.com>
2021-05-24 09:21:13 +0800

    add more comments

--- include/ALocal_IEN.hpp
+++ include/ALocal_IEN.hpp
@@ -1,47 +1,52 @@
 #ifndef ALOCAL_IEN_HPP
 #define ALOCAL_IEN_HPP
-// ==================================================================
+// ============================================================================
 // ALocal_IEN.hpp
-// Local IEN for Analysis code.
-// This class stores the IEN double array for all elements. The IEN
-// array is stored in a one-dimensional vector, with stride length
-// nLocBas.
+// Local IEN array for Analysis code.
+// This class stores the IEN array for volumetric elements. The IEN array is 
+// stored in a one-dimensional vector, with stride length nLocBas. For the ee-th
+// element, its ii-th node has volumetric mesh node index LIEN[ ee * nLocBas +
+// ii ].
 //
 // Author: Ju Liu
 // Date: Nov. 10th 2013
-// ==================================================================
+// ============================================================================
 #include "HDF5_Reader.hpp"
 
 class ALocal_IEN
 {
   public:
-    // --------------------------------------------------------------
-    // Constructor : read data by specifying the part file base name 
-    //               and cpu rank
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // ! Constructor : read data by specifying the part file base name and cpu 
+    //   rank
+    // ------------------------------------------------------------------------
     ALocal_IEN( const std::string &fileBaseName, const int &cpu_rank );
 
-    // Destructor
+    // ------------------------------------------------------------------------
+    // ! Destructor
+    // ------------------------------------------------------------------------
     virtual ~ALocal_IEN();
 
-    // --------------------------------------------------------------
-    // Get stride length
-    // This returns the stride length of the IEN array. For problems with
-    // non-isoparametric elements, this result is different from the nLocBas
-    // stored in AGlobal_Mesh_Info. The nLocBas stored in AGlobal_Mesh_Info
-    // is related to the original data in the preprocessor, i.e. the geometry.
-    // The one stored in this class is the number of basis functions for the 
-    // physics interpolation.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // ! Get stride length
+    //   This returns the stride length of the IEN array. For problems with
+    //   non-isoparametric elements, this result is different from the nLocBas
+    //   stored in AGlobal_Mesh_Info. The nLocBas stored in AGlobal_Mesh_Info
+    //   is related to the original data in the preprocessor, i.e. the geometry.
+    //   The one stored in this class is the number of basis functions for the 
+    //   physics interpolation.
+    // ------------------------------------------------------------------------
     virtual int get_stride() const {return nLocBas;}
 
-    // --------------------------------------------------------------
-    // Get the number of local elements. This value should be compatible
-    // with the one stored in the ALocal_Elem class.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // ! Get the number of local elements. This value should be compatible
+    //   with the one stored in the ALocal_Elem class.
+    // ------------------------------------------------------------------------
     virtual int get_nlocalele() const {return nlocalele;}
 
-    // Data access functions
+    // ------------------------------------------------------------------------
+    // ! Data access functions
+    // ------------------------------------------------------------------------
     virtual int get_LIEN(const int &elem, const int &node) const
     { return LIEN[elem*nLocBas + node]; }
     
@@ -54,11 +59,11 @@ class ALocal_IEN
       VEC_T::shrink2fit(elem_ien);
     }
 
-    // --------------------------------------------------------------
-    // get the element e's ien array in an int array: elem_ien.
-    // Users are responsible for allocating and deallocating memory
-    // for elem_ien.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // ! get the element e's ien array in an int array: elem_ien.
+    //   Users are responsible for allocating and deallocating memory
+    //   for elem_ien.
+    // ------------------------------------------------------------------------
     virtual void get_LIEN_e(const int &elem, int * const &elem_ien) const
     {
       const int offset = elem * nLocBas;
@@ -66,13 +71,13 @@ class ALocal_IEN
         elem_ien[ii] = LIEN[offset + ii];
     }
 
-    // --------------------------------------------------------------
-    // isNode_in_Elem returns a bool value that tells if the node ii
-    // is in the element ee.
-    // Input
-    // ee : the local processor's index for element
-    // ii : the local processor's index for node
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // ! isNode_in_Elem returns a bool value that tells if the node with volume
+    //   mesh node index ii is in the element ee or not.
+    //   Input
+    //   ee : the local processor's index for element
+    //   ii : the local processor's index for node
+    // ------------------------------------------------------------------------
     virtual bool isNode_in_Elem(const int &elem, const int &node) const
     {
       std::vector<int> eIEN;
@@ -81,26 +86,29 @@ class ALocal_IEN
       return (it != eIEN.end());
     }
 
-    // Print the info of this class.
+    // ------------------------------------------------------------------------
+    // ! Print the info of this class.
+    // ------------------------------------------------------------------------
     virtual void print_info() const;
   
   protected:
-    // --------------------------------------------------------------
-    // The number of local element. This integer data is assumed to be 
-    // also stored in ALocal_Elem.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // The number of local element. 
+    // This integer data is assumed to be also stored in the ALocal_Elem class.
+    // ------------------------------------------------------------------------
     int nlocalele; 
     
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // The number of local/elemental basis functions.
     // The value can be changed due to the FEM basis function enrichment
     // Users should check if one uses a derived class.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     int nLocBas; 
     
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     // The LIEN array may be enriched due to use of non-isoparametric elements.
     // Users should check if one uses a derived class.
-    // --------------------------------------------------------------
+    // ------------------------------------------------------------------------
     std::vector<int> LIEN; 
 };
 
