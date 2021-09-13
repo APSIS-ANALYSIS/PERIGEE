9da21e768bf88d327f5688fb078236cbef298109
rrrabittt <12132408@mail.sustech.edu.cn>
2021-09-13 16:39:16 +0800

    PS-ALE : update material model NeoHookean NeoHookean RWR2000 function

--- include/MaterialModel_NeoHookean_RWR2000.hpp
+++ include/MaterialModel_NeoHookean_RWR2000.hpp
@@ -23,6 +23,14 @@ class MaterialModel_NeoHookean_RWR2000 : public IMaterialModel
     virtual ~MaterialModel_NeoHookean_RWR2000();
 
     virtual void print_info() const;
+
+    virtual std::string get_model_name() const
+    {
+      const std::string mname = "NeoHookean-RWR2000";
+      return mname;
+    }
+
+    virtual void write_hdf5( const char * const &fname = "material_model.h5") const;
     
     virtual void get_PK( const Matrix_3x3 &F, Matrix_3x3 &P, Matrix_3x3 &S );
 
