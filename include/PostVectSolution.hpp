d454605fdc215152bfbd7fc85260c0b952621b66
Ju Liu <liujuy@gmail.com>
2021-09-27 17:31:08 +0800

    SYS-DEV : role back to one previous version

--- include/PostVectSolution.hpp
+++ include/PostVectSolution.hpp
@@ -1,108 +1,108 @@
 #ifndef POSTVECTSOLUTION_HPP
 #define POSTVECTSOLUTION_HPP
-// ==================================================================
+// ============================================================================
 // PostVectSolution.hpp
-// ------------------------------------------------------------------
-// This is a solution class that is used for postprocessing, including
-// calculating solution errors, generating vtk visualization files, 
-// etc. Notice, since the solution vector generated from fem analysis
-// code is usually in PETSc binary format and reordered based on the
-// node partition, this class has to read in the binary solutino and
-// reorder the solution indices in order to make it compatible with 
-// the partition of the postprocessing. Hence, there are three sets
-// of numbering involved: (a) the natural golbal numbering; (b) the
-// analysis code's numbering; (b) the postprocessing code's numbering.
+// ----------------------------------------------------------------------------
+// This is a solution class that is used for postprocessing, including calculating 
+// solution errors, generating vtk visualization files, etc. Notice, since the 
+// solution vector generated from fem analysis code is usually in PETSc binary 
+// format and reordered based on the node partition, this class has to read the 
+// binary solutino and reorder the solution indices in order to make it compatible 
+// with the partition of the postprocessing. Hence, there are three sets of numbering 
+// involved: (a) the natural golbal numbering; (b) the analysis code's numbering; 
+// (c) the postprocessing code's numbering.
 //
-// To do these, this code should
+// To do these, this code
 // 1. read the PETSc vector into memory as a whole;
-// 2. rearrange the numbering of the vector based on postproce code's
-//    node partition;
+// 2. rearrange the numbering of the vector based on postproce code's node partition;
 // 3. extract local vector for each processor in postprocessing.
 //
+// Author: Ju Liu
 // Date: Dec 10 2013
-// ==================================================================
+// ============================================================================
 #include "APart_Node.hpp"
-#include "IAGlobal_Mesh_Info.hpp"
 #include "petscvec.h"
-#include "HDF5_Reader.hpp"
 
 class PostVectSolution
 {
   public:
-    // ! Constructor:
-    // \para solution_file_name: the name of PETSc binary file that
-    //                           records the solution vector;
-    // \para analysis_node_mapping_file: the old_2_new and new_2_old 
-    //                           mapping from analysis run;
-    // \para aNode_ptr: the partition of node information from
-    //                  post_part files;
-    // \para input_dof: the degree of freedom of the input solution vector 
+    // ------------------------------------------------------------------------
+    // Constructor:
+    //   \para solution_file_name: the name of PETSc binary file that
+    //                             records the solution vector;
+    //   \para analysis_node_mapping_file: the old_2_new and new_2_old 
+    //                             mapping from analysis run;
+    //   \para aNode_ptr: the partition of node information from
+    //                    post_part files;
+    //   \para input_dof: the degree of freedom of the input solution vector 
     // Output: Construct the vector that stores the solution vector
     //         which belongs to the local partition, i.e., the post
     //         processes' local_to_global indices. 
-    PostVectSolution( const std::string &solution_file_name,
-       const std::string &analysis_node_mapping_file,
-       const std::string &post_node_mapping_file,
-       const APart_Node * const &aNode_ptr,
-       const IAGlobal_Mesh_Info * const &gInfo_ptr,
-       const int &input_dof );
-
-    
-    // ! Constructor.
-    // \para in_nfunc : the number of total number of basis
-    //                  functions for this solution vector.
+    // ------------------------------------------------------------------------
     PostVectSolution( const std::string &solution_file_name,
        const std::string &analysis_node_mapping_file,
        const std::string &post_node_mapping_file,
        const APart_Node * const &aNode_ptr,
        const int &in_nfunc, const int &input_dof );
 
-
-    // ! Destructor
+    // ------------------------------------------------------------------------
+    // Destructor
+    // ------------------------------------------------------------------------
     ~PostVectSolution();
 
-    // ! Print the loc_solution vector
+    // ------------------------------------------------------------------------
+    // Print the loc_solution vector
+    // ------------------------------------------------------------------------
     void print_info() const;
 
-    // ! PlusAX: the loc_solution is updated by adding val * input_sol
-    void PlusAX(const PostVectSolution * const &input_sol, const double &val);
-
-    // ! get_xxx functions that give read permission to private data
+    // ------------------------------------------------------------------------
+    // get functions that give read permission to private data
+    // ------------------------------------------------------------------------
     double get_locsol(const int &pos) const {return loc_solution[pos];}
+    
     int get_dof() const {return dof_per_node;}
+    
     int get_solsize() const {return loc_sol_size;}
 
-    // ! get local element's solution
-    // \para field: the dof field that we need, field < dof_per_node
-    // \para nLocBas: the number of solution that needs to be passed out
-    // \para eien: the element ien array
-    // \para esol: the double array that passes the solution vector,
-    // users are responsible for allocating and deleting eien & esol
+    // ------------------------------------------------------------------------
+    // get local element's solution
+    //   \para field: the dof field that we need, field < dof_per_node
+    //   \para nLocBas: the number of solution that needs to be passed out
+    //   \para eien: the element ien array
+    //   \para esol: the double array that passes the solution vector
+    // Users are responsible for allocating and deleting eien & esol
+    // ------------------------------------------------------------------------
     void get_esol( const int &field, const int &nLocBas, 
         const int * const &eien, double * const &esol) const;
 
   private:
     const int dof_per_node; // dof of the solution vector
     const int loc_sol_size; // nlocghonode x dof
+    
+    // loc_solution is the data structure that holds the solution vector of the
+    // local postprocessing partition.
     double * loc_solution;
     
-    // ! ReadPETSc_vec: read a PETSc vector into memory as a double array
-    // \para vec_size: the total length of the PETSc vector. This is used
-    //                 to check the correcness of the reading.
-    // \para veccopy: the double array that is used to allocate the solution
-    //                users are responsible for allocating and deleting this
-    //                dynamic array. This array should have length vec_size.
+    // ------------------------------------------------------------------------
+    // ReadPETSc_vec: read a PETSc vector into memory as a double array
+    //   \para vec_size: the total length of the PETSc vector. This is used
+    //                   to check the correcness of the reading.
+    //   \para veccopy: the double array that is used to allocate the solution
+    //                  users are responsible for allocating and deleting this
+    //                  dynamic array. This array should have length vec_size.
+    // ------------------------------------------------------------------------
     void ReadPETSc_vec( const std::string &solution_file_name,
         const int &vec_size, double * const &veccopy );
 
-    // ! ReadNodeMapping: reads the old_2_new or new_2_old array into 
-    //                    the nodemap array. The users are responsible for
-    //                    allocating nFunc length for nodemap and deleting
-    //                    it after use.
+    // ------------------------------------------------------------------------
+    // ReadNodeMapping: reads the old_2_new or new_2_old array into the nodemap 
+    //                  array. The users are responsible for allocating nFunc 
+    //                  length for nodemap and deleting it after use.
     // \para node_mapping_file: the file that stores the mapping arrays
     // \para mapping_type: data_name in the file: new_2_old / old_2_new
     // \para node_size: the allocated length for nodemap
     // \para nodemap: the array that passes out the mapping.
+    // ------------------------------------------------------------------------
     void ReadNodeMapping( const std::string &node_mapping_file,
         const char * const &mapping_type, const int &node_size,
         int * const &nodemap ) const;
--- src/Postproc_Tool/PostVectSolution.cpp
+++ src/Postproc_Tool/PostVectSolution.cpp
@@ -4,91 +4,44 @@ PostVectSolution::PostVectSolution( const std::string &solution_file_name,
     const std::string &analysis_node_mapping_file,
     const std::string &post_node_mapping_file,
     const APart_Node * const &aNode_ptr,
-    const IAGlobal_Mesh_Info * const &gInfo_ptr,
-    const int &input_dof )
+    const int &nFunc, const int &input_dof )
 : dof_per_node( input_dof ), 
   loc_sol_size( aNode_ptr->get_nlocghonode() * dof_per_node )
 {
+  // Allocate the space for the data loc_solution
   loc_solution = new double [loc_sol_size];
 
-  int nFunc = gInfo_ptr->get_nFunc();
-  int whole_vec_size = nFunc * dof_per_node;
-  double * vec_temp = new double [whole_vec_size];
+  double * vec_temp = new double [ nFunc * dof_per_node ];
 
   int * analysis_old2new = new int [nFunc];
   int * postproc_new2old = new int [nFunc];
 
-  // Read PETSc solution vector into vec_temp
-  ReadPETSc_vec(solution_file_name, whole_vec_size, vec_temp);
+  // Read the full PETSc solution vector into vec_temp
+  ReadPETSc_vec(solution_file_name, nFunc * dof_per_node, vec_temp);
 
-  // Read new2old and old2new mappings
+  // Read new2old and old2new mappings from HDF5 files
   ReadNodeMapping(analysis_node_mapping_file, "old_2_new", nFunc, analysis_old2new );
   ReadNodeMapping(post_node_mapping_file, "new_2_old", nFunc, postproc_new2old );
 
-  int index;
   for( int ii=0; ii<aNode_ptr->get_nlocghonode(); ++ii )
   {
-    index = aNode_ptr->get_local_to_global(ii); // in postprocess partition's new index
-    index = postproc_new2old[index]; // map back to natural global index
-    index = analysis_old2new[index]; // map forward to analysis partitioned new index
+    int index = aNode_ptr->get_local_to_global(ii); // in postprocess partition's new index
+    index = postproc_new2old[index];                // map back to natural global index
+    index = analysis_old2new[index];                // map forward to analysis partitioned new index
 
     for(int jj=0; jj<dof_per_node; ++jj)
       loc_solution[ii*dof_per_node + jj] = vec_temp[index*dof_per_node + jj];
   }
 
-  delete [] analysis_old2new;
-  delete [] postproc_new2old;
-  delete [] vec_temp;
+  delete [] analysis_old2new; analysis_old2new = nullptr;
+  delete [] postproc_new2old; postproc_new2old = nullptr;
+  delete [] vec_temp;         vec_temp         = nullptr;
 }
-
-
-PostVectSolution::PostVectSolution( const std::string &solution_file_name,
-    const std::string &analysis_node_mapping_file,
-    const std::string &post_node_mapping_file,
-    const APart_Node * const &aNode_ptr,
-    const int &in_nfunc, const int &input_dof )
-: dof_per_node( input_dof ), 
-  loc_sol_size( aNode_ptr->get_nlocghonode() * dof_per_node )
-{
-  loc_solution = new double [loc_sol_size];
-
-  int nFunc = in_nfunc;
-  int whole_vec_size = nFunc * dof_per_node;
-  double * vec_temp = new double [whole_vec_size];
-
-  int * analysis_old2new = new int [nFunc];
-  int * postproc_new2old = new int [nFunc];
-
-  // Read PETSc solution vector into vec_temp
-  ReadPETSc_vec(solution_file_name, whole_vec_size, vec_temp);
-
-  // Read new2old and old2new mappings
-  ReadNodeMapping(analysis_node_mapping_file, "old_2_new", nFunc, analysis_old2new );
-  ReadNodeMapping(post_node_mapping_file, "new_2_old", nFunc, postproc_new2old );
-
-  int index;
-  for( int ii=0; ii<aNode_ptr->get_nlocghonode(); ++ii )
-  {
-    index = aNode_ptr->get_local_to_global(ii); // in postprocess partition's new index
-    index = postproc_new2old[index]; // map back to natural global index
-    index = analysis_old2new[index]; // map forward to analysis partitioned new index
-
-    for(int jj=0; jj<dof_per_node; ++jj)
-      loc_solution[ii*dof_per_node + jj] = vec_temp[index*dof_per_node + jj];
-  }
-
-  delete [] analysis_old2new;
-  delete [] postproc_new2old;
-  delete [] vec_temp;
-}
-
-
 
 PostVectSolution::~PostVectSolution()
 {
-  delete [] loc_solution;
+  delete [] loc_solution; loc_solution = nullptr;
 }
-
 
 void PostVectSolution::print_info() const
 {
@@ -99,43 +52,15 @@ void PostVectSolution::print_info() const
   std::cout<<std::endl;
 }
 
-void PostVectSolution::PlusAX(const PostVectSolution * const &input_sol, 
-    const double &val)
-{
-  if(input_sol->get_dof() != dof_per_node)
-  {
-    PetscPrintf(PETSC_COMM_SELF, "Error: dof of the two PostVectSolution does not match. \n");
-    MPI_Abort(PETSC_COMM_WORLD, 1);
-  }
-
-  if(input_sol->get_solsize() != loc_sol_size)
-  {
-    PetscPrintf(PETSC_COMM_SELF, "Error: size of the two PostVectSolution does not match. \n");
-    MPI_Abort(PETSC_COMM_WORLD, 1);
-  }
-
-  for(int ii=0; ii<loc_sol_size; ++ii)
-    loc_solution[ii] = loc_solution[ii] + val * input_sol->get_locsol(ii);
-}
-
-
 void PostVectSolution::get_esol(const int &field, const int &nLocBas,
     const int * const &eien, double * const &esol) const
 {
   // check the input field index
-  if(field >= dof_per_node)
-  {
-    PetscPrintf(PETSC_COMM_WORLD, "Error: field is out of range. \n");
-    MPI_Abort(PETSC_COMM_WORLD, 1);
-  }
-  int index;
-  for(int ii=0; ii<nLocBas; ++ii)
-  {
-    index = eien[ii];
-    esol[ii] = loc_solution[dof_per_node * index + field];
-  }
-}
+  SYS_T::print_fatal_if( field >= dof_per_node, "Error: field is out of range. \n" );
 
+  for(int ii=0; ii<nLocBas; ++ii)
+    esol[ii] = loc_solution[dof_per_node * eien[ii] + field];
+}
 
 void PostVectSolution::ReadPETSc_vec( const std::string &solution_file_name,
     const int &vec_size, double * const &veccopy )
@@ -145,8 +70,7 @@ void PostVectSolution::ReadPETSc_vec( const std::string &solution_file_name,
   VecSetType(sol_temp, VECSEQ);
 
   PetscViewer viewer;
-  PetscViewerBinaryOpen(PETSC_COMM_SELF, solution_file_name.c_str(),
-      FILE_MODE_READ, &viewer);
+  PetscViewerBinaryOpen(PETSC_COMM_SELF, solution_file_name.c_str(), FILE_MODE_READ, &viewer);
   VecLoad(sol_temp, viewer);
   PetscViewerDestroy(&viewer);
 
@@ -172,26 +96,26 @@ void PostVectSolution::ReadPETSc_vec( const std::string &solution_file_name,
   VecDestroy(&sol_temp);
 }
 
-
 void PostVectSolution::ReadNodeMapping( const std::string &node_mapping_file,
     const char * const &mapping_type, const int &node_size,
     int * const &nodemap ) const
 {
   hid_t file_id = H5Fopen(node_mapping_file.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-  hid_t data_rank;
-  hsize_t * data_dims;
-  int * p = nodemap;
+  hid_t data_id = H5Dopen(file_id, mapping_type, H5P_DEFAULT); 
 
-  HDF5_Reader * h5r = new HDF5_Reader( file_id );
-
-  h5r -> read_intArray( node_mapping_file.c_str(), mapping_type, data_rank, data_dims, p );
+  hid_t data_space = H5Dget_space( data_id );
+  hid_t data_rank = H5Sget_simple_extent_ndims( data_space );
 
   if( data_rank != 1)
   {
     PetscPrintf(PETSC_COMM_SELF, "Error: the node mapping file has wrong format. \n");
     MPI_Abort(PETSC_COMM_WORLD, 1);
   }
-  
+
+  hsize_t * data_dims = new hsize_t [1];
+
+  H5Sget_simple_extent_dims( data_space, data_dims, NULL );
+
   hid_t mem_space = H5Screate_simple(data_rank, data_dims, NULL);
 
   hsize_t dSize = data_dims[0]; 
@@ -202,7 +126,14 @@ void PostVectSolution::ReadNodeMapping( const std::string &node_mapping_file,
     MPI_Abort(PETSC_COMM_WORLD, 1);
   }
 
+  H5Dread( data_id, H5T_NATIVE_INT, mem_space, data_space,
+      H5P_DEFAULT, nodemap );
+
+  delete [] data_dims;
   H5Sclose( mem_space );
+  H5Sclose(data_space);
+  H5Dclose(data_id);
+  H5Fclose(file_id);
 }
 
 // EOF
