4a8a82bbf1ab0f778953aaa31120ea1196567173
Ingrid Lan <ingridl@stanford.edu>
2021-10-05 15:39:05 -0700

    NBC_Partition_3D_inflow: compatibility with NodalBC_3D_inflow

--- include/NBC_Partition_3D_inflow.hpp
+++ include/NBC_Partition_3D_inflow.hpp
@@ -30,37 +30,44 @@ class NBC_Partition_3D_inflow : public NBC_Partition_3D
     virtual void write_hdf5( const char * FileName ) const;
 
   private:
-    double actarea, facearea;
+    // active area & full area. Length num_nbc
+    std::vector<double> actarea, facearea;
 
-    // unit outward normal vector on this surface
-    Vector_3 outvec;
+    // unit outward normal vector on this surface. Length num_nbc
+    std::vector<Vector_3> outvec;
 
-    // number of boundary points of this surface
-    int num_out_bc_pts;
+    // number of boundary points of this surface. Length num_nbc
+    std::vector<int> num_out_bc_pts;
 
-    // spatial coordinates of the centroid
-    Vector_3 centroid;
+    // spatial coordinates of the centroid. Length num_nbc
+    std::vector<Vector_3> centroid;
 
-    // coordindates of the boundary points
-    std::vector<double> outline_pts;
+    // coordinates of the boundary points
+    // num_nbc x num_out_bc_pts[ii]
+    std::vector< std::vector<double> > outline_pts;
 
-    // number of local node / cell, element number of nodes
-    int num_local_node, num_local_cell, cell_nLocBas;
+    // number of local node / cell, element number of nodes. Length num_nbc
+    std::vector<int> num_local_node, num_local_cell, cell_nLocBas;
 
-    // local nodes' coordinates, length 3 x num_local_node
-    std::vector<double> local_pt_xyz;
+    // local nodes' coordinates
+    // num_nbc times (3 x num_local_node[ii])
+    std::vector< std::vector<double> > local_pt_xyz;
 
-    // local cell IEN array, size cell_nLocBas x num_local_cell
-    std::vector<int> local_tri_ien;
+    // local cell IEN array
+    // num_nbc times (size cell_nLocBas[ii] x num_local_cell[ii])
+    std::vector< std::vector<int> > local_tri_ien;
 
-    // local node's global index, length num_local_node
-    std::vector<int> local_global_node;
+    // local node's global index
+    // num_nbc x num_local_node[ii]
+    std::vector< std::vector<int> > local_global_node;
 
-    // local node's postion in the local_to_global_array
-    std::vector<int> local_node_pos;
+    // local node's position in the local_to_global_array
+    // num_nbc x num_local_node[ii]
+    std::vector< std::vector<int> > local_node_pos;
 
-    // local cell's global index, length num_local_cell
-    std::vector<int> local_global_cell;
+    // local cell's global index
+    // num_nbc x num_local_cell[ii]
+    std::vector< std::vector<int> > local_global_cell;
 };
 
 #endif
--- src/Mesh/NBC_Partition_3D_inflow.cpp
+++ src/Mesh/NBC_Partition_3D_inflow.cpp
@@ -6,78 +6,89 @@ NBC_Partition_3D_inflow::NBC_Partition_3D_inflow(
     const INodalBC * const &nbc ) 
 : NBC_Partition_3D( part, mnindex, nbc )
 {
-  // Area of the cap surface
-  actarea  = nbc -> get_para_1();
-  facearea = nbc -> get_para_6();
+  actarea.resize(num_nbc); facearea.resize(num_nbc);
+  outvec.resize(num_nbc);  centroid.resize(num_nbc);
+  num_out_bc_pts.resize(num_nbc); outline_pts.resize(num_nbc);
+  cell_nLocBas.resize(num_nbc);      local_tri_ien.resize(num_nbc);
+  num_local_node.resize(num_nbc);    num_local_cell.resize(num_nbc);
+  local_global_node.resize(num_nbc); local_global_cell.resize(num_nbc);
+  local_node_pos.resize(num_nbc);    local_pt_xyz.resize(num_nbc);
 
-  // Outward normal vector
-  outvec = nbc->get_para_2();
-
-  // Centroid and outline points
-  num_out_bc_pts = nbc->get_para_3();
-
-  centroid = nbc->get_para_4();
-
-  outline_pts.resize( 3*num_out_bc_pts );
-  for(int ii=0; ii<3*num_out_bc_pts; ++ii)
-    outline_pts[ii] = nbc->get_para_5(ii);
-
-  // Record the geometrical info of the inlet in this CPU
-  cell_nLocBas = nbc -> get_nLocBas();
-
-  std::vector<int> local_node, local_elem;
-  local_node.clear(); local_elem.clear();
-
-  local_global_cell.clear();
-
-  // Loop over all cells on inlet cap surface
-  for(int jj=0; jj < nbc -> get_num_cell(); ++jj)
+  for(int ii=0; ii<num_nbc; ++ii)
   {
-    const int elem_index = nbc -> get_global_cell(jj); // cell vol id
+    // Area of the cap surface
+    actarea[ii]  = nbc -> get_para_1(ii);
+    facearea[ii] = nbc -> get_para_6(ii);
 
-    if( part -> get_elemLocIndex( elem_index ) != -1  )
+    // Outward normal vector
+    outvec[ii] = nbc->get_para_2(ii);
+
+    // Centroid and outline points
+    num_out_bc_pts[ii] = nbc->get_para_3(ii);
+
+    centroid[ii] = nbc->get_para_4(ii);
+
+    outline_pts[ii].resize( 3*num_out_bc_pts[ii] );
+    for(int jj=0; jj<3*num_out_bc_pts[ii]; ++jj)
+      outline_pts[ii][jj] = nbc->get_para_5(ii, jj);
+
+    // Record the geometrical info of the inlet in this CPU
+    cell_nLocBas[ii] = nbc -> get_nLocBas(ii);
+
+    std::vector<int> local_node, local_elem;
+    local_node.clear(); local_elem.clear();
+
+    local_global_cell[ii].clear();
+
+    // Loop over all cells on inlet cap surface
+    for(int jj=0; jj < nbc->get_num_cell(ii); ++jj)
     {
-      local_elem.push_back( jj );
+      const int elem_index = nbc -> get_global_cell(ii, jj); // cell vol id
 
-      local_global_cell.push_back( elem_index );
+      if( part -> get_elemLocIndex( elem_index ) != -1  )
+      {
+        local_elem.push_back( jj );
 
-      for( int kk=0; kk < cell_nLocBas; ++kk )
-        local_node.push_back( nbc->get_ien(jj, kk) );
+        local_global_cell[ii].push_back( elem_index );
+
+        for( int kk=0; kk<cell_nLocBas[ii]; ++kk )
+          local_node.push_back( nbc->get_ien(ii, jj, kk) );
+      }
     }
-  }
 
-  VEC_T::sort_unique_resize( local_node );
+    VEC_T::sort_unique_resize( local_node );
 
-  num_local_node = static_cast<int>( local_node.size() );
-  num_local_cell = static_cast<int>( local_global_cell.size() );
+    num_local_node[ii] = static_cast<int>( local_node.size() );
+    num_local_cell[ii] = static_cast<int>( local_global_cell[ii].size() );
 
-  local_pt_xyz.resize( num_local_node * 3 );
-  local_global_node.resize( num_local_node );
-  local_node_pos.resize( num_local_node );
+    local_pt_xyz[ii].resize(      num_local_node[ii] * 3 );
+    local_global_node[ii].resize( num_local_node[ii] );
+    local_node_pos[ii].resize(    num_local_node[ii] );
 
-  for(int jj=0; jj<num_local_node; ++jj)
-  {
-    local_pt_xyz[3*jj+0] = nbc -> get_pt_xyz( local_node[jj], 0 );
-    local_pt_xyz[3*jj+1] = nbc -> get_pt_xyz( local_node[jj], 1 );
-    local_pt_xyz[3*jj+2] = nbc -> get_pt_xyz( local_node[jj], 2 );
-
-    local_global_node[jj] = nbc -> get_global_node( local_node[jj] );
-
-    local_node_pos[jj] = part->get_nodeLocGhoIndex( mnindex->get_old2new(local_global_node[jj]) );
-  }
-
-  // create new IEN
-  local_tri_ien.resize( num_local_cell * cell_nLocBas );
-
-  for(int jj=0; jj<num_local_cell; ++jj)
-  {
-    for(int kk=0; kk<cell_nLocBas; ++kk)
+    for(int jj=0; jj<num_local_node[ii]; ++jj)
     {
-      const int temp_node = nbc -> get_ien( local_elem[jj], kk );
-      const int temp_npos = VEC_T::get_pos( local_node, temp_node );
-      local_tri_ien[jj*cell_nLocBas + kk] = temp_npos;
+      local_pt_xyz[ii][3*jj+0] = nbc -> get_pt_xyz( ii, local_node[jj], 0 );
+      local_pt_xyz[ii][3*jj+1] = nbc -> get_pt_xyz( ii, local_node[jj], 1 );
+      local_pt_xyz[ii][3*jj+2] = nbc -> get_pt_xyz( ii, local_node[jj], 2 );
+
+      local_global_node[ii][jj] = nbc -> get_global_node( ii, local_node[jj] );
+
+      local_node_pos[ii][jj] = part->get_nodeLocGhoIndex( mnindex->get_old2new(local_global_node[ii][jj]) );
     }
-  }
+
+    // create new IEN
+    local_tri_ien[ii].resize( num_local_cell[ii] * cell_nLocBas[ii] );
+
+    for(int jj=0; jj<num_local_cell[ii]; ++jj)
+    {
+      for(int kk=0; kk<cell_nLocBas[ii]; ++kk)
+      {
+        const int temp_node = nbc -> get_ien( ii, local_elem[jj], kk );
+        const int temp_npos = VEC_T::get_pos( local_node, temp_node );
+        local_tri_ien[ii][jj*cell_nLocBas[ii] + kk] = temp_npos;
+      }
+    }
+  } // end ii-loop over num_nbc
 }
 
 NBC_Partition_3D_inflow::~NBC_Partition_3D_inflow()
@@ -90,44 +101,59 @@ void NBC_Partition_3D_inflow::write_hdf5( const char * FileName ) const
   
   hid_t file_id = H5Fopen(fName.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
 
-  hid_t group_id = H5Gcreate(file_id, "/inflow", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  hid_t g_id = H5Gcreate(file_id, "/inflow", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-  HDF5_Writer * h5writer = new HDF5_Writer(file_id);
+  HDF5_Writer * h5w = new HDF5_Writer(file_id);
 
-  if(LDN.size() > 0)
-    h5writer->write_intVector(group_id, "LDN", LDN);
+  h5w -> write_intScalar( g_id, "num_nbc", num_nbc );
 
-  h5writer->write_intVector(group_id, "Num_LD", Num_LD);
-
-  h5writer->write_Vector_3( group_id, "Outward_normal_vector", outvec );
-
-  h5writer->write_doubleScalar( group_id, "Inflow_active_area", actarea );
+  h5w->write_doubleVector( g_id, "Inflow_active_area", actarea );
   
-  h5writer->write_doubleScalar( group_id, "Inflow_full_area", facearea );
+  h5w->write_doubleVector( g_id, "Inflow_full_area", facearea );
 
-  h5writer->write_intScalar( group_id, "num_out_bc_pts", num_out_bc_pts);
+  h5w->write_intVector( g_id, "num_out_bc_pts", num_out_bc_pts);
 
-  h5writer->write_Vector_3( group_id, "centroid", centroid);
-
-  h5writer->write_doubleVector( group_id, "outline_pts", outline_pts);
-
-  h5writer->write_intScalar( group_id, "num_local_node", num_local_node );
+  h5w->write_intVector( g_id, "num_local_node", num_local_node );
   
-  h5writer->write_intScalar( group_id, "num_local_cell", num_local_cell );
+  h5w->write_intVector( g_id, "num_local_cell", num_local_cell );
   
-  h5writer->write_intScalar( group_id, "cell_nLocBas", cell_nLocBas );
+  h5w->write_intVector( g_id, "cell_nLocBas", cell_nLocBas );
 
-  h5writer->write_doubleVector( group_id, "local_pt_xyz", local_pt_xyz );
+  const std::string groupbase("nbcid_");
 
-  h5writer->write_intVector( group_id, "local_tri_ien", local_tri_ien );
+  for(int ii=0; ii<num_nbc; ++ii)
+  {
+    std::string subgroup_name(groupbase);
+    subgroup_name.append( SYS_T::to_string(ii) );
 
-  h5writer->write_intVector( group_id, "local_global_node", local_global_node );
+    hid_t group_id = H5Gcreate(g_id, subgroup_name.c_str(),
+          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
-  h5writer->write_intVector( group_id, "local_node_pos", local_node_pos );
+    if( LDN[ii].size() > 0 )
+      h5w->write_intVector( group_id, "LDN", LDN[ii] );
 
-  h5writer->write_intVector( group_id, "local_global_cell", local_global_cell );
+    h5w->write_intVector( group_id, "Num_LD", Num_LD[ii] );
 
-  delete h5writer; H5Gclose(group_id); H5Fclose(file_id);
+    h5w->write_Vector_3( group_id, "Outward_normal_vector", outvec[ii] );
+
+    h5w->write_Vector_3( group_id, "centroid", centroid[ii] );
+
+    h5w->write_doubleVector( group_id, "outline_pts", outline_pts[ii] );
+
+    h5w->write_doubleVector( group_id, "local_pt_xyz", local_pt_xyz[ii] );
+
+    h5w->write_intVector( group_id, "local_tri_ien", local_tri_ien[ii] );
+
+    h5w->write_intVector( group_id, "local_global_node", local_global_node[ii] );
+
+    h5w->write_intVector( group_id, "local_node_pos", local_node_pos[ii] );
+
+    h5w->write_intVector( group_id, "local_global_cell", local_global_cell[ii] );
+
+    H5Gclose( group_id );
+  }
+
+  delete h5w; H5Gclose( g_id ); H5Fclose( file_id );
 }
 
 // EOF
