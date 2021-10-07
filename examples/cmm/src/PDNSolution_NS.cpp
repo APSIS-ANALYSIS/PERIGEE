9e8303a47547b92d56014263b38fe13b196b6911
Ingrid Lan <ingridl@stanford.edu>
2021-10-07 12:09:52 -0700

    PDNSolution_NS: parabolic velo profile for multiple inlets

--- examples/cmm/src/PDNSolution_NS.cpp
+++ examples/cmm/src/PDNSolution_NS.cpp
@@ -105,57 +105,63 @@ void PDNSolution_NS::Init_flow_parabolic(
     VecSetValues(solution, 4, location, value, INSERT_VALUES);
   }
 
-  // Maximum speed formula is 
-  //             2.0 x flow rate (1.0) / surface area
-  // Here I use the unit flow rate, and the actual flow rate is adjusted
-  // based on the CVFlowRate class.
-  const double vmax = 2.0 / infbc->get_fularea();
+  const int num_nbc = infbc->get_num_nbc();
 
-  const double out_nx = infbc->get_outvec().x();
-  const double out_ny = infbc->get_outvec().y();
-  const double out_nz = infbc->get_outvec().z();
- 
-  // If there are inflow nodes, set their value to be parabolic flow
-  if( infbc->get_Num_LD() > 0)
+  for(int nbc_id=0; nbc_id<num_nbc; ++nbc_id)
   {
-    for(int ii=0; ii<nlocalnode; ++ii)
+    // Maximum speed formula is 
+    //             2.0 x flow rate (1.0) / surface area
+    // Here I use the unit flow rate, and the actual flow rate is adjusted
+    // based on the CVFlowRate class.
+    const double vmax = 2.0 / infbc->get_fularea(nbc_id);
+
+    const double out_nx = infbc->get_outvec(nbc_id).x();
+    const double out_ny = infbc->get_outvec(nbc_id).y();
+    const double out_nz = infbc->get_outvec(nbc_id).z();
+ 
+    // If there are inflow nodes, set their value to be parabolic flow
+    if( infbc->get_Num_LD(nbc_id) > 0)
     {
-      if( infbc->is_inLDN(pNode_ptr->get_node_loc(ii)) )
+      for(int ii=0; ii<nlocalnode; ++ii)
       {
-        location[0] = pNode_ptr->get_node_loc(ii) * 4;
-        location[1] = location[0] + 1;
-        location[2] = location[0] + 2;
-        location[3] = location[0] + 3;
+        if( infbc->is_inLDN(nbc_id, pNode_ptr->get_node_loc(ii)) )
+        {
+          location[0] = pNode_ptr->get_node_loc(ii) * 4;
+          location[1] = location[0] + 1;
+          location[2] = location[0] + 2;
+          location[3] = location[0] + 3;
 
-        const Vector_3 pt = fNode_ptr -> get_ctrlPts_xyz(ii);
-        const double r =  infbc -> get_radius( pt );
+          const Vector_3 pt = fNode_ptr -> get_ctrlPts_xyz(ii);
+          const double r =  infbc -> get_radius( nbc_id, pt );
 
-        const double vel = vmax * (1.0 - r*r);
+          const double vel = vmax * (1.0 - r*r);
 
-        // -1.0 is multiplied to make the flow direction inward
-        value[1] = vel * (-1.0) * out_nx;
-        value[2] = vel * (-1.0) * out_ny;
-        value[3] = vel * (-1.0) * out_nz;
+          // -1.0 is multiplied to make the flow direction inward
+          value[1] = vel * (-1.0) * out_nx;
+          value[2] = vel * (-1.0) * out_ny;
+          value[3] = vel * (-1.0) * out_nz;
 
-        VecSetValues(solution, 4, location, value, INSERT_VALUES);
+          VecSetValues(solution, 4, location, value, INSERT_VALUES);
+        }
       }
     }
-  }
 
-  VecAssemblyBegin(solution); VecAssemblyEnd(solution);
-  GhostUpdate();
+    VecAssemblyBegin(solution); VecAssemblyEnd(solution);
+    GhostUpdate();
 
-  if(is_print)
-  {
-    SYS_T::commPrint("===> Initial solution: pres   = 0.0 \n");
-    SYS_T::commPrint("                       velo_x = parabolic \n");
-    SYS_T::commPrint("                       velo_y = parabolic \n");
-    SYS_T::commPrint("                       velo_z = parabolic \n");
-    SYS_T::commPrint("                       flow rate 1.0 .\n");
-    SYS_T::commPrint("                       max speed %e.\n", vmax);
-    SYS_T::commPrint("                       active area is %e.\n", infbc->get_actarea() );
-    SYS_T::commPrint("                       full area is %e.\n", infbc->get_fularea() );
-    SYS_T::commPrint("                       direction [%e %e %e].\n", out_nx, out_ny, out_nz);
+    if(is_print)
+    {
+      SYS_T::commPrint("     Initial solution: pres   = 0.0 \n");
+      SYS_T::commPrint("                       velo_x = parabolic \n");
+      SYS_T::commPrint("                       velo_y = parabolic \n");
+      SYS_T::commPrint("                       velo_z = parabolic \n");
+      SYS_T::commPrint("                       flow rate 1.0 .\n");
+      SYS_T::commPrint("                       -- nbc_id = %d \n", nbc_id);
+      SYS_T::commPrint("                          max speed %e.\n", vmax);
+      SYS_T::commPrint("                          active area is %e.\n", infbc->get_actarea(nbc_id) );
+      SYS_T::commPrint("                          full area is %e.\n", infbc->get_fularea(nbc_id) );
+      SYS_T::commPrint("                          direction [%e %e %e].\n", out_nx, out_ny, out_nz);
+    }
   }
 }
 
