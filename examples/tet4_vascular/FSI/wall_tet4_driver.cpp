5b1ddbffa187a5b12e2dd205afdc94aafccd7b86
Ju Liu <liujuy@gmail.com>
2021-11-30 17:51:25 +0800

    to work on the prestress algorithm in a new nonlinear solver context

--- examples/tet4_vascular/FSI/wall_tet4_driver.cpp
+++ examples/tet4_vascular/FSI/wall_tet4_driver.cpp
@@ -28,6 +28,9 @@
 
 int main( int argc, char *argv[] )
 {
+  // solution file name to be loaded for prestressing
+  std::string restart_name = "SOL_re";
+
   // (Pseudo-) time integration parameters
   double genA_rho_inf = 0.0;
   bool is_backward_Euler = true;
@@ -67,6 +70,9 @@ int main( int argc, char *argv[] )
   const double fl_density = cmd_h5r -> read_doubleScalar("/", "fl_density");
   const double fl_mu = cmd_h5r -> read_doubleScalar("/", "fl_mu");
   const double fl_bs_beta = 0.0; // backflow stabilization parameter
+  const double sl_nu = cmd_h5r -> read_doubleScalar("/", "sl_nu");
+  const double mesh_E = cmd_h5r -> read_doubleScalar("/", "mesh_E");
+  const double mesh_nu = cmd_h5r -> read_doubleScalar("/", "mesh_nu");
 
   delete cmd_h5r; H5Fclose(solver_cmd_file);
 
@@ -77,6 +83,7 @@ int main( int argc, char *argv[] )
 
   delete pcmd_h5r; H5Fclose(prepcmd_file);
 
+  // Initialize PETSc
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
 
   const PetscMPIInt rank = SYS_T::get_MPI_rank();
@@ -106,6 +113,7 @@ int main( int argc, char *argv[] )
   SYS_T::GetOptionInt(   "-ttan_freq",           ttan_renew_freq);
   SYS_T::GetOptionBool(  "-is_record_sol",       is_record_sol);
   SYS_T::GetOptionInt(   "-sol_rec_freq",        sol_record_freq);
+  SYS_T::GetOptionString("-restart_name",        restart_name);
 
   // ===== Print Command Line Arguments =====
   SYS_T::cmdPrint(      "part_file:",          part_file);
@@ -122,6 +130,7 @@ int main( int argc, char *argv[] )
   else
     SYS_T::cmdPrint(     "-rho_inf:",            genA_rho_inf);
 
+  SYS_T::cmdPrint(       "-restart_name:",       restart_name);
   SYS_T::cmdPrint(       "-init_time:",          initial_time);
   SYS_T::cmdPrint(       "-init_step:",          initial_step);
   SYS_T::cmdPrint(       "-init_index:",         initial_index);
@@ -190,35 +199,103 @@ int main( int argc, char *argv[] )
   IPLocAssem * locAssem_fluid_ptr = new PLocAssem_Tet4_ALE_VMS_NS_3D_GenAlpha(
       tm_galpha_ptr, quadv->get_num_quadPts(), fl_density, fl_mu, fl_bs_beta );
 
-  IMaterialModel * matmodel = nullptr;
+  IMaterialModel * matmodel       = nullptr;
   IPLocAssem * locAssem_solid_ptr = nullptr;
 
-  
+  if( sl_nu == 0.5 )
+  {
+    IMaterialModel * matmodel = new MaterialModel_NeoHookean_Incompressible_Mixed( "material_model.h5" );
+
+    IPLocAssem * locAssem_solid_ptr = new PLocAssem_Tet4_VMS_Seg_Incompressible(
+        matmodel, tm_galpha_ptr, quadv->get_num_quadPts() );
+  }
+  else
+  {
+    IMaterialModel * matmodel = new MaterialModel_NeoHookean_M94_Mixed( "material_model.h5" );
+
+    locAssem_solid_ptr = new PLocAssem_Tet4_VMS_Seg_Hyperelastic_3D_FEM_GenAlpha(
+        matmodel, tm_galpha_ptr, quadv->get_num_quadPts() );
+  }
+
+  IPLocAssem * locAssem_mesh_ptr = new PLocAssem_Tet4_FSI_Mesh_Elastostatic( mesh_E, mesh_nu );
+
+  // ===== Initial condition =====
+  PDNSolution * sol = new PDNSolution_Mixed_UPV_3D( pNode, fNode, 0 );
+
+  PDNSolution * dot_sol = new PDNSolution_Mixed_UPV_3D( pNode, fNode, 0 );
+
+  // Read sol file
+  SYS_T::file_check(restart_name.c_str());
+  sol->ReadBinary(restart_name.c_str());
+
+  // Read dot_sol file
+  std::string restart_dot_name = "dot_";
+  restart_dot_name.append(restart_name);
+  SYS_T::file_check(restart_dot_name.c_str());
+  dot_sol->ReadBinary(restart_dot_name.c_str());
+
+  SYS_T::commPrint("===> Read sol from disk as a restart run: \n");
+  SYS_T::commPrint("     restart_name:     %s \n",     restart_name.c_str());
+  SYS_T::commPrint("     restart_dot_name: %s \n", restart_dot_name.c_str());
+
+  // ===== Time step info =====
+  PDNTimeStep * timeinfo = new PDNTimeStep(initial_index, initial_time, initial_step);
+
+  // ===== GenBC =====
+  IGenBC * gbc = nullptr;
+
+  // ===== Global assembly routine =====
+  SYS_T::commPrint("===> Initializing Mat K and Vec G ... \n");
+  IPGAssem * gloAssem_ptr = new PGAssem_FSI_FEM( locAssem_fluid_ptr,
+      locAssem_solid_ptr, elements, quads, GMIptr, locElem, locIEN,
+      pNode, locnbc, locebc, gbc, nz_estimate );
+
+  SYS_T::commPrint("===> Assembly nonzero estimate matrix ... \n");
+  gloAssem_ptr->Assem_nonzero_estimate( locElem, locAssem_fluid_ptr,
+      locAssem_solid_ptr, elements, quads, locIEN, pNode, locnbc, locebc, gbc );
+
+  SYS_T::commPrint("===> Matrix nonzero structure fixed. \n");
+  gloAssem_ptr->Fix_nonzero_err_str();
+  gloAssem_ptr->Clear_KG();
+
+  // ===== Global assembly for mesh motion =====
+  SYS_T::commPrint("===> Initializing Mat K_mesh and Vec G_mesh ... \n");
+  IPGAssem * gloAssem_mesh_ptr = new PGAssem_Seg_FEM( locAssem_mesh_ptr,
+      GMIptr, locElem, locIEN, pNode, mesh_locnbc, mesh_locebc );
+
+  SYS_T::commPrint("===> Assembly nonzero estimate for K_mesh ... \n");
+  gloAssem_mesh_ptr->Assem_nonzero_estimate( locElem, locAssem_mesh_ptr, locIEN,
+      pNode, mesh_locnbc );
+
+  SYS_T::commPrint("===> Matrix K_mesh nonzero structure fixed. \n");
+  gloAssem_mesh_ptr->Fix_nonzero_err_str();
+  gloAssem_mesh_ptr->Clear_KG();
+
+  // ===== Linear and nonlinear solver context =====
+  PLinear_Solver_PETSc * lsolver = new PLinear_Solver_PETSc();
+
+  PC upc; lsolver->GetPC(&upc);
+  const PetscInt pfield[1] = {0}, vfields[] = {1,2,3};
+  PCFieldSplitSetBlockSize(upc,4);
+  PCFieldSplitSetFields(upc,"u",3,vfields,vfields);
+  PCFieldSplitSetFields(upc,"p",1,pfield,pfield);
+
+  PLinear_Solver_PETSc * mesh_lsolver = new PLinear_Solver_PETSc(
+      1.0e-12, 1.0e-55, 1.0e30, 500, "mesh_", "mesh_" );
+
+  gloAssem_mesh_ptr->Assem_tangent_residual( sol, sol, 0.0,
+      timeinfo->get_step(), locElem, locAssem_mesh_ptr, elementv,
+      elements, quadv, quads, locIEN, pNode, fNode, mesh_locnbc,
+      mesh_locebc );
+
+  mesh_lsolver -> SetOperator( gloAssem_mesh_ptr->K );
+  PC mesh_pc; mesh_lsolver->GetPC(&mesh_pc);
+  PCFieldSplitSetBlockSize( mesh_pc, 3 );
+
+  SYS_T::commPrint("===> mesh solver LHS setted up.\n");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // Prestress generation nonlinear solver and time solver to be filled
 
 
 
