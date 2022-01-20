b79ed0e1c7eaca1f5fa1aa29e084b2bc7df94293
SUNYujie <12032912@mail.sustech.edu.cn>
2022-01-20 16:11:40 +0800

    This is viis_solid

--- examples/tet4_fsi/include/VTK_Writer_FSI_Tet4.hpp
+++ examples/tet4_fsi/include/VTK_Writer_FSI_Tet4.hpp
@@ -58,6 +58,39 @@ class VTK_Writer_FSI_Tet4
         const std::string &outputName,
         const bool &isXML );
 
+    void writeOutput_solid(
+    	const FEANode * const &fnode_ptr,
+    	const ALocal_IEN * const &lien_v,
+    	const ALocal_IEN * const &lien_p,
+    	const std::vector<int> &sien,
+    	const ALocal_Elem * const &lelem_ptr,
+    	const IVisDataPrep * const &vdata_ptr,
+    	FEAElement * const &elemptr,
+    	const IQuadPts * const &quad,
+    	const double * const * const &pointArrays,
+    	const int &rank, const int &size,
+    	const int &num_of_nodes,
+    	const double &sol_time,
+    	const std::string &outputBName,
+    	const std::string &outputName,
+    	const bool &isXML );
+   
+    void writeOutput_solid_ref(
+    	const FEANode * const &fnode_ptr,
+    	const ALocal_IEN * const &lien_v,
+    	const ALocal_IEN * const &lien_p,
+    	const std::vector<int> &sien,
+    	const ALocal_Elem * const &lelem_ptr,
+    	const IVisDataPrep * const &vdata_ptr,
+    	FEAElement * const &elemptr,
+    	const IQuadPts * const &quad,
+    	const double * const * const &pointArrays,
+    	const int &rank, const int &size,
+    	const int &num_of_nodes,
+    	const double &sol_time,
+    	const std::string &outputBName,
+    	const std::string &outputName,
+    	const bool &isXML );
 
 
   private:
--- examples/tet4_fsi/src/VTK_Writer_FSI_Tet4.cpp
+++ examples/tet4_fsi/src/VTK_Writer_FSI_Tet4.cpp
@@ -428,4 +428,305 @@ void VTK_Writer_FSI_Tet4::writeOutput_fluid(
   gridData->Delete();
 }
 
+
+void VTK_Writer_FSI_Tet4::writeOutput_solid(
+    const FEANode * const &fnode_ptr,
+    const ALocal_IEN * const &lien_v,
+    const ALocal_IEN * const &lien_p,
+    const std::vector<int> &sien,
+    const ALocal_Elem * const &lelem_ptr,
+    const IVisDataPrep * const &vdata_ptr,
+    FEAElement * const &elemptr,
+    const IQuadPts * const &quad,
+    const double * const * const &pointArrays,
+    const int &rank, const int &size,
+    const int &num_of_nodes,
+    const double &sol_time,
+    const std::string &outputBName,
+    const std::string &outputName,
+    const bool &isXML )
+{
+  // This routine requires nqp = 4
+  SYS_T::print_fatal_if(quad->get_num_quadPts() != 4, "Error: VTK_Writer_Tet4 requires 4 quadrature points.\n");
+
+  Interpolater intep( nLocBas );
+
+  // Allocate gridData
+  vtkUnstructuredGrid * gridData = vtkUnstructuredGrid::New();
+
+  // Now generate and store data to gridData
+  // Point object initialized
+  vtkPoints * points = vtkPoints::New();
+
+  // DoubleArray & IntArray allocation
+  const int numDArrays = vdata_ptr->get_arrayCompSize();
+
+  // Check if the number of visualization quantities equal a given number
+  // I will have to manually interpolate these quantities in the for-loop
+  if(numDArrays != 4) SYS_T::print_fatal("Error: vdata size numDArrays != 4. \n");
+
+  // dataVecs is the holder of the interpolated data
+  vtkDoubleArray ** dataVecs = new vtkDoubleArray * [numDArrays];
+  for(int ii=0; ii<numDArrays; ++ii)
+  {
+    dataVecs[ii] = vtkDoubleArray::New();
+    dataVecs[ii] -> SetNumberOfComponents( vdata_ptr->get_arraySizes(ii) );
+    dataVecs[ii] -> SetName( vdata_ptr->get_arrayNames(ii).c_str() );
+    dataVecs[ii] -> SetNumberOfTuples( num_of_nodes );
+  }
+
+  // An additional array for the analysis partition info
+  vtkIntArray * anaprocId = vtkIntArray::New();
+  anaprocId -> SetName("Analysis_Partition");
+  anaprocId -> SetNumberOfComponents(1);
+
+  for(int ee=0; ee<lelem_ptr->get_nlocalele(); ++ee)
+  {
+    if( lelem_ptr->get_elem_tag(ee) == 1 )
+    {
+      const std::vector<int> IEN_v = lien_v -> get_LIEN(ee);
+      const std::vector<int> IEN_p = lien_p -> get_LIEN(ee);
+
+      double ectrl_x[4], ectrl_y[4], ectrl_z[4];
+
+      fnode_ptr -> get_ctrlPts_xyz(nLocBas, &IEN_v[0], ectrl_x, ectrl_y, ectrl_z);
+
+      elemptr->buildBasis( quad, ectrl_x, ectrl_y, ectrl_z );
+
+      const int IEN_s[4] = { sien[ee*4+0], sien[ee*4+1], sien[ee*4+2], sien[ee*4+3] };
+
+      // Interpolate data and assign to dataVecs
+      std::vector<double> inputInfo; inputInfo.clear();
+      int asize = vdata_ptr -> get_arraySizes(0);
+
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_v[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[0][pt_index * asize + kk] );
+      }
+
+      // displacement interpolation
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[0] );
+
+      // use displacement to update points
+      intep.interpolateVTKPts( IEN_s, ectrl_x, ectrl_y, ectrl_z, inputInfo, elemptr, points );
+
+      // Interpolate detF
+      interpolateJ( IEN_s, inputInfo, elemptr, dataVecs[1] );
+
+      // Interpolate the pressure scalar
+      inputInfo.clear();
+      asize = vdata_ptr->get_arraySizes(2);  
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_p[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[1][pt_index * asize + kk ] );
+      }
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[2] ); 
+      // Interpolate the velocity vector
+      inputInfo.clear();
+      asize = vdata_ptr->get_arraySizes( 3 );  
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_v[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[2][pt_index * asize + kk ] );
+      }
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[3] );
+      
+      // Set mesh connectivity
+      VIS_T::setTetraelem( IEN_s[0], IEN_s[1], IEN_s[2], IEN_s[3], gridData );
+
+      // Analysis mesh partition
+      const int e_global = lelem_ptr->get_elem_loc(ee);
+      anaprocId->InsertNextValue( epart_map[e_global] );
+    }
+  }
+
+  gridData -> SetPoints( points ); points -> Delete();
+
+  for(int ii=0; ii<numDArrays; ++ii)
+  {
+    gridData->GetPointData()->AddArray( dataVecs[ii] );
+    dataVecs[ii]->Delete();
+  }
+  delete [] dataVecs;
+
+  // Add cell data
+  gridData->GetCellData()->AddArray(anaprocId);
+  anaprocId->Delete();
+
+  // If postprocess is parallel, record its partition
+  if(size > 1)
+  {
+    int numCells = gridData->GetNumberOfCells();
+    vtkIntArray * procId = vtkIntArray::New();
+    procId->SetName("PostProcess_ID");
+    procId->SetNumberOfComponents(1);
+    for(int ii=0; ii<numCells; ++ii)
+      procId->InsertComponent(ii, 0 , rank);
+    gridData->GetCellData()->AddArray(procId);
+    procId->Delete();
+  }
+
+  // Write gridData
+  VIS_T::writeVisFile( gridData, outputBName, outputName, rank, size, sol_time, isXML );
+
+  // Clean gridData
+  PetscPrintf(PETSC_COMM_WORLD, "-- Clean gridData object.\n");
+  gridData->Delete();
+}
+
+
+void VTK_Writer_FSI_Tet4::writeOutput_solid_ref(
+    const FEANode * const &fnode_ptr,
+    const ALocal_IEN * const &lien_v,
+    const ALocal_IEN * const &lien_p,
+    const std::vector<int> &sien,
+    const ALocal_Elem * const &lelem_ptr,
+    const IVisDataPrep * const &vdata_ptr,
+    FEAElement * const &elemptr,
+    const IQuadPts * const &quad,
+    const double * const * const &pointArrays,
+    const int &rank, const int &size,
+    const int &num_of_nodes,
+    const double &sol_time,
+    const std::string &outputBName,
+    const std::string &outputName,
+    const bool &isXML )
+{
+  // This routine requires nqp = 4
+  SYS_T::print_fatal_if(quad->get_num_quadPts() != 4, "Error: VTK_Writer_Tet4 requires 4 quadrature points.\n");
+
+  Interpolater intep( nLocBas );
+
+  // Allocate gridData
+  vtkUnstructuredGrid * gridData = vtkUnstructuredGrid::New();
+
+  // Now generate and store data to gridData
+  // Point object initialized
+  vtkPoints * points = vtkPoints::New();
+
+  // DoubleArray & IntArray allocation
+  const int numDArrays = vdata_ptr->get_arrayCompSize();
+
+  // Check if the number of visualization quantities equal a given number
+  // I will have to manually interpolate these quantities in the for-loop
+  if(numDArrays != 3) SYS_T::print_fatal("Error: vdata size numDArrays != 3. \n");
+
+  // dataVecs is the holder of the interpolated data
+  vtkDoubleArray ** dataVecs = new vtkDoubleArray * [numDArrays];
+  for(int ii=0; ii<numDArrays; ++ii)
+  {
+    dataVecs[ii] = vtkDoubleArray::New();
+    dataVecs[ii] -> SetNumberOfComponents( vdata_ptr->get_arraySizes(ii) );
+    dataVecs[ii] -> SetName( vdata_ptr->get_arrayNames(ii).c_str() );
+    dataVecs[ii] -> SetNumberOfTuples( num_of_nodes );
+  }
+
+  // An additional array for the analysis partition info
+  vtkIntArray * anaprocId = vtkIntArray::New();
+  anaprocId -> SetName("Analysis_Partition");
+  anaprocId -> SetNumberOfComponents(1);
+
+
+  for(int ee=0; ee<lelem_ptr->get_nlocalele(); ++ee)
+  {
+    if( lelem_ptr->get_elem_tag(ee) == 1 )
+    {
+      const std::vector<int> IEN_v = lien_v -> get_LIEN(ee);
+      const std::vector<int> IEN_p = lien_p -> get_LIEN(ee);
+
+      double ectrl_x[4], ectrl_y[4], ectrl_z[4];
+
+      fnode_ptr -> get_ctrlPts_xyz(nLocBas, &IEN_v[0], ectrl_x, ectrl_y, ectrl_z);
+
+      elemptr->buildBasis( quad, ectrl_x, ectrl_y, ectrl_z );
+
+      const int IEN_s[4] = { sien[ee*4+0], sien[ee*4+1], sien[ee*4+2], sien[ee*4+3] };
+
+      // Interpolate data and assign to dataVecs
+      std::vector<double> inputInfo; inputInfo.clear();
+      int asize = vdata_ptr -> get_arraySizes(0);
+
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_v[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[0][pt_index * asize + kk] );
+      }
+
+      // displacement interpolation
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[0] );
+
+      // use displacement to update points
+      intep.interpolateVTKPts( IEN_s, ectrl_x, ectrl_y, ectrl_z, inputInfo, elemptr, points );
+
+      // Interpolate the pressure scalar
+      inputInfo.clear();
+      asize = vdata_ptr->get_arraySizes(1);  
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_p[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[1][pt_index * asize + kk ] );
+      }
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[1] ); 
+      // Interpolate the velocity vector
+      inputInfo.clear();
+      asize = vdata_ptr->get_arraySizes( 2 );  
+      for(int jj=0; jj<nLocBas; ++jj)
+      {
+        int pt_index = IEN_v[jj];
+        for(int kk=0; kk<asize; ++kk)
+          inputInfo.push_back( pointArrays[2][pt_index * asize + kk ] );
+      }
+      intep.interpolateVTKData( asize, IEN_s, inputInfo, elemptr, dataVecs[2] );
+      
+
+      // Set mesh connectivity
+      VIS_T::setTetraelem( IEN_s[0], IEN_s[1], IEN_s[2], IEN_s[3], gridData );
+
+      // Analysis mesh partition
+      const int e_global = lelem_ptr->get_elem_loc(ee);
+      anaprocId->InsertNextValue( epart_map[e_global] );
+    }
+  }
+
+  gridData -> SetPoints( points ); points -> Delete();
+
+  for(int ii=0; ii<numDArrays; ++ii)
+  {
+    gridData->GetPointData()->AddArray( dataVecs[ii] );
+    dataVecs[ii]->Delete();
+  }
+  delete [] dataVecs;
+
+  // Add cell data
+  gridData->GetCellData()->AddArray(anaprocId);
+  anaprocId->Delete();
+
+  // If postprocess is parallel, record its partition
+  if(size > 1)
+  {
+    int numCells = gridData->GetNumberOfCells();
+    vtkIntArray * procId = vtkIntArray::New();
+    procId->SetName("PostProcess_ID");
+    procId->SetNumberOfComponents(1);
+    for(int ii=0; ii<numCells; ++ii)
+      procId->InsertComponent(ii, 0 , rank);
+    gridData->GetCellData()->AddArray(procId);
+    procId->Delete();
+  }
+
+  // Write gridData
+  VIS_T::writeVisFile( gridData, outputBName, outputName, rank, size, sol_time, isXML );
+
+  // Clean gridData
+  PetscPrintf(PETSC_COMM_WORLD, "-- Clean gridData object.\n");
+  gridData->Delete();
+}
+
 // EOF
