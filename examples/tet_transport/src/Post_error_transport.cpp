d921b7b2d0b3e520674b280badd2cdcca6901b1a
SUNYujie <12032912@mail.sustech.edu.cn>
2022-01-28 14:01:00 +0800

    add a get_manu_sol_errorH1 function to comput H1 and 3 new functions to comput spatial derivatives of exact solutions for Post_error_transport.cpp

--- examples/tet_transport/src/Post_error_transport.cpp
+++ examples/tet_transport/src/Post_error_transport.cpp
@@ -25,6 +25,27 @@ double POST_ERROR_T::exact_sol_z( const double &x, const double &y, const double
 }
 
 
+double POST_ERROR_T::exact_sol_dx( const double &x, const double &y, const double &z, const double &time )
+{
+  const double pi = MATH_T::PI;
+  return time * time * pi * cos(pi*x) * sin(pi*y) * sin(pi*z);
+}
+
+
+double POST_ERROR_T::exact_sol_dy( const double &x, const double &y, const double &z, const double &time )
+{
+  const double pi = MATH_T::PI;
+  return time * time * pi * sin(pi*x) * cos(pi*y) * sin(pi*z);
+}
+
+
+double POST_ERROR_T::exact_sol_dz( const double &x, const double &y, const double &z, const double &time )
+{
+  const double pi = MATH_T::PI;
+  return time * time * pi * sin(pi*x) * sin(pi*y) * cos(pi*z);
+}
+
+
 double POST_ERROR_T::get_manu_sol_error(
     const double &time,
     const double * const &solu,
@@ -58,6 +79,58 @@ double POST_ERROR_T::get_manu_sol_error(
   }
 
   return error;
+}
+
+
+double POST_ERROR_T::get_manu_sol_errorH1(
+    const double &time,
+    const double * const &solu,
+    const FEAElement * const &element,
+    const double * const &ectrlPts_x,
+    const double * const &ectrlPts_y,
+    const double * const &ectrlPts_z,
+    const IQuadPts * const &quad )
+{
+  double errorH1 = 0.0;
+
+  for(int qua=0; qua<quad->get_num_quadPts(); ++qua)
+  {
+    const double gwts = element->get_detJac(qua) * quad->get_qw(qua);
+
+    const std::vector<double> R = element -> get_R( qua );
+
+    const std::vector<double> R_dx = element -> get_dR_dx( qua );
+    
+    const std::vector<double> R_dy = element -> get_dR_dy( qua );
+    
+    const std::vector<double> R_dz = element -> get_dR_dz( qua );
+    
+    double sol = 0.0, sol_dx = 0.0, sol_dy = 0.0, sol_dz = 0.0;
+    double coor_x = 0.0, coor_y = 0.0, coor_z = 0.0;
+
+    for(int ii=0; ii<element-> get_nLocBas(); ++ii)
+    {
+      coor_x += ectrlPts_x[ii] * R[ii];
+      coor_y += ectrlPts_y[ii] * R[ii];
+      coor_z += ectrlPts_z[ii] * R[ii];
+      sol    += solu[ii]       * R[ii];
+      sol_dx += solu[ii]    * R_dx[ii];
+      sol_dy += solu[ii]    * R_dy[ii];
+      sol_dz += solu[ii]    * R_dz[ii];
+    }
+
+    const double exact = exact_sol(coor_x, coor_y, coor_z, time );
+    const double exact_dx = exact_sol_dx(coor_x, coor_y, coor_z, time );
+    const double exact_dy = exact_sol_dy(coor_x, coor_y, coor_z, time );
+    const double exact_dz = exact_sol_dz(coor_x, coor_y, coor_z, time );
+
+    errorH1 += (sol_dx - exact_dx) * (sol_dx - exact_dx) * gwts 
+	     + (sol_dy - exact_dy) * (sol_dy - exact_dy) * gwts 
+	     + (sol_dz - exact_dz) * (sol_dz - exact_dz) * gwts 
+	     + (sol - exact) * (sol - exact) * gwts;
+  }
+
+  return errorH1;
 }
 
 // EOF
