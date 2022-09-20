c1ed6589beeb528c071bdd9aad84c799c6caf67d
Ju Liu <liujuy@gmail.com>
2022-09-20 12:05:41 +0800

    in

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -2,6 +2,7 @@
 #include "Vec_Tools.hpp"
 #include "Vector_3.hpp"
 #include "Matrix_3x3.hpp"
+#include "SymmMatrix_3x3.hpp"
 #include "HDF5_Reader.hpp"
 #include "PostVectSolution.hpp"
 
@@ -13,25 +14,16 @@ int main( int argc, char * argv[] )
   a.push_back("july 31th \n");
   a.push_back("aug 31th \n");
 
-  for(auto &out : a) out = "hello\n";
+  //for(auto &out : a) out = "hello\n";
 
   //for(auto out : a) std::cout<<out;
 
-  Matrix_3x3 A;
+  SymmMatrix_3x3 A;
 
-  A.gen_rand();
+  A.gen_id();
 
-  Matrix_3x3 B = cofactor(A);
 
-  Matrix_3x3 C( A );
-
-  C.inverse();
-  C.transpose();
-  C.scale(A.det());
-
-  C -= B;
-
-  C.print_in_row();
+  A.print_in_row();
 
   return EXIT_SUCCESS;
 }
