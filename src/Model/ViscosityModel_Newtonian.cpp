45c37be872cd679028f4327c072861113404b925
SeaVegetable <11911226@mail.sustech.edu.cn>
2022-10-03 14:35:18 +0800

    add implementation of get_info and write_hdf5 of Newtonian model

--- src/Model/ViscosityModel_Newtonian.cpp
+++ src/Model/ViscosityModel_Newtonian.cpp
@@ -22,3 +22,26 @@ ViscosityModel_Newtonian::ViscosityModel_Newtonian( const char * const &fname )
 ViscosityModel_Newtonian::~ViscosityModel_Newtonian()
 {}
 
+void ViscosityModel_Newtonian::print_info() const
+{
+  SYS_T::commPrint("\t  ViscosityModel_Newtonian:: \n");
+  SYS_T::commPrint("\t  Viscosity mu = %e \n", mu);
+}
+
+void ViscosityModel_Newtonian::write_hdf5( const char * const &fname ) const
+{
+  if( SYS_T::get_MPI_rank() == 0 )
+  {
+    hid_t file_id = H5Fcreate( fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT );
+    HDF5_Writer * h5w = new HDF5_Writer( file_id );
+
+    h5w -> write_string("model_name", get_model_name());
+    h5w -> write_doubleScalar( "mu", mu );
+
+    delete h5w;
+    H5Fclose(file_id);
+  }
+
+  MPI_Barrier(PETSC_COMM_WORLD);
+}
+
