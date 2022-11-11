508959027bc8ed2b376fcf59f5244bc608057159
Sun-Yujie <12032912@mail.sustech.edu.cn>
2022-11-11 12:04:10 +0800

    update VTK on taiyi

--- conf/Sun_taiyi.cmake
+++ conf/Sun_taiyi.cmake
@@ -13,7 +13,7 @@
 # VTK_DIR should be the vtk directory/lib/cmake/vtk-version,
 # which contains VTKConfig.cmake file.
 # In the guide, vtk directory is $HOME/lib/VTK-7.1.1-shared
-set(VTK_DIR /work/mae-sunyj/lib/VTK-8.2.0-shared/lib/cmake/vtk-8.2)
+set(VTK_DIR /work/mae-sunyj/lib/VTK-8.2.0-shared/lib64/cmake/vtk-8.2)
 
 # Modify the PETSC_DIR variable to point to the location of PETSc.
 set(PETSC_DIR /work/mae-sunyj/lib/petsc-3.11.3-opt)
