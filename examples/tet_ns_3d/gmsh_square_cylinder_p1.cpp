be0dea036e7ed65078f16eb2224e0396091694f1
Ju Liu <liujuy@gmail.com>
2022-12-07 19:08:14 +0800

    working on the gmsh converter

--- examples/tet_ns_3d/gmsh_square_cylinder_p1.cpp
+++ examples/tet_ns_3d/gmsh_square_cylinder_p1.cpp
@@ -12,13 +12,23 @@ int main(int argc, char * argv[] )
 {
   std::string gmshFile = "turbulent_cylinder_LR.msh";
 
+  PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
+
+  SYS_T::GetOptionString("-gmsh_file", gmshFile);
+
+  std::cout<<" -gmsh_file: "<<gmshFile<<std::endl;
+
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
   GIO -> print_info();
 
+  const std::string wmname("whole_vol");
+  const bool isXML = true;
+  GIO -> write_vtu( wmname, isXML );
 
+  delete GIO;
+  PetscFinalize();
   return EXIT_SUCCESS;
 }
-
 
 // EOF
