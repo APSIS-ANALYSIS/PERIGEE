f7bd2174ff9073d782695937a3170417c8d26597
Ju Liu <liujuy@gmail.com>
2022-12-07 18:10:29 +0800

    update the solution vector to account for multiple inlets

--- examples/tet_ns_3d/src/PDNSolution_NS.cpp
+++ examples/tet_ns_3d/src/PDNSolution_NS.cpp
@@ -49,17 +49,13 @@ PDNSolution_NS::~PDNSolution_NS()
 
 void PDNSolution_NS::Init_zero(const APart_Node * const &pNode_ptr)
 {
-  int location[4];
   const double value[4] = {0.0, 0.0, 0.0, 0.0};
   const int nlocalnode = pNode_ptr->get_nlocalnode();
 
   for(int ii=0; ii<nlocalnode; ++ii)
   {
-    location[0] = pNode_ptr->get_node_loc(ii) * 4;
-    location[1] = location[0] + 1;
-    location[2] = location[0] + 2;
-    location[3] = location[0] + 3;
-
+    const int pos = pNode_ptr->get_node_loc(ii) * 4;
+    const int location[4] = { pos, pos + 1, pos +2, pos + 3 };
     VecSetValues(solution, 4, location, value, INSERT_VALUES);
   }
 
@@ -79,59 +75,16 @@ void PDNSolution_NS::Init_flow_parabolic(
     const FEANode * const &fNode_ptr,
     const ALocal_Inflow_NodalBC * const &infbc )
 {
-  int location[4];
   double value[4] = {0.0, 0.0, 0.0, 0.0};
   const int nlocalnode = pNode_ptr->get_nlocalnode();
 
   // First enforce everything to be zero
   for(int ii=0; ii<nlocalnode; ++ii)
   {
-    location[0] = pNode_ptr->get_node_loc(ii) * 4;
-    location[1] = location[0] + 1;
-    location[2] = location[0] + 2;
-    location[3] = location[0] + 3;
-
+    const int pos = pNode_ptr->get_node_loc(ii) * 4;
+    const int location[4] = { pos, pos + 1, pos +2, pos + 3 };
     VecSetValues(solution, 4, location, value, INSERT_VALUES);
   }
-
-  // Maximum speed formula is 
-  //             2.0 x flow rate (1.0) / surface area
-  // Here I use the unit flow rate, and the actual flow rate is adjusted
-  // based on the CVFlowRate class.
-  const double vmax = 2.0 / infbc->get_fularea();
-
-  const double out_nx = infbc->get_outvec().x();
-  const double out_ny = infbc->get_outvec().y();
-  const double out_nz = infbc->get_outvec().z();
-
-  // If there are inflow nodes, set their value to be parabolic flow
-  if( infbc->get_Num_LD() > 0)
-  {
-    for(int ii=0; ii<nlocalnode; ++ii)
-    {
-      if( infbc->is_inLDN(pNode_ptr->get_node_loc(ii)) )
-      {
-        location[0] = pNode_ptr->get_node_loc(ii) * 4;
-        location[1] = location[0] + 1;
-        location[2] = location[0] + 2;
-        location[3] = location[0] + 3;
-
-        const Vector_3 pt = fNode_ptr -> get_ctrlPts_xyz(ii);
-        const double r =  infbc -> get_radius( pt );
-
-        const double vel = vmax * (1.0 - r*r);
-
-        // -1.0 is multiplied to make the flow direction inward
-        value[1] = vel * out_nx;
-        value[2] = vel * out_ny;
-        value[3] = vel * out_nz;
-
-        VecSetValues(solution, 4, location, value, INSERT_VALUES);
-      }
-    }
-  }
-
-  Assembly_GhostUpdate();
 
   if(is_print)
   {
@@ -140,28 +93,71 @@ void PDNSolution_NS::Init_flow_parabolic(
     SYS_T::commPrint("                       velo_y = parabolic \n");
     SYS_T::commPrint("                       velo_z = parabolic \n");
     SYS_T::commPrint("                       flow rate 1.0 .\n");
-    SYS_T::commPrint("                       max speed %e.\n", vmax);
-    SYS_T::commPrint("                       active area is %e.\n", infbc->get_actarea() );
-    SYS_T::commPrint("                       full area is %e.\n", infbc->get_fularea() );
-    SYS_T::commPrint("                       direction [%e %e %e].\n", out_nx, out_ny, out_nz);
   }
+
+  const int num_nbc = infbc->get_num_nbc();
+
+  for(int nbc_id=0; nbc_id<num_nbc; ++nbc_id)
+  {
+    // Maximum speed formula is 
+    //             2.0 x flow rate (1.0) / surface area
+    // Here I use the unit flow rate, and the actual flow rate is adjusted
+    // based on the CVFlowRate class.
+    const double vmax = 2.0 / infbc->get_fularea(nbc_id);
+    
+    const double out_nx = infbc->get_outvec(nbc_id).x();
+    const double out_ny = infbc->get_outvec(nbc_id).y();
+    const double out_nz = infbc->get_outvec(nbc_id).z();
+
+    // If there are inflow nodes, set their value to be parabolic flow
+    if( infbc->get_Num_LD(nbc_id) > 0)
+    {
+      for(int ii=0; ii<nlocalnode; ++ii)
+      {
+        if( infbc->is_inLDN(nbc_id, pNode_ptr->get_node_loc(ii)) )
+        {
+          const int pos = pNode_ptr->get_node_loc(ii) * 4;
+          const int location[4] = { pos, pos + 1, pos +2, pos + 3 };
+
+          const Vector_3 pt = fNode_ptr -> get_ctrlPts_xyz(ii);
+          const double r =  infbc -> get_radius( nbc_id, pt );
+
+          const double vel = vmax * (1.0 - r*r);
+
+          // -1.0 is multiplied to make the flow direction inward
+          value[1] = vel * out_nx;
+          value[2] = vel * out_ny;
+          value[3] = vel * out_nz;
+
+          VecSetValues(solution, 4, location, value, INSERT_VALUES);
+        }
+      }
+    }
+
+    if(is_print)
+    {
+      SYS_T::commPrint("                       -- nbc_id = %d \n", nbc_id);
+      SYS_T::commPrint("                          max speed %e.\n", vmax);
+      SYS_T::commPrint("                          active area is %e.\n", infbc->get_actarea(nbc_id) );
+      SYS_T::commPrint("                          full area is %e.\n", infbc->get_fularea(nbc_id) );      SYS_T::commPrint("                          outward normal direction [%e %e %e].\n", out_nx, out_ny, out_nz);
+    }
+  }
+
+  Assembly_GhostUpdate();
 }
 
 void PDNSolution_NS::Init_pipe_parabolic(
     const APart_Node * const &pNode_ptr,
     const FEANode * const &fNode_ptr )
 {
-  int location[4];
   double value[4] = {0.0, 0.0, 0.0, 0.0};
   const int nlocalnode = pNode_ptr->get_nlocalnode();
 
   // First enforce everything to be zero
   for(int ii=0; ii<nlocalnode; ++ii)
   {
-    location[0] = pNode_ptr->get_node_loc(ii) * 4;
-    location[1] = location[0] + 1;
-    location[2] = location[0] + 2;
-    location[3] = location[0] + 3;
+    const int pos = pNode_ptr->get_node_loc(ii) * 4;
+    const int location[4] = { pos, pos + 1, pos +2, pos + 3 };
 
     VecSetValues(solution, 4, location, value, INSERT_VALUES);
   }
@@ -178,10 +174,8 @@ void PDNSolution_NS::Init_pipe_parabolic(
 
   for(int ii=0; ii<nlocalnode; ++ii)
   {
-    location[0] = pNode_ptr->get_node_loc(ii) * 4;
-    location[1] = location[0] + 1;
-    location[2] = location[0] + 2;
-    location[3] = location[0] + 3;
+    const int pos = pNode_ptr->get_node_loc(ii) * 4;
+    const int location[4] = { pos, pos + 1, pos +2, pos + 3 };
 
     const double x = fNode_ptr->get_ctrlPts_x(ii);
     const double y = fNode_ptr->get_ctrlPts_y(ii);
