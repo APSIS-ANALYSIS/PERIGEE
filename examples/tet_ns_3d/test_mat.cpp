0d3a8d475d0622e624ac67be149ef10499175f3d
Ju Liu <liujuy@gmail.com>
2023-01-09 19:38:05 +0800

    a temporary test driver in

---
+++ examples/tet_ns_3d/test_mat.cpp
@@ -0,0 +1,42 @@
+#include "HDF5_Writer.hpp"
+#include "AGlobal_Mesh_Info_FEM_3D.hpp"
+#include "APart_Basic_Info.hpp"
+#include "ALocal_EBC_outflow.hpp"
+#include "ALocal_Inflow_NodalBC.hpp"
+#include "QuadPts_Gauss_Triangle.hpp"
+#include "QuadPts_Gauss_Tet.hpp"
+#include "FEAElement_Tet4.hpp"
+#include "FEAElement_Tet10_v2.hpp"
+#include "FEAElement_Triangle3_3D_der0.hpp"
+#include "FEAElement_Triangle6_3D_der0.hpp"
+#include "CVFlowRate_Unsteady.hpp"
+#include "CVFlowRate_Linear2Steady.hpp"
+#include "GenBC_Resistance.hpp"
+#include "GenBC_RCR.hpp"
+#include "GenBC_Inductance.hpp"
+#include "GenBC_Coronary.hpp"
+#include "GenBC_Pressure.hpp"
+#include "PLocAssem_Tet_VMS_NS_GenAlpha.hpp"
+#include "PGAssem_NS_FEM.hpp"
+#include "PTime_NS_Solver.hpp"
+
+int main( int argc, char *argv[])
+{
+  std::string part_file("part");
+
+  PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
+
+  const PetscMPIInt rank = SYS_T::get_MPI_rank();
+  ALocal_NodalBC * locnbc = new ALocal_NodalBC(part_file, rank);
+  APart_Node * pNode = new APart_Node(part_file, rank);
+
+  Matrix_PETSc * pmat = new Matrix_PETSc(pNode, locnbc);
+
+  pmat->gen_perm_bc(pNode, locnbc);
+
+
+  PetscFinalize();
+  return EXIT_SUCCESS;
+}
+
+//EOF
