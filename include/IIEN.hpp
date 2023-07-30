a5a951ac153f30a3e4cc916e8d95d8a16a1ea6b8
Liu, Ju <liujuy@gmail.com>
2023-07-30 18:05:29 +0800

    clean the folder

--- include/IIEN.hpp
+++ include/IIEN.hpp
@@ -2,8 +2,9 @@
 #define IIEN_HPP
 // ==================================================================
 // IIEN.hpp
-// The interface for IEN array classes.
+// This is an interface file for the IEN array classes.
 //
+// Author: Ju Liu
 // Date: Sept. 24th 2013
 // ==================================================================
 #include <cstdlib>
@@ -13,11 +14,13 @@ class IIEN
 {
   public:
     IIEN(){};
+
     virtual ~IIEN(){};
 
     // get the IEN arrray for element e at local node l_node
     virtual int get_IEN( const int &ee, const int &l_node ) const = 0;
 
+    // get the number of local basis functions (per element)
     virtual int get_nLocBas( const int &ee = 0 ) const = 0;
 
     // print IEN array
