e2a6e46c296b6c7c2a28fb6d08e6a7bd908a4436
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2023-08-21 20:11:18 +0800

    Modified the function all of read_vtu/vtp_grid in NodalBC_3D_wall. In for-loops only the gnode would be used

--- src/Mesh/NodalBC_3D_wall.cpp
+++ src/Mesh/NodalBC_3D_wall.cpp
@@ -22,7 +22,7 @@ NodalBC_3D_wall::NodalBC_3D_wall(
 
   int wall_numpts, wall_numcels;
   std::vector<double> wall_pts;
-  std::vector<int> wall_ien, wall_gnode, wall_gelem;
+  std::vector<int> wall_ien;
 
   std::vector<int> ring_gnode {};
 
@@ -31,13 +31,14 @@ NodalBC_3D_wall::NodalBC_3D_wall(
     SYS_T::file_check(wall_file);
 
     VTK_T::read_vtp_grid( wall_file, wall_numpts, wall_numcels, wall_pts,
-        wall_ien, wall_gnode, wall_gelem );
+        wall_ien );
 
     for( const auto &capfile : cap_files )
     {
       SYS_T::file_check( capfile );
 
-      VTK_T::read_vtp_grid( capfile, numpts, numcels, pts, ien, gnode, gelem );
+      VTK_T::read_vtp_grid( capfile, numpts, numcels, pts, ien );
+      gnode = VTK_T::read_int_PointData(capfile, "GlobalNodeID"); 
     
       VEC_T::insert_end( ring_gnode, gnode );
     }
@@ -47,19 +48,24 @@ NodalBC_3D_wall::NodalBC_3D_wall(
     SYS_T::file_check(wall_file);
 
     VTK_T::read_vtu_grid( wall_file, wall_numpts, wall_numcels, wall_pts,
-        wall_ien, wall_gnode, wall_gelem );
+        wall_ien );
 
     for( const auto &capfile : cap_files )
     {
       SYS_T::file_check( capfile );
 
-      VTK_T::read_vtu_grid( capfile, numpts, numcels, pts, ien, gnode, gelem );
+      VTK_T::read_vtu_grid( capfile, numpts, numcels, pts, ien );
+      gnode = VTK_T::read_int_PointData(capfile, "GlobalNodeID");
 
       VEC_T::insert_end( ring_gnode, gnode );
     }
   }
   else
     SYS_T::print_fatal("Error: Nodal_3D_wall unknown file type.\n");
+
+  const std::vector<int> wall_gnode = VTK_T::read_int_PointData(wall_file, "GlobalNodeID");
+  const std::vector<int> wall_gelem = VTK_T::read_int_CellData(wall_file, "GlobalElementID");
+
 
   VEC_T::sort_unique_resize( ring_gnode );
 
