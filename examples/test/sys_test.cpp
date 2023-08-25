07bf7323a6347badd8fd2f2f141484269a5e7934
Ju Liu <liujuy@gmail.com>
2023-08-25 09:31:13 +0800

    get rid of the last a few pieces of nodalbc_3d_vtp

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -12,42 +12,9 @@
 #include "Matrix_double_6by6_Array.hpp"
 #include "VTK_Tools.hpp"
 #include "NodalBC.hpp"
-#include "NodalBC_3D_vtp.hpp"
-#include "NodalBC_3D_vtu.hpp"
 
 int main(int argc, char *argv[])
 {
-  const int nFunc = 7500;
-
-  std::vector<std::string> fname { "wall_vol.vtu" };
-
-  INodalBC * a = new NodalBC_3D_vtu( fname, nFunc );
-
-  INodalBC * b = new NodalBC( fname, nFunc );
-
-  for(int ii=0; ii<nFunc; ++ii)
-  {
-    if( a-> get_ID(ii) != b-> get_ID(ii) )
-      std::cout<<"Error "<<a-> get_ID(ii)<<'\t'<<b-> get_ID(ii)<<'\n';
-  }
-
-  for(unsigned int ii=0; ii< a->get_num_dir_nodes(); ++ii)
-  {
-    if( a->get_dir_nodes(ii) != b->get_dir_nodes(ii) )
-      std::cout<<"Error! \n";
-  }
-
-  for( unsigned int ii=0; ii< a->get_num_per_nodes(); ++ii )
-  {
-    if( a->get_per_slave_nodes(ii) != b->get_per_slave_nodes(ii) )
-      std::cout<<"ERROR! \n";
-    
-    if( a->get_per_master_nodes(ii) != b->get_per_master_nodes(ii) )
-      std::cout<<"ERROR! \n";
-  }
-  
-  delete a; delete b;
-
   return EXIT_SUCCESS;
 }
 
