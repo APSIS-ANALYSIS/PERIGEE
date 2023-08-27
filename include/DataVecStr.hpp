5da2cdff06487517ec772a9832f10a05fe457d45
Ju Liu <liujuy@gmail.com>
2023-08-27 19:43:45 +0800

    define a struct for vector with string

---
+++ include/DataVecStr.hpp
@@ -0,0 +1,22 @@
+#ifndef DATAVECSTR_HPP
+#define DATAVECSTR_HPP
+// ----------------------------------------------------------------------------
+// DataVecStr.hpp
+// ----------------------------------------------------------------------------
+// DataVecStr defines a data type for a std::vector with a string holding its
+// name.
+//
+// Author: Ju Liu
+// Date: Aug. 27 2023
+// ----------------------------------------------------------------------------
+#include <vector>
+#include <string>
+
+template <typename T>
+struct DataVecStr
+{
+  std::vector<T> data {};
+  std::string name {"undefined"};
+};
+
+#endif
