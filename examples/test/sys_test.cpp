b5316ee48658da9a9e5c3c1418c1b57f5ebb11a8
Ju Liu <liujuy@gmail.com>
2023-08-30 09:06:59 +0800

    tested the implementation of write tet grid

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -13,27 +13,23 @@
 #include "VTK_Tools.hpp"
 #include "NodalBC.hpp"
 #include "DataVecStr.hpp"
+#include "Tet_Tools.hpp"
 
 int main(int argc, char *argv[])
 {
-  AssociateObject ao = AssociateObject::Cell;
+  PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
+  std::vector<int> eid {11, 21};
+  std::vector<int> ptag {-2, 23};
+  TET_T::write_tet_grid( "old", 5, 2, {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0}, {0,1,2,3,1, 2, 3,4}, {-1, 32, 5, 3, 22}, eid, ptag, true );
 
-  std::vector<int> b {1,2};
+  std::vector<DataVecStr<int>> input {};
+  input.push_back({{-1,32,5,3, 22}, "GlobalNodeID", AssociateObject::Node});
+  input.push_back({eid, "GlobalElementID", AssociateObject::Cell});
+  input.push_back({{-2, 23}, "Physics_tag", AssociateObject::Cell});
 
-  DataVecStr<int> a { {1,2,3,4,5,6}, "tt", AssociateObject::Node };
+  TET_T::write_tet_grid( "new", 5, 2, {0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0}, {0,1,2,3, 1,2,3,4}, input, true ); 
 
-  std::cout<<a<<std::endl;
-
-  std::vector<DataVecStr<int>> mm {};
-
-  mm.push_back({ b, "first", ao});
-
-  VEC_T::print(mm[0].get_data());
-
-  std::cout<<mm[0].get_name()<<std::endl;
-  
-  std::cout<<mm[0]<<std::endl;
-  
+  PetscFinalize();
   return EXIT_SUCCESS;
 }
 
