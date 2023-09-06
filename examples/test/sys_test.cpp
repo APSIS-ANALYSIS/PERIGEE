3e6188fb74d238389b29c0f40735aeb89b4d1b47
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2023-09-06 17:42:46 +0800

    Test code for read_mesh4, run 'run_test1.sh' on Mac, then upload *.h5 and 'compare2.sh' to Taiyi, then run 'compare2.sh'

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -12,17 +12,33 @@
 #include "Matrix_double_6by6_Array.hpp"
 #include "VTK_Tools.hpp"
 #include "NodalBC.hpp"
+#include "Gmsh_FileIO.hpp"
 
 int main(int argc, char *argv[])
 {
-  std::vector<int> a(5, -1);
+  // test 1
+  Gmsh_FileIO * GIO_1 = new Gmsh_FileIO( "fsi_cylinder.msh" );
+  GIO_1 -> print_info();
+  GIO_1 -> write_tet_h5(0, {0, 1, 2});
+  GIO_1 -> write_tet_h5(1, {3, 4, 5});
 
-  std::vector<int> b;
-  b.assign(5, -1);
+  delete GIO_1;
 
-  VEC_T::print(a);
-  VEC_T::print(b);
+  // test 2
+  Gmsh_FileIO * GIO_2 = new Gmsh_FileIO( "fsi_beam.msh" );
+  GIO_2 -> print_info();
+  GIO_2 -> write_tet_h5(0, {0, 1, 2, 3, 4, 5});
+  GIO_2 -> write_tet_h5(1, {7, 8, 9, 10, 11, 12});
 
+  delete GIO_2;
+
+  // test 3
+  Gmsh_FileIO * GIO_3 = new Gmsh_FileIO( "cook_membrane.msh" );
+
+  GIO_3 -> print_info();
+  GIO_3 -> write_tri_h5(0, {0, 1, 2, 3}); // 2d problem
+
+  delete GIO_3;
 
   return EXIT_SUCCESS;
 }
