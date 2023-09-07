87ea98016d0cebc742e89700235ca49ade8ed273
SeaVegetable <1135167789@qq.com>
2023-09-07 21:41:10 +0800

    add a head file

--- include/QuadPts_Gauss_Quadrel.hpp
+++ include/QuadPts_Gauss_Quadrel.hpp
@@ -1,32 +1,31 @@
-#ifndef QUADPTS_GAUSS_HEX_HPP
-#define QUADPTS_GAUSS_HEX_HPP
+#ifndef QUADPTS_GAUSS_QUADREL_HPP
+#define QUADPTS_GAUSS_QUADREL_HPP
 // ==================================================================
-// QuadPts_Gauss_Hex.hpp
-// The Gaussian quadrature rule for a Hexagon domain defined by 
-// eight vertex points:
-// [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 0.0],
-// [0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [1.0, 1.0, 1.0], [0.0, 1.0, 1.0],
+// QuadPts_Gauss_Quadrel.hpp
+// The Gaussian quadrature rule for a quadrelateral domain defined by 
+// vertices
+// [0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]
 //
-// Date Created: Sep 7 2023
+// Date Created: Sep. 7 2023
 // ==================================================================
 #include "Vec_Tools.hpp"
 #include "IQuadPts.hpp"
 
-class QuadPts_Gauss_Hex : public IQuadPts
+class QuadPts_Gauss_Quadrel : public IQuadPts
 {
   public:
-    QuadPts_Gauss_Hex( const int &in_num_pts );
-
-    virtual ~QuadPts_Gauss_Hex();
+    QuadPts_Gauss_Quadrel( const int &in_num_pts );
+    
+    virtual ~QuadPts_Gauss_Quadrel();
 
     virtual void print_info() const;
 
-    virtual int get_dim() const {return 3;}
+    virtual int get_dim() const {return 2;}
 
     virtual int get_num_quadPts() const {return num_pts;}
 
     virtual double get_qp(unsigned int ii, unsigned int comp) const
-    {return qp[3*ii+comp];}
+    {return qp[2*ii+comp];}
 
     virtual double get_qw(unsigned int ii) const
     {return qw[ii];}
@@ -34,7 +33,7 @@ class QuadPts_Gauss_Hex : public IQuadPts
   private:
     const int num_pts;
 
-    // qp : length 3 * num_pts. Stores the r-s-t coordinates of the 
+    // qp : length 2 x num_pts. Stores the r-s coordinates of the
     //      quadrature points.
     // qw : length num_pts. Stores the quadrature weights.
     std::vector<double> qp, qw;
