ceffd9ddd6015478e23bdd817839523a2b083059
SeaVegetable <1135167789@qq.com>
2023-09-14 17:31:53 +0800

    add some head files

---
+++ include/FEAElement_Quad9.hpp
@@ -0,0 +1,90 @@
+#ifndef FEAELEMENT_QUAD9_HPP
+#define FEAELEMENT_QUAD9_HPP
+// ==================================================================
+// FEAElement_Quad9.hpp
+// This is an implementation of the element routine for biquadratic
+// quadrilateral element in 2D.
+//
+// This class is designed mainly for the 2D FEM assembly.
+//
+// Date created: Sep. 2023
+// ==================================================================
+#include "FEAElement.hpp"
+#include "Matrix_double_3by3_Array.hpp"
+
+class FEAElement_Quad9 : public FEAElement
+{
+  public:
+    FEAElement_Quad9( const int &in_nqua );
+
+    virtual ~FEAElement_Quad9();
+
+    virtual int get_elemDim() const {return 2;}
+
+    virtual int get_Type() const {return 652;}
+
+    virtual int get_numQuapts() const {return numQuapts;}
+
+    virtual int get_nLocBas() const {return 9;}
+
+    virtual void print_info() const;
+
+    virtual double get_memory_usage() const;
+
+    virtual void buildBasis( const IQuadPts * const &quad_rule,
+        const double * const &ctrl_x,
+        const double * const &ctrl_y );
+
+    virtual double get_h( const double * const &ctrl_x,
+        const double * const &ctrl_y ) const;
+
+    virtual void get_R( const int &quaindex, double * const &basis ) const;
+
+    virtual std::vector<double> get_R( const int &quaindex ) const;
+
+    virtual void get_gradR( const int &quaindex, double * const &basis_x,
+        double * const &basis_y ) const;
+
+    virtual void get_R_gradR( const int &quaindex, double * const &basis,
+        double * const &basis_x, double * const &basis_y ) const;
+
+    virtual std::vector<double> get_dR_dx( const int &quaindex ) const;
+
+    virtual std::vector<double> get_dR_dy( const int &quaindex ) const;
+
+    virtual void get_2D_R_dR_d2R( const int &quaindex,
+        double * const &basis,
+        double * const &basis_x, double * const &basis_y,
+        double * const &basis_xx, double * const &basis_yy,
+        double * const &basis_xy ) const;
+
+    virtual std::vector<double> get_d2R_dxx( const int &quaindex ) const;
+
+    virtual std::vector<double> get_d2R_dyy( const int &quaindex ) const;
+
+    virtual std::vector<double> get_d2R_dxy( const int &quaindex ) const;
+
+    virtual void get_Jacobian(const int &quaindex,
+        double * const &jac_value) const;
+
+    virtual void get_invJacobian(const int &quaindex,
+        double * const &jac_value) const;
+
+    virtual double get_detJac(const int &quaindex) const
+    {return Jac[8*numQuapts + quaindex];}
+
+  private:
+    const int numQuapts;
+
+    // length 9 x numQuapts
+    double * R, * dR_dx, * dR_dy;
+    double * d2R_dxx, * d2R_dyy, * d2R_dxy;
+
+    // length 9 x numQuapts
+    // dx_ds : 0             <= ii < 4 * numQuapts
+    // ds_dx : 4 * numQuapts <= ii < 8 * numQuapts
+    // detJac: 8 * numQuapts <= ii < 9 * numQuapts
+    double * Jac;
+};
+
+#endif
---
+++ include/FEAElement_Quad9_3D_der0.hpp
@@ -0,0 +1,91 @@
+#ifndef FEAELEMENT_QUAD9_3D_DER0_HPP
+#define FEAELEMENT_QUAD9_3D_DER0_HPP
+// ==================================================================
+// FEAElement_Quad9_3D_der0.hpp
+// Element routine for the biquadratic quadrilateral element in 
+// three-dimensional space, with evaluation of the basis functions
+// only (no derivatives).
+//
+//     s
+//     |
+//     |
+//     3--------6---------2
+//     |                  |
+//     |                  |
+//     |                  |
+//     7        8         5
+//     |                  |
+//     |                  |
+//     |                  |
+//     0--------4-------- 1 -- r
+//
+//
+// Quad4 means 9-node quad; _3D means the element has
+// coordinates in three-dimensions; _der0 means that only the function
+// itself is evaluated.
+//
+// This class is designed for boundary integrations for elemental/
+// natural boundary conditions in 3D problems.
+//
+// Author: Ju Liu
+// Date Created: Sep. 12 2023.
+// ==================================================================
+#include "FEAElement.hpp"
+#include "Math_Tools.hpp"
+
+class FEAElement_Quad9_3D_der0 : public FEAElement
+{
+  public:
+    FEAElement_Quad9_3D_der0( const int &in_nqua );
+
+    virtual ~FEAElement_Quad9_3D_der0();
+
+    virtual int get_elemDim() const {return 2;}
+
+    virtual int get_Type() const {return 622;}
+
+    virtual int get_numQuapts() const {return numQuapts;}
+
+    virtual int get_nLocBas() const {return 9;}
+
+    virtual void print_info() const;
+
+    virtual double get_memory_usage() const;
+
+    virtual void buildBasis( const IQuadPts * const &quad_rule,
+        const double * const &ctrl_x,
+        const double * const &ctrl_y,
+        const double * const &ctrl_z );
+
+    virtual void get_R( const int &quaindex, double * const &basis ) const;
+
+    virtual std::vector<double> get_R( const int &quaindex ) const;
+
+    // Assuming the quad nodes are arranged such that the outward
+    // direction is given by dx_dr x dx_ds
+    virtual Vector_3 get_2d_normal_out( const int &quaindex, double &area ) const;
+    
+    // If the quad nodes are NOT arranged in any particular order,
+    // use an interior node to define the outward direction. 
+    virtual void get_normal_out( const int &quaindex,
+        const double &sur_pt_x, const double &sur_pt_y, const double &sur_pt_z,
+        const double &intpt_x, const double &intpt_y, const double &intpt_z,
+        double &nx, double &ny, double &nz, double &len ) const;
+
+    virtual double get_detJac(const int &quaindex) const {return detJac[quaindex];}
+
+  private:
+    const int numQuapts;
+
+    // Container for R
+    // 0 <= ii < 9 x numQuapts
+    double * R;
+
+    // unit normal vector components, each of length numQuapts
+    double * unx, * uny, * unz;
+
+    // Jacobian determinant, length numQuapts
+    double * detJac; 
+};
+
+#endif
