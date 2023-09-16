434257b6cf63f684b1fb501e405963fbf978f23c
ZhaoChongran <imchongran@gmail.com>
2023-09-16 13:13:45 +0800

    add -gmsh_file and -num_outlet

--- tools/gmsh_to_vtk/gmsh2vtk.cpp
+++ tools/gmsh_to_vtk/gmsh2vtk.cpp
@@ -9,6 +9,16 @@
 int main( int argc, char * argv[] )
 {
   std::string gmshFile = "cylinder.msh";
+  int num_outlet = 1;
+
+  for(int ii=1; ii<argc; ++ii)
+  {
+    if( std::string(argv[ii])=="-gmsh_file"  && ii+1<argc) gmshFile=argv[ii+1];
+    if( std::string(argv[ii])=="-num_outlet" && ii+1<argc) num_outlet=std::stoi(argv[ii+1]);   
+  }
+
+  std::cout<<" -gmsh_file: "<<gmshFile<<std::endl;
+  std::cout<<" -num_outlet: "<<num_outlet<<std::endl;
 
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
