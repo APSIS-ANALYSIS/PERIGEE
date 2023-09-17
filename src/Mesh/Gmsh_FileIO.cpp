f2a513db8a1f22878aae624a7a8caa38f4c5dff3
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2023-09-17 16:03:36 +0800

    Minor

--- src/Mesh/Gmsh_FileIO.cpp
+++ src/Mesh/Gmsh_FileIO.cpp
@@ -422,13 +422,13 @@ void Gmsh_FileIO::write_vtp( const std::string &vtp_filename,
   std::string ele_3d {};
   if (nlocbas_2d == 3 && nlocbas_3d == 4)
   {
-    ele_2d += static_cast<std::string>("triangle");
-    ele_3d += static_cast<std::string>("tetrahedron");
+    ele_2d = static_cast<std::string>("triangle");
+    ele_3d = static_cast<std::string>("tetrahedron");
   }
   else if (nlocbas_2d == 4 && nlocbas_3d == 8)
   {
-    ele_2d += static_cast<std::string>("quadrilateral");
-    ele_3d += static_cast<std::string>("hexahedron");
+    ele_2d = static_cast<std::string>("quadrilateral");
+    ele_3d = static_cast<std::string>("hexahedron");
   }
   else
     SYS_T::print_exit("Error: Gmsh_FileIO::write_vtp, element types of surface and volume donnot match. \n");
@@ -1395,15 +1395,15 @@ void Gmsh_FileIO::write_quadratic_sur_vtu( const std::string &vtu_filename,
   { 
     nVertex_2d = 3;
     nVertex_3d = 4;
-    ele_2d += static_cast<std::string>("triangle");
-    ele_3d += static_cast<std::string>("tetrahedron");
+    ele_2d = static_cast<std::string>("triangle");
+    ele_3d = static_cast<std::string>("tetrahedron");
   }
   else if (nlocbas_2d == 9 && nlocbas_3d == 27)
   {
     nVertex_2d = 4;
     nVertex_3d = 8;
-    ele_2d += static_cast<std::string>("quadrilateral");
-    ele_3d += static_cast<std::string>("hexahedron");
+    ele_2d = static_cast<std::string>("quadrilateral");
+    ele_3d = static_cast<std::string>("hexahedron");
   }
   else
     SYS_T::print_exit("Error: Gmsh_FileIO::write_quadratic_sur_vtu, element types of surface and volume donnot match. \n");
