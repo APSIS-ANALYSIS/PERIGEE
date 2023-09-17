c1dedd6e564f63e661db7fdb2f6f362a4d4c615c
SUNYujie <12231157@mail.sustech.edu.cn>
2023-09-17 17:26:22 +0800

    error fixed in get_face_id

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -24,11 +24,13 @@
 
 int main(int argc, char *argv[])
 {
-  Gmsh_FileIO gfio( "two_cube.msh" );
 
-  gfio.print_info();
+  HEX_T::Hex8 * test_hex = new HEX_T::Hex8();  
+  test_hex -> reset(8, 10, 1, 6, 21, 22, 5, 7);
 
-  gfio.write_interior_vtp(2, 0, 1);
+  std::cout << "face_id:" << test_hex.get_face_id(10, 1, 22, 5) << std::endl;
+  std::cout << "face_id:" << test_hex.get_face_id(22, 10, 1, 5) << std::endl;
+  std::cout << "face_id:" << test_hex.get_face_id(23, 10, 1, 5) << std::endl;
 
   return EXIT_SUCCESS;
 }
