aed2a203a83953925de750611d0be245d1e86000
Liu, Ju <liujuy@gmail.com>
2023-09-19 22:14:18 +0800

    renamed matrix_2x2 to tensor2_2d

--- src/System/Tensor2_2D.cpp
+++ src/System/Tensor2_2D.cpp
@@ -1,27 +1,27 @@
-#include "Matrix_2x2.hpp"
+#include "Tensor2_2D.hpp"
 
-Matrix_2x2::Matrix_2x2()
+Tensor2_2D::Tensor2_2D()
 {
   mat[0] = 1.0; mat[1] = 0.0; mat[2] = 0.0; mat[3] = 1.0;
 }
 
-Matrix_2x2::Matrix_2x2( const Matrix_2x2 &source )
+Tensor2_2D::Tensor2_2D( const Tensor2_2D &source )
 {
   mat[0] = source(0); mat[1] = source(1);
   mat[2] = source(2); mat[3] = source(3);
 }
 
-Matrix_2x2::Matrix_2x2( const double &a11, const double &a12,
+Tensor2_2D::Tensor2_2D( const double &a11, const double &a12,
     const double &a21, const double &a22 )
 {
   mat[0] = a11; mat[1] = a12;
   mat[2] = a21; mat[3] = a22;
 }
 
-Matrix_2x2::~Matrix_2x2()
+Tensor2_2D::~Tensor2_2D()
 {}
 
-void Matrix_2x2::copy( const Matrix_2x2 &source )
+void Tensor2_2D::copy( const Tensor2_2D &source )
 {
   mat[0] = source(0);
   mat[1] = source(1);
@@ -29,7 +29,7 @@ void Matrix_2x2::copy( const Matrix_2x2 &source )
   mat[3] = source(3);
 }
 
-void Matrix_2x2::copy( double source[4] )
+void Tensor2_2D::copy( double source[4] )
 {
   mat[0] = source[0];
   mat[1] = source[1];
@@ -37,17 +37,17 @@ void Matrix_2x2::copy( double source[4] )
   mat[3] = source[3];
 }
 
-void Matrix_2x2::gen_zero()
+void Tensor2_2D::gen_zero()
 {
   mat[0] = 0.0; mat[1] = 0.0; mat[2] = 0.0; mat[3] = 0.0;
 }
 
-void Matrix_2x2::gen_id()
+void Tensor2_2D::gen_id()
 {
   mat[0] = 1.0; mat[1] = 0.0; mat[2] = 0.0; mat[3] = 1.0;
 }
 
-void Matrix_2x2::gen_rand(const double &min, const double &max)
+void Tensor2_2D::gen_rand(const double &min, const double &max)
 {
   std::random_device rd;
   std::mt19937_64 gen( rd() );
@@ -55,19 +55,19 @@ void Matrix_2x2::gen_rand(const double &min, const double &max)
   for(int ii=0; ii<4; ++ii) mat[ii] = dis(gen);
 }
 
-void Matrix_2x2::gen_hilb()
+void Tensor2_2D::gen_hilb()
 {
   for(int ii=0; ii<2; ++ii)
     for(int jj=0; jj<2; ++jj)
       mat[ii*2+jj] = 1.0 / (ii + jj + 1.0);
 }
 
-void Matrix_2x2::transpose()
+void Tensor2_2D::transpose()
 {
   double temp = mat[1]; mat[1] = mat[2]; mat[2] = temp;
 }
 
-void Matrix_2x2::inverse()
+void Tensor2_2D::inverse()
 {
   const double invdetA = 1.0 / det();
   double temp[4];
@@ -79,39 +79,39 @@ void Matrix_2x2::inverse()
   mat[0] = temp[0]; mat[1] = temp[1]; mat[2] = temp[2]; mat[3] = temp[3];
 }
 
-void Matrix_2x2::scale( const double &val )
+void Tensor2_2D::scale( const double &val )
 {
   for(int ii=0; ii<4; ++ii) mat[ii] *= val;
 }
 
-void Matrix_2x2::AXPY( const double &val, const Matrix_2x2 &source )
+void Tensor2_2D::AXPY( const double &val, const Tensor2_2D &source )
 {
   for(int ii=0; ii<4; ++ii) mat[ii] += val * source(ii);
 }
 
-void Matrix_2x2::PY( const Matrix_2x2 &source )
+void Tensor2_2D::PY( const Tensor2_2D &source )
 {
   for(int ii=0; ii<4; ++ii) mat[ii] += source(ii);
 }
 
-double Matrix_2x2::det() const
+double Tensor2_2D::det() const
 {
   return mat[0] * mat[3] - mat[1] * mat[2];
 }
 
-double Matrix_2x2:: VecMatVec( const double * const &x,
+double Tensor2_2D:: VecMatVec( const double * const &x,
     const double * const &y ) const
 {
   return x[0] * (mat[0]*y[0] + mat[1]*y[1]) + x[1] * (mat[2]*y[0] + mat[3]*y[1]);
 }
 
-void Matrix_2x2::VecMult( const double * const &x, double * const &y ) const
+void Tensor2_2D::VecMult( const double * const &x, double * const &y ) const
 {
   y[0] = mat[0] * x[0] + mat[1] * x[1];
   y[1] = mat[2] * x[0] + mat[3] * x[1];
 }
 
-void Matrix_2x2::VecMult( double * const &x ) const
+void Tensor2_2D::VecMult( double * const &x ) const
 {
   double a, b;
   a = mat[0] * x[0] + mat[1] * x[1];
@@ -120,7 +120,7 @@ void Matrix_2x2::VecMult( double * const &x ) const
   x[1] = b;
 }
 
-void Matrix_2x2::MatMult( const Matrix_2x2 &mleft, const Matrix_2x2 &mright )
+void Tensor2_2D::MatMult( const Tensor2_2D &mleft, const Tensor2_2D &mright )
 {
   double temp[4];
   temp[0] = mleft(0) * mright(0) + mleft(1) * mright(2);
@@ -132,7 +132,7 @@ void Matrix_2x2::MatMult( const Matrix_2x2 &mleft, const Matrix_2x2 &mright )
   mat[2] = temp[2]; mat[3] = temp[3];
 }
 
-void Matrix_2x2::MatMultTransposeLeft( const Matrix_2x2 &source )
+void Tensor2_2D::MatMultTransposeLeft( const Tensor2_2D &source )
 {
   double temp[4];
   temp[0] = source(0) * source(0) + source(2) * source(2);
@@ -144,7 +144,7 @@ void Matrix_2x2::MatMultTransposeLeft( const Matrix_2x2 &source )
   mat[2] = temp[2]; mat[3] = temp[3];
 }
 
-void Matrix_2x2::MatMultTransposeRight( const Matrix_2x2 &source )
+void Tensor2_2D::MatMultTransposeRight( const Tensor2_2D &source )
 {
   double temp[4];
   temp[0] = source(0) * source(0) + source(1) * source(1);
@@ -156,12 +156,12 @@ void Matrix_2x2::MatMultTransposeRight( const Matrix_2x2 &source )
   mat[2] = temp[2]; mat[3] = temp[3];
 }
 
-double Matrix_2x2::MatContraction( const Matrix_2x2 &source ) const
+double Tensor2_2D::MatContraction( const Tensor2_2D &source ) const
 {
   return mat[0] * source(0) + mat[1] * source(1) + mat[2] * source(2) + mat[3] * source(3);
 }
 
-void Matrix_2x2::print() const
+void Tensor2_2D::print() const
 {
   std::cout<<std::setprecision(9)<<mat[0]<<'\t'<<mat[1]<<'\n';
   std::cout<<std::setprecision(9)<<mat[2]<<'\t'<<mat[3]<<'\n';
