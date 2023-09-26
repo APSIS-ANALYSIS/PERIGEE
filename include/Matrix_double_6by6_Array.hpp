547442d974b5fa081331486472cf9b1288078d75
ZhaoChongran <imchongran@gmail.com>
2023-09-26 17:12:26 +0800

    modify the name of var in Matrix_double_6by6_Array.hpp

--- include/Matrix_double_6by6_Array.hpp
+++ include/Matrix_double_6by6_Array.hpp
@@ -17,10 +17,10 @@ class Matrix_double_6by6_Array
   public:
     // This constructor is designed for the special structure when
     // solving for 2nd order derivatives.
-    Matrix_double_6by6_Array(const double &a, const double &b,
-        const double &c, const double &d, const double &e,
-        const double &f, const double &g, const double &h,
-        const double &i);
+    Matrix_double_6by6_Array(const double &aa, const double &bb,
+        const double &cc, const double &dd, const double &ee,
+        const double &ff, const double &gg, const double &hh,
+        const double &ii);
     
     ~Matrix_double_6by6_Array();
 
@@ -32,8 +32,7 @@ class Matrix_double_6by6_Array
 
   private:
     double Mat[6][6];
-    int p[6];
-    double invm0, invm1, invm2, invm3, invm4, invm5;
+    int pp[6];
 };
 
 #endif
