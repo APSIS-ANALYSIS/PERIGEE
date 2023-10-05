f30afe6f42225d7070c47ccc7a5ef111b89f779e
SUNYujie <12231157@mail.sustech.edu.cn>
2023-10-05 10:12:33 +0800

    declare var for 502,601,602 in ElemBC_3D_outflow

--- src/Mesh/ElemBC_3D_outflow.cpp
+++ src/Mesh/ElemBC_3D_outflow.cpp
@@ -33,16 +33,16 @@ ElemBC_3D_outflow::ElemBC_3D_outflow(
         const int node_idx[3] { get_ien(fid, ee, 0), get_ien(fid, ee, 1), get_ien(fid, ee, 2) };
 
         const double ectrl_x[3] { get_pt_xyz(fid, node_idx[0], 0),
-          get_pt_xyz(fid, node_idx[1], 0),
-          get_pt_xyz(fid, node_idx[2], 0) };
+                                  get_pt_xyz(fid, node_idx[1], 0),
+                                  get_pt_xyz(fid, node_idx[2], 0) };
 
         const double ectrl_y[3] { get_pt_xyz(fid, node_idx[0], 1),
-          get_pt_xyz(fid, node_idx[1], 1),
-          get_pt_xyz(fid, node_idx[2], 1) };
+                                  get_pt_xyz(fid, node_idx[1], 1),
+                                  get_pt_xyz(fid, node_idx[2], 1) };
 
         const double ectrl_z[3] { get_pt_xyz(fid, node_idx[0], 2),
-          get_pt_xyz(fid, node_idx[1], 2),
-          get_pt_xyz(fid, node_idx[2], 2) };
+                                  get_pt_xyz(fid, node_idx[1], 2),
+                                  get_pt_xyz(fid, node_idx[2], 2) };
 
         elems -> buildBasis(quads, ectrl_x, ectrl_y, ectrl_z);
 
@@ -67,26 +67,40 @@ ElemBC_3D_outflow::ElemBC_3D_outflow(
 
     IQuadPts * quads = new QuadPts_Gauss_Triangle( nqp_tri );
     FEAElement * elems = new FEAElement_Triangle6_3D_der0( nqp_tri );
-  
-    double ectrl_x[6]{}; double ectrl_y[6]{}; double ectrl_z[6]{};
-    int node_idx[6]{}; double R[6]{};
     
     for( int fid=0; fid<num_ebc; ++fid )
     {
       for( int ee=0; ee<num_cell[fid]; ++ee )
       {
-        for(int ii=0; ii<6; ++ii)
-        {
-          node_idx[ii] = get_ien(fid, ee, ii);
-          ectrl_x[ii] = get_pt_xyz(fid, node_idx[ii], 0);
-          ectrl_y[ii] = get_pt_xyz(fid, node_idx[ii], 1);
-          ectrl_z[ii] = get_pt_xyz(fid, node_idx[ii], 2);
-        }
+        const int node_idx[6] { get_ien(fid, ee, 0), get_ien(fid, ee, 1), get_ien(fid, ee, 2), 
+                                get_ien(fid, ee, 3), get_ien(fid, ee, 4), get_ien(fid, ee, 5) }; 
 
+        const double ectrl_x[6] { get_pt_xyz(fid, node_idx[0], 0),
+                                  get_pt_xyz(fid, node_idx[1], 0),
+                                  get_pt_xyz(fid, node_idx[2], 0), 
+                                  get_pt_xyz(fid, node_idx[3], 0), 
+                                  get_pt_xyz(fid, node_idx[4], 0), 
+                                  get_pt_xyz(fid, node_idx[5], 0) };
+
+        const double ectrl_y[6] { get_pt_xyz(fid, node_idx[0], 1),
+                                  get_pt_xyz(fid, node_idx[1], 1),
+                                  get_pt_xyz(fid, node_idx[2], 1), 
+                                  get_pt_xyz(fid, node_idx[3], 1), 
+                                  get_pt_xyz(fid, node_idx[4], 1), 
+                                  get_pt_xyz(fid, node_idx[5], 1) };
+
+        const double ectrl_z[6] { get_pt_xyz(fid, node_idx[0], 2),
+                                  get_pt_xyz(fid, node_idx[1], 2),
+                                  get_pt_xyz(fid, node_idx[2], 2), 
+                                  get_pt_xyz(fid, node_idx[3], 2), 
+                                  get_pt_xyz(fid, node_idx[4], 2), 
+                                  get_pt_xyz(fid, node_idx[5], 2) };
+           
         elems -> buildBasis(quads, ectrl_x, ectrl_y, ectrl_z);
 
         for(int qua=0; qua<nqp_tri; ++qua)
         {
+          double R[6]{};
           elems -> get_R(qua, R);
 
           const double gwts = elems -> get_detJac(qua) * quads -> get_qw( qua );
@@ -105,37 +119,33 @@ ElemBC_3D_outflow::ElemBC_3D_outflow(
     IQuadPts * quads = new QuadPts_Gauss_Quad( nqp_quad );
     FEAElement * elems = new FEAElement_Quad4_3D_der0( nqp_quad * nqp_quad );
 
-    double ectrl_x[4]{}; double ectrl_y[4]{}; double ectrl_z[4]{};
-    int node_idx[4]{}; double R[4]{};
-
     for( int fid=0; fid<num_ebc; ++fid )
     {
       for( int ee=0; ee<num_cell[fid]; ++ee )
       {
-        node_idx[0] = get_ien(fid, ee, 0);
-        node_idx[1] = get_ien(fid, ee, 1);
-        node_idx[2] = get_ien(fid, ee, 2);
-        node_idx[3] = get_ien(fid, ee, 3);
+        const int node_idx[4] { get_ien(fid, ee, 0), get_ien(fid, ee, 1),
+                                get_ien(fid, ee, 2), get_ien(fid, ee, 3) };
 
-        ectrl_x[0] = get_pt_xyz(fid, node_idx[0], 0);
-        ectrl_x[1] = get_pt_xyz(fid, node_idx[1], 0);
-        ectrl_x[2] = get_pt_xyz(fid, node_idx[2], 0);
-        ectrl_x[3] = get_pt_xyz(fid, node_idx[3], 0);
+        const double ectrl_x[4] { get_pt_xyz(fid, node_idx[0], 0),
+                                  get_pt_xyz(fid, node_idx[1], 0), 
+                                  get_pt_xyz(fid, node_idx[2], 0),
+                                  get_pt_xyz(fid, node_idx[3], 0) };                    
 
-        ectrl_y[0] = get_pt_xyz(fid, node_idx[0], 1);
-        ectrl_y[1] = get_pt_xyz(fid, node_idx[1], 1);
-        ectrl_y[2] = get_pt_xyz(fid, node_idx[2], 1);
-        ectrl_y[3] = get_pt_xyz(fid, node_idx[3], 1);
+        const double ectrl_y[4] { get_pt_xyz(fid, node_idx[0], 1),
+                                  get_pt_xyz(fid, node_idx[1], 1), 
+                                  get_pt_xyz(fid, node_idx[2], 1),
+                                  get_pt_xyz(fid, node_idx[3], 1) };     
 
-        ectrl_z[0] = get_pt_xyz(fid, node_idx[0], 2);
-        ectrl_z[1] = get_pt_xyz(fid, node_idx[1], 2);
-        ectrl_z[2] = get_pt_xyz(fid, node_idx[2], 2);
-        ectrl_z[3] = get_pt_xyz(fid, node_idx[3], 2);
+        const double ectrl_z[4] { get_pt_xyz(fid, node_idx[0], 2),
+                                  get_pt_xyz(fid, node_idx[1], 2), 
+                                  get_pt_xyz(fid, node_idx[2], 2),
+                                  get_pt_xyz(fid, node_idx[3], 2) };  
 
         elems -> buildBasis(quads, ectrl_x, ectrl_y, ectrl_z);
 
         for(int qua=0; qua<nqp_quad * nqp_quad; ++qua)
         {
+          double R[4]{};
           elems -> get_R(qua, R);
 
           const double gwts = elems -> get_detJac( qua ) * quads -> get_qw( qua );
@@ -155,26 +165,50 @@ ElemBC_3D_outflow::ElemBC_3D_outflow(
 
     IQuadPts * quads = new QuadPts_Gauss_Quad( nqp_quad );
     FEAElement * elems = new FEAElement_Quad9_3D_der0( nqp_quad * nqp_quad );
-  
-    double ectrl_x[9]{}; double ectrl_y[9]{}; double ectrl_z[9]{};
-    int node_idx[9]{}; double R[9]{};  
     
     for( int fid=0; fid<num_ebc; ++fid )
     {
       for( int ee=0; ee<num_cell[fid]; ++ee )
       {
-        for(int ii=0; ii<9; ++ii)
-        {
-          node_idx[ii] = get_ien(fid, ee, ii);
-          ectrl_x[ii] = get_pt_xyz(fid, node_idx[ii], 0);
-          ectrl_y[ii] = get_pt_xyz(fid, node_idx[ii], 1);
-          ectrl_z[ii] = get_pt_xyz(fid, node_idx[ii], 2);
-        }
+        const int node_idx[9] { get_ien(fid, ee, 0), get_ien(fid, ee, 1), get_ien(fid, ee, 2), 
+                                get_ien(fid, ee, 3), get_ien(fid, ee, 4), get_ien(fid, ee, 5),
+                                get_ien(fid, ee, 6), get_ien(fid, ee, 7), get_ien(fid, ee, 8) }; 
+
+        const double ectrl_x[9] { get_pt_xyz(fid, node_idx[0], 0),
+                                  get_pt_xyz(fid, node_idx[1], 0),
+                                  get_pt_xyz(fid, node_idx[2], 0), 
+                                  get_pt_xyz(fid, node_idx[3], 0), 
+                                  get_pt_xyz(fid, node_idx[4], 0), 
+                                  get_pt_xyz(fid, node_idx[5], 0), 
+                                  get_pt_xyz(fid, node_idx[6], 0), 
+                                  get_pt_xyz(fid, node_idx[7], 0), 
+                                  get_pt_xyz(fid, node_idx[8], 0) };
+
+        const double ectrl_y[9] { get_pt_xyz(fid, node_idx[0], 1),
+                                  get_pt_xyz(fid, node_idx[1], 1),
+                                  get_pt_xyz(fid, node_idx[2], 1), 
+                                  get_pt_xyz(fid, node_idx[3], 1), 
+                                  get_pt_xyz(fid, node_idx[4], 1), 
+                                  get_pt_xyz(fid, node_idx[5], 1), 
+                                  get_pt_xyz(fid, node_idx[6], 1), 
+                                  get_pt_xyz(fid, node_idx[7], 1), 
+                                  get_pt_xyz(fid, node_idx[8], 1) };
+
+        const double ectrl_z[9] { get_pt_xyz(fid, node_idx[0], 2),
+                                  get_pt_xyz(fid, node_idx[1], 2),
+                                  get_pt_xyz(fid, node_idx[2], 2), 
+                                  get_pt_xyz(fid, node_idx[3], 2), 
+                                  get_pt_xyz(fid, node_idx[4], 2), 
+                                  get_pt_xyz(fid, node_idx[5], 2), 
+                                  get_pt_xyz(fid, node_idx[6], 2), 
+                                  get_pt_xyz(fid, node_idx[7], 2), 
+                                  get_pt_xyz(fid, node_idx[8], 2) };
 
         elems -> buildBasis(quads, ectrl_x, ectrl_y, ectrl_z);
 
         for(int qua=0; qua<nqp_quad * nqp_quad; ++qua)
         {
+          double R[9]{};
           elems -> get_R(qua, R);
 
           const double gwts = elems -> get_detJac(qua) * quads -> get_qw( qua );
