fc1121f5b3eb87a6f9737012b283cde3b25f2dfc
Ju Liu <liujuy@gmail.com>
2023-10-06 08:55:45 +0800

    JL minor comments

--- tools/gmsh_to_vtk/gmsh2vtk.cpp
+++ tools/gmsh_to_vtk/gmsh2vtk.cpp
@@ -5,10 +5,11 @@
 // into vtk files.
 // ============================================================================
 #include "Gmsh_FileIO.hpp"
-#include <yaml-cpp/yaml.h>
+#include "yaml-cpp/yaml.h"
 
 int main( int argc, char * argv[] )
 {
+  // WE DO NOT NEED THESE
   std::string gmshFile = "cylinder.msh";
   int num_outlet = 1;
 
@@ -20,10 +21,13 @@ int main( int argc, char * argv[] )
 
   std::cout<<" -gmsh_file: "<<gmshFile<<std::endl;
   std::cout<<" -num_outlet: "<<num_outlet<<std::endl;
+  // ==============
 
   Gmsh_FileIO * GIO = new Gmsh_FileIO( gmshFile );
 
   GIO -> print_info();
+
+  // Get element type from GIO
 
   YAML::Node config = YAML::LoadFile("example.yml");
 
@@ -35,12 +39,19 @@ int main( int argc, char * argv[] )
   std::vector<std::string> ebc_face_name = config["ebc_face_name"].as<std::vector<std::string>>();
   std::vector<int> ebc_vol_id = config["ebc_vol_id"].as<std::vector<int>>();
 
+  // if element type is linear tet or hex
+  // NEED TO ADD FILENAME TO BE WRITTEN
   for( int ii=0; ii<nbc_face_id.size(); ++ii )
     GIO -> write_vtp( nbc_face_id[ii], nbc_vol_id[ii], false );
 
   for( int ii=0; ii<ebc_face_id.size(); ++ii )
     GIO -> write_vtp( ebc_face_id[ii], ebc_vol_id[ii], true );
 
+  // else if element type is quadratic tet or quadratic hex
+  // ... update node numbering and write into vtu (not vtp)
+  // else print message
+
+  // PASS THE TWO PARAMETERS FROM YAML FILE AS WELL
   const std::string wmname("whole_vol");
   const bool isXML = true;
   GIO -> write_vtu( wmname, isXML );
