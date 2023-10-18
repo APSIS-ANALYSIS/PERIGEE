b41fc3764268fa382edcda8b4a222d43fb6d79ec
Ju Liu <liujuy@gmail.com>
2023-10-18 10:11:35 +0800

    ux_t uy_t uz_t are not needed

--- examples/fsi/src/PLocAssem_2x2Block_Tet4_VMS_Incompressible.cpp
+++ examples/fsi/src/PLocAssem_2x2Block_Tet4_VMS_Incompressible.cpp
@@ -120,7 +120,6 @@ void PLocAssem_2x2Block_Tet4_VMS_Incompressible::Assem_Residual(
   {
     double p = 0.0, p_x = 0.0, p_y = 0.0, p_z = 0.0;
 
-    double ux_t = 0.0, uy_t = 0.0, uz_t = 0.0;
     double vx   = 0.0, vy   = 0.0, vz   = 0.0;
     double vx_t = 0.0, vy_t = 0.0, vz_t = 0.0;
 
@@ -145,9 +144,6 @@ void PLocAssem_2x2Block_Tet4_VMS_Incompressible::Assem_Residual(
       p_y += pres[ii] * dR_dy[ii];
       p_z += pres[ii] * dR_dz[ii];
 
-      ux_t += dot_disp[ii*3  ] * R[ii];
-      uy_t += dot_disp[ii*3+1] * R[ii];
-      uz_t += dot_disp[ii*3+2] * R[ii];
       vx_t += dot_velo[ii*3  ] * R[ii];
       vy_t += dot_velo[ii*3+1] * R[ii];
       vz_t += dot_velo[ii*3+2] * R[ii];
@@ -286,7 +282,6 @@ void PLocAssem_2x2Block_Tet4_VMS_Incompressible::Assem_Tangent_Residual(
   {
     double p = 0.0, p_x = 0.0, p_y = 0.0, p_z = 0.0;
 
-    double ux_t = 0.0, uy_t = 0.0, uz_t = 0.0;
     double vx   = 0.0, vy   = 0.0, vz   = 0.0;
     double vx_t = 0.0, vy_t = 0.0, vz_t = 0.0;
 
@@ -311,9 +306,6 @@ void PLocAssem_2x2Block_Tet4_VMS_Incompressible::Assem_Tangent_Residual(
       p_y += pres[ii] * dR_dy[ii];
       p_z += pres[ii] * dR_dz[ii];
 
-      ux_t += dot_disp[ii*3  ] * R[ii];
-      uy_t += dot_disp[ii*3+1] * R[ii];
-      uz_t += dot_disp[ii*3+2] * R[ii];
       vx_t += dot_velo[ii*3  ] * R[ii];
       vy_t += dot_velo[ii*3+1] * R[ii];
       vz_t += dot_velo[ii*3+2] * R[ii];
