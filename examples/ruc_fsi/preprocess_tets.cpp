2b3af5796b12f6b1284f4cbca8d470a57d54f1ee
ZhaoChongran <imchongran@gmail.com>
2023-10-18 17:18:24 +0800

    update ruc_fsi/CMakeLists.txt and ruc_fsi/preprocess_tets.cpp

--- examples/ruc_fsi/preprocess_tets.cpp
+++ examples/ruc_fsi/preprocess_tets.cpp
@@ -30,6 +30,12 @@
 
 int main( int argc, char * argv[] )
 {
+  // Set number of threads and  print info of OpenMP
+#ifdef _OPENMP
+  omp_set_num_threads( omp_get_num_procs() );
+  SYS_T::print_omp_info();
+#endif
+
   // Clean the potentially pre-existing hdf5 files in the job folder
   SYS_T::execute("rm -rf preprocessor_cmd.h5");
   SYS_T::execute("rm -rf apart"); 
