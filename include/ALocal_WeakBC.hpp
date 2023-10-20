71d09192e2211e407e179118fb9ccaaa09db8391
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2023-10-20 13:08:06 +0800

    ALocal_WeakBC need redesign

--- include/ALocal_WeakBC.hpp
+++ include/ALocal_WeakBC.hpp
@@ -21,15 +21,6 @@ class ALocal_WeakBC
     // Get the type of weak enforced Dirichlet BC
     virtual int get_weakbc_type() const { return weakbc_type; }
 
-    // Get the global index of a Dirichlet node in the local partition
-    // para node ranges [ 0, num_LD )
-    virtual int get_LDN( const int &node ) const { return LDN[node]; }
-
-    // Get the number of Dirichlet nodes in the local partition
-    virtual int get_num_LD() const { return num_LD; }
-
-    // Get the number of surface cells in the local partition
-    virtual int get_num_local_cell() const { return num_local_cell; }
 
 
   protected:
@@ -40,28 +31,6 @@ class ALocal_WeakBC
     //           of wall nodes are set to be weakly essential BC.       
     int weakbc_type;
 
-    // Number of local Dirichlet nodes of the weak BC type
-    int num_LD;
-
-    // Number of local cell
-    int num_local_cell;
-
-    // Number of nodes in a cell
-    int cell_nLocBas;
-
-    // Local cell's IEN array
-    std::vector<int> local_cell_ien {};
-
-    // Local cell coordinates, the length is 3 x num_LD
-    std::vector<double> LD_xyz {};
-
-    // Store the global indices of the local Dirichlet nodes of the weak BC type,
-    // the length is num_LD
-    std::vector<int> LDN {};
-
-    // If weakbc_type = 2, store the rotation matrices at nodes,
-    // the length is num_LD
-    std::vector<Tensor2_3D> Q {};
 
     // ------------------------------------------------------------------------
     // Disallow default constructor
--- src/Analysis_Tool/ALocal_WeakBC.cpp
+++ src/Analysis_Tool/ALocal_WeakBC.cpp
@@ -14,30 +14,12 @@ ALocal_WeakBC::ALocal_WeakBC( const std::string &fileBaseName, const int &cpu_ra
 
   if (weakbc_type > 0)
   {
-    num_LD = h5r -> read_intScalar( gname.c_str(), "num_LD_weak" ); // dependent from NBC
-
-    num_local_cell = h5r -> read_intScalar( gname.c_str(), "num_local_cell_weak" ); // dependent from EBC
-
-    cell_nLocBas = h5r -> read_intScalar( gname.c_str(), "cell_nLocBas" );  // shared with EBC
-
-    local_cell_ien = h5r -> read_intVector( gname.c_str(), "local_cell_ien_weak" ); // dependent from EBC
-
-    LDN = h5r -> read_intVector( gname.c_str(), "LDN_weak" ); // dependent from NBC
-
-    LD_xyz = h5r -> read_doubleVector( gname.c_str(), "LD_xyz");  // dependent from EBC
+    
   }
 
   if(weakbc_type == 2)
   {
-    const std::vector<double> Q_vec = h5r -> read_doubleVector( gname.c_str(), "wall_rotation_matrix" );  // dependent from ringBC
 
-    Q.resize( num_LD );
-    for(int ii{0}; ii < num_LD; ++ii)
-    {
-      Q[ii] = Tensor2_3D( Q_vec[9*ii+0], Q_vec[9*ii+1], Q_vec[9*ii+2],
-                          Q_vec[9*ii+3], Q_vec[9*ii+4], Q_vec[9*ii+5],
-                          Q_vec[9*ii+6], Q_vec[9*ii+7], Q_vec[9*ii+8]  );
-    }
   }
 
 }
