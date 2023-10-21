5c967376e989eea6004ee602f56565e6762afd31
Goldeninscriptions <12232444@mail.sustech.edu.cn>
2023-10-21 21:01:17 +0800

    EBC_Partition_weak corrected mistake, added write_hdf5

--- include/EBC_Partition_weak.hpp
+++ include/EBC_Partition_weak.hpp
@@ -35,6 +35,9 @@ class EBC_Partition_weak : public EBC_Partition
     // stores the face id of the volume element
     std::vector< std::vector<int> > ele_face_id;
 
+    // stores the rotation matrix
+    std::vector< std::vector<double> > rot_mat;
+
 };
 
 #endif
--- src/Mesh/EBC_Partition_weak.cpp
+++ src/Mesh/EBC_Partition_weak.cpp
@@ -12,15 +12,19 @@ weak_bc_type {ebc->get_weak_bc_type()}, C_bI {ebc->get_C_bI()}
 
     vol_ele_id.resize(num_ebc);
     ele_face_id.resize(num_ebc);
+    if(weak_bc_type == 2)
+      rot_mat.resize(num_ebc);
+
     for(int ii{0}; ii < num_ebc; ++ii)
     {   
-        vol_ele_id.resize(get_num_local_cell(ii));
-        ele_face_id.resize(get_num_local_cell(ii));
-        for(int ee{0}; ee < get_num_local_cell(ii); ++ee)
-        {
-          vol_ele_id[ii][ee] = ebc->get_global_cell(ii, ee);
-          ele_face_id[ii][ee] = ebc->get_face_id(ii, ee);
-        }
+      vol_ele_id[ii].resize(get_num_local_cell(ii));
+      for(int ee{0}; ee < get_num_local_cell(ii); ++ee)
+        vol_ele_id[ii][ee] = ebc -> get_global_cell(ii, ee);
+
+      ele_face_id[ii] = ebc -> get_faceID(ii);
+
+      if(weak_bc_type == 2)
+        rot_mat[ii] = ebc -> get_rotation_matrix(ii);
     }
   }
   else
@@ -34,4 +38,57 @@ EBC_Partition_weak::~EBC_Partition_weak()
 {
   vol_ele_id.clear();
   ele_face_id.clear();
+}
+
+void EBC_Partition_weak::write_hdf5(const std::string &FileName) const
+{
+  const std::string fName = SYS_T::gen_partfile_name( FileName, cpu_rank );
+
+  hid_t file_id = H5Fopen(fName.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+
+  hid_t g_id = H5Gcreate(file_id, "weak", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+  HDF5_Writer * h5w = new HDF5_Writer( file_id );
+
+  h5w -> write_intScalar( g_id, "weakBC_type", weak_bc_type );
+
+  h5w -> write_intScalar( g_id, "num_weakBC", num_ebc );
+
+  if(weak_bc_type > 0)
+  {
+    h5w -> write_doubleScalar( g_id, "C_bI", C_bI );
+
+    h5w -> write_intVector( g_id, "num_local_cell", num_local_cell );
+
+    if(weak_bc_type == 2)
+      h5w -> write_intVector( g_id, "num_local_cell_node", num_local_cell_node );
+
+    const std::string groupbase("weakBCid_");
+
+    for(int ii{0}; ii < num_ebc; ++ii)
+    {
+      std::string subgroup_name(groupbase);
+      subgroup_name.append( std::to_string(ii) );
+
+      hid_t group_id = H5Gcreate(g_id, subgroup_name.c_str(), 
+          H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+      h5w -> write_intVector( group_id, "volume_cell_id", vol_ele_id[ii] );
+
+      h5w -> write_intVector( group_id, "cell_face_id", ele_face_id[ii] );
+
+      if(weak_bc_type == 2)
+      {
+        h5w -> write_intVector( group_id, "global_node_id", local_cell_node_vol_id[ii] );
+
+        h5w -> write_doubleVector( group_id, "node_rotation_matrix", rot_mat[ii] );
+      }
+      
+      H5Gclose( group_id );
+    }
+  }
+  else
+    ;   // stop writing if weak_bc_type = 0
+
+  delete h5w; H5Gclose( g_id ); H5Fclose( file_id );
 }
