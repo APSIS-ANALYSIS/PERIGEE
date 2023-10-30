d6cdf12e3c19eaa69ef7242a58fcd825334f5c95
SUNYujie <12231157@mail.sustech.edu.cn>
2023-10-30 17:31:59 +0800

    format

--- src/Mesh/QuadPts_vis_quad4.cpp
+++ src/Mesh/QuadPts_vis_quad4.cpp
@@ -18,14 +18,13 @@ QuadPts_vis_quad4::QuadPts_vis_quad4()
 }
 
 QuadPts_vis_quad4::~QuadPts_vis_quad4()
-{
-}
+{}
 
 void QuadPts_vis_quad4::print_info() const
 {
    SYS_T::commPrint("\n===== Visualization Points for Quad4 ===== \n");
   for(int ii=0; ii<4; ++ii)
-    SYS_T::commPrint("%e, %e, %e \n",
+    PetscPrintf(PETSC_COMM_WORLD, "%e, %e, %e \n",
         qw[ii], qp[2*ii], qp[2*ii+1]);
   SYS_T::commPrint("========================================= \n");
 }
