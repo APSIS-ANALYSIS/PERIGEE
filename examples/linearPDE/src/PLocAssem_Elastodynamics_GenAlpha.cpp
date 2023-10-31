574687c26819a75cd44d9f90e88eab8733ff5719
SeaVegetable <1135167789@qq.com>
2023-10-31 20:14:20 +0800

    correct some mistakes

--- examples/linearPDE/src/PLocAssem_Elastodynamics_GenAlpha.cpp
+++ examples/linearPDE/src/PLocAssem_Elastodynamics_GenAlpha.cpp
@@ -75,6 +75,8 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Residual(
   
   const double curr = time + alpha_f * dt;
 
+  const double l2mu = lambda + 2.0 * mu;
+
   Zero_Residual();
 
   std::vector<double> R(nLocBas, 0.0), dR_dx(nLocBas, 0.0), dR_dy(nLocBas, 0.0), dR_dz(nLocBas, 0.0);
@@ -125,21 +127,21 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Residual(
       const double NA = R[A], NA_x = dR_dx[A], NA_y = dR_dy[A], NA_z = dR_dz[A];
 
       Residual[3*A  ] += gwts * ( NA * rho * vx_t
-          + NA_x * ((2.0 * mu + lambda) * ux_x + lambda * (uy_y + uz_z))
-          + NA_y * lambda * (ux_y + uy_x)
-          + NA_z * lambda * (ux_z + uz_x)
+          + NA_x * (l2mu * ux_x + lambda * (uy_y + uz_z))
+          + NA_y * mu * (ux_y + uy_x)
+          + NA_z * mu * (ux_z + uz_x)
           - NA * rho * f_body.x() );
 
       Residual[3*A+1] += gwts * ( NA * rho * vy_t
-          + NA_x * lambda * (ux_y + uy_x)
-          + NA_y * ((2.0 * mu + lambda) * uy_y + lambda * (ux_x + uz_z))
-          + NA_z * lambda * (uy_z + uz_y)
+          + NA_x * mu * (ux_y + uy_x)
+          + NA_y * (l2mu * uy_y + lambda * (ux_x + uz_z))
+          + NA_z * mu * (uy_z + uz_y)
           - NA * rho * f_body.y() );
 
       Residual[3*A+2] += gwts * ( NA * rho * vz_t
-          + NA_x * lambda * (ux_z + uz_x)
-          + NA_y * lambda * (uy_z + uz_y)
-          + NA_z * ((2.0 * mu + lambda) * uz_z + lambda * (ux_x + uy_y))
+          + NA_x * mu * (ux_z + uz_x)
+          + NA_y * mu * (uy_z + uz_y)
+          + NA_z * (l2mu * uz_z + lambda * (ux_x + uy_y))
           - NA * rho * f_body.z() );
     }
   }
@@ -160,6 +162,8 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Tangent_Residual(
   element->buildBasis( quad, eleCtrlPts_x, eleCtrlPts_y, eleCtrlPts_z );
   
   const double curr = time + alpha_f * dt;
+
+  const double l2mu = lambda + 2.0 * mu;
 
   const double dd_dv = alpha_f * alpha_f * gamma * gamma * dt * dt / alpha_m;
 
@@ -213,21 +217,21 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Tangent_Residual(
       const double NA = R[A], NA_x = dR_dx[A], NA_y = dR_dy[A], NA_z = dR_dz[A];
 
       Residual[3*A  ] += gwts * ( NA * rho * vx_t
-          + NA_x * ((2.0 * mu + lambda) * ux_x + lambda * (uy_y + uz_z))
-          + NA_y * lambda * (ux_y + uy_x)
-          + NA_z * lambda * (ux_z + uz_x)
+          + NA_x * (l2mu * ux_x + lambda * (uy_y + uz_z))
+          + NA_y * mu * (ux_y + uy_x)
+          + NA_z * mu * (ux_z + uz_x)
           - NA * rho * f_body.x() );
 
       Residual[3*A+1] += gwts * ( NA * rho * vy_t
-          + NA_x * lambda * (ux_y + uy_x)
-          + NA_y * ((2.0 * mu + lambda) * uy_y + lambda * (ux_x + uz_z))
-          + NA_z * lambda * (uy_z + uz_y)
+          + NA_x * mu * (ux_y + uy_x)
+          + NA_y * (l2mu * uy_y + lambda * (ux_x + uz_z))
+          + NA_z * mu * (uy_z + uz_y)
           - NA * rho * f_body.y() );
 
       Residual[3*A+2] += gwts * ( NA * rho * vz_t
-          + NA_x * lambda * (ux_z + uz_x)
-          + NA_y * lambda * (uy_z + uz_y)
-          + NA_z * ((2.0 * mu + lambda) * uz_z + lambda * (ux_x + uy_y))
+          + NA_x * mu * (ux_z + uz_x)
+          + NA_y * mu * (uy_z + uz_y)
+          + NA_z * (l2mu * uz_z + lambda * (ux_x + uy_y))
           - NA * rho * f_body.z() );
 
       for(int B=0; B<nLocBas; ++B)
@@ -235,28 +239,28 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Tangent_Residual(
         const double NB = R[B], NB_x = dR_dx[B], NB_y = dR_dy[B], NB_z = dR_dz[B];
 
         Tangent[9*nLocBas*A+3*B  ] += gwts * (NA * rho * alpha_m * NB 
-            + dd_dv * ((2.0 * mu + lambda) * NA_x * NB_x
-            + lambda * (NA_y * NB_y + NA_z * NB_z)));
+            + dd_dv * (l2mu * NA_x * NB_x
+            + mu * (NA_y * NB_y + NA_z * NB_z)));
 
-        Tangent[9*nLocBas*A+3*B+1] += gwts * dd_dv * lambda * (NA_x * NB_y + NA_y * NB_x);
+        Tangent[9*nLocBas*A+3*B+1] += gwts * dd_dv * (lambda * NA_x * NB_y + mu * NA_y * NB_x);
 
-        Tangent[9*nLocBas*A+3*B+2] += gwts * dd_dv * lambda * (NA_x * NB_z + NA_z * NB_x);
+        Tangent[9*nLocBas*A+3*B+2] += gwts * dd_dv * (lambda * NA_x * NB_z + mu * NA_z * NB_x);
 
-        Tangent[3*nLocBas*(3*A+1)+3*B  ] += gwts * dd_dv * lambda * (NA_x * NB_y + NA_y * NB_x);
+        Tangent[3*nLocBas*(3*A+1)+3*B  ] += gwts * dd_dv * (lambda * NA_y * NB_x + mu * NA_x * NB_y);
 
         Tangent[3*nLocBas*(3*A+1)+3*B+1] += gwts * ( NA * rho * alpha_m * NB 
-            + dd_dv * ((2.0 * mu + lambda) * NA_y * NB_y
-            + lambda * (NA_x * NB_x + NA_z * NB_z)));
+            + dd_dv * (l2mu * NA_y * NB_y
+            + mu * (NA_x * NB_x + NA_z * NB_z)));
 
-        Tangent[3*nLocBas*(3*A+1)+3*B+2] += gwts * dd_dv * lambda * (NA_y * NB_z + NA_z * NB_y);
+        Tangent[3*nLocBas*(3*A+1)+3*B+2] += gwts * dd_dv * (lambda * NA_y * NB_z + mu * NA_z * NB_y);
 
-        Tangent[3*nLocBas*(3*A+2)+3*B  ] += gwts * dd_dv * lambda * (NA_x * NB_z + NA_z * NB_x);
+        Tangent[3*nLocBas*(3*A+2)+3*B  ] += gwts * dd_dv * (lambda * NA_z * NB_x + mu * NA_x * NB_z);
 
-        Tangent[3*nLocBas*(3*A+2)+3*B+1] += gwts * dd_dv * lambda * (NA_y * NB_z + NA_z * NB_y);
+        Tangent[3*nLocBas*(3*A+2)+3*B+1] += gwts * dd_dv * (lambda * NA_z * NB_y + mu * NA_y * NB_z);
 
         Tangent[3*nLocBas*(3*A+2)+3*B+2] += gwts * (NA * rho * alpha_m * NB 
-            + dd_dv * ((2.0 * mu + lambda) * NA_z * NB_z
-            + lambda * (NA_x * NB_x + NA_y * NB_y)));     
+            + dd_dv * (l2mu * NA_z * NB_z
+            + mu * (NA_x * NB_x + NA_y * NB_y)));     
       } 
     }
   } // End-of-quadrature-loop
@@ -275,6 +279,8 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Mass_Residual(
   element->buildBasis( quad, eleCtrlPts_x, eleCtrlPts_y, eleCtrlPts_z );
 
   const double curr = 0.0;
+
+  const double l2mu = lambda + 2.0 * mu;
 
   Zero_Tangent_Residual();
 
@@ -320,21 +326,21 @@ void PLocAssem_Elastodynamics_GenAlpha::Assem_Mass_Residual(
       const double NA = R[A], NA_x = dR_dx[A], NA_y = dR_dy[A], NA_z = dR_dz[A];
 
       Residual[3*A  ] += gwts * (
-          + NA_x * ((2.0 * mu + lambda) * ux_x + lambda * (uy_y + uz_z))
-          + NA_y * lambda * (ux_y + uy_x)
-          + NA_z * lambda * (ux_z + uz_x)
+          + NA_x * (l2mu * ux_x + lambda * (uy_y + uz_z))
+          + NA_y * mu * (ux_y + uy_x)
+          + NA_z * mu * (ux_z + uz_x)
           - NA * rho * f_body.x() );
 
       Residual[3*A+1] += gwts * (
-          + NA_x * lambda * (ux_y + uy_x)
-          + NA_y * ((2.0 * mu + lambda) * uy_y + lambda * (ux_x + uz_z))
-          + NA_z * lambda * (uy_z + uz_y)
+          + NA_x * mu * (ux_y + uy_x)
+          + NA_y * (l2mu * uy_y + lambda * (ux_x + uz_z))
+          + NA_z * mu * (uy_z + uz_y)
           - NA * rho * f_body.y() );
 
       Residual[3*A+2] += gwts * (
-          + NA_x * lambda * (ux_z + uz_x)
-          + NA_y * lambda * (uy_z + uz_y)
-          + NA_z * ((2.0 * mu + lambda) * uz_z + lambda * (ux_x + uy_y))
+          + NA_x * mu * (ux_z + uz_x)
+          + NA_y * mu * (uy_z + uz_y)
+          + NA_z * (l2mu * uz_z + lambda * (ux_x + uy_y))
           - NA * rho * f_body.z() );
 
       for(int B=0; B<nLocBas; ++B)
