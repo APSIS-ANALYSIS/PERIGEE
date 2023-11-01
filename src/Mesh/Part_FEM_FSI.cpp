ff343f051b1f1dd8044b64b8fa5dc84d844ad35d
Ju Liu <liujuy@gmail.com>
2023-11-01 09:10:07 +0800

    I re-use the base class writer to simplify the writer in Part_FEM_FSI

--- src/Mesh/Part_FEM_FSI.cpp
+++ src/Mesh/Part_FEM_FSI.cpp
@@ -51,35 +51,26 @@ Part_FEM_FSI::~Part_FEM_FSI()
 
 void Part_FEM_FSI::write( const std::string &inputFileName ) const
 {
+  // ------------------------------------------------------
+  // Call the base class writer to write the base class data
+  Part_FEM::write( inputFileName );
+  // ------------------------------------------------------
+
   const std::string fName = SYS_T::gen_partfile_name( inputFileName, cpu_rank );
 
-  hid_t file_id = H5Fcreate(fName.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  hid_t file_id = H5Fopen(fName.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
 
   HDF5_Writer * h5w = new HDF5_Writer(file_id);
 
-  // group 1: local element
-  hid_t group_id_1 = H5Gcreate(file_id, "/Local_Elem", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  // open group 1: local element
+  hid_t group_id_1 = H5Gopen(file_id, "/Local_Elem", H5P_DEFAULT);
 
-  h5w->write_intScalar( group_id_1, "nlocalele", nlocalele );
-  h5w->write_intVector( group_id_1, "elem_loc", elem_loc );
   h5w->write_intVector( group_id_1, "elem_phy_tag", elem_phy_tag );
 
   H5Gclose( group_id_1 );
 
   // group 2: local node
-  hid_t group_id_2 = H5Gcreate( file_id, "/Local_Node", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT );
-
-  h5w->write_intScalar( group_id_2, "nlocalnode", nlocalnode );
-  h5w->write_intScalar( group_id_2, "nghostnode", nghostnode );
-  h5w->write_intScalar( group_id_2, "ntotalnode", ntotalnode );
-  h5w->write_intScalar( group_id_2, "nbadnode", nbadnode );
-  h5w->write_intScalar( group_id_2, "nlocghonode", nlocghonode );
-
-  h5w->write_intVector( group_id_2, "node_loc", node_loc );
-  h5w->write_intVector( group_id_2, "node_loc_original", node_loc_original );
-  h5w->write_intVector( group_id_2, "local_to_global", local_to_global );
-  if(nghostnode > 0)
-    h5w->write_intVector( group_id_2, "node_ghost", node_ghost );
+  hid_t group_id_2 = H5Gopen( file_id, "/Local_Node", H5P_DEFAULT );
 
   h5w->write_intScalar( group_id_2, "nlocalnode_fluid", nlocalnode_fluid );
   h5w->write_intScalar( group_id_2, "nlocalnode_solid", nlocalnode_solid );
@@ -92,60 +83,6 @@ void Part_FEM_FSI::write( const std::string &inputFileName ) const
 
   H5Gclose( group_id_2 );
 
-  // group 3: global mesh info
-  hid_t group_id_3 = H5Gcreate(file_id, "/Global_Mesh_Info", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-
-  h5w->write_intScalar( group_id_3, "nElem", nElem );
-  h5w->write_intScalar( group_id_3, "nFunc", nFunc );
-
-  const std::vector<int> deg { sDegree, tDegree, uDegree };
-
-  h5w->write_intVector( group_id_3, "degree", deg );
-
-  h5w->write_intScalar( group_id_3, "nLocBas", nLocBas );
-
-  h5w->write_intScalar( group_id_3, "probDim", probDim );
-  h5w->write_intScalar( group_id_3, "dofNum",  dofNum );
-  h5w->write_intScalar( group_id_3, "elemType", elemType );
-
-  H5Gclose( group_id_3 );
-
-  // group 4: part info
-  hid_t group_id_4 = H5Gcreate( file_id, "/Part_Info", H5P_DEFAULT, H5P_DEFAULT,
-      H5P_DEFAULT ); 
-
-  h5w->write_intScalar( group_id_4, "cpu_rank", cpu_rank );
-  h5w->write_intScalar( group_id_4, "cpu_size", cpu_size );
-
-  H5Gclose( group_id_4 );
-
-  // group 5: LIEN
-  hid_t group_id_5 = H5Gcreate(file_id, "/LIEN", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-
-  std::vector<int> row_LIEN(nlocalele * nLocBas, -1);
-
-  for(int e=0; e<nlocalele; ++e)
-  {
-    for(int ii=0; ii<nLocBas; ++ii)
-      row_LIEN[e*nLocBas + ii] = LIEN[e][ii];
-  }
-
-  h5w -> write_intMatrix( group_id_5, "LIEN", row_LIEN, nlocalele, nLocBas);
-
-  H5Gclose( group_id_5 );
-
-  // group 6: control points
-  if( is_geo_field)
-  {
-    hid_t group_id_6 = H5Gcreate(file_id, "/ctrlPts_loc", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-
-    h5w -> write_doubleVector( group_id_6, "ctrlPts_x_loc", ctrlPts_x_loc );
-    h5w -> write_doubleVector( group_id_6, "ctrlPts_y_loc", ctrlPts_y_loc );
-    h5w -> write_doubleVector( group_id_6, "ctrlPts_z_loc", ctrlPts_z_loc );
-
-    H5Gclose( group_id_6 );
-  }
-
   // group 7: DOF mapper
   hid_t group_id_7 = H5Gcreate(file_id, "/DOF_mapper", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
 
@@ -153,7 +90,7 @@ void Part_FEM_FSI::write( const std::string &inputFileName ) const
 
   H5Gclose( group_id_7 );
 
-  // Finish writing, clean up
+  // Finish the writing of hdf5 file
   delete h5w;
   H5Fclose(file_id);
 }
