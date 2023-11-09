300131cb7b984af7c10c30b878552bf4414087ed
Ju Liu <liujuy@gmail.com>
2023-11-09 18:21:51 +0800

    updated fsi project

--- examples/fsi/wall_ps_driver.cpp
+++ examples/fsi/wall_ps_driver.cpp
@@ -90,7 +90,11 @@ int main( int argc, char *argv[] )
   delete pcmd_h5r; H5Fclose(prepcmd_file);
 
   // Initialize PETSc
+#if PETSC_VERSION_LT(3,19,0)
   PetscInitialize(&argc, &argv, (char *)0, PETSC_NULL);
+#else
+  PetscInitialize(&argc, &argv, (char *)0, PETSC_NULLPTR);
+#endif
 
   const PetscMPIInt rank = SYS_T::get_MPI_rank();
   const PetscMPIInt size = SYS_T::get_MPI_size();
