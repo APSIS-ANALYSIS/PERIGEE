88c066ce669bdb8ed15b7a1b8085a4e24f096284
rrrabittt <12132408@mail.sustch.edu.cn>
2023-11-20 02:26:09 +0800

    new class for tissue property

---
+++ include/Tissue_property.hpp
@@ -0,0 +1,63 @@
+#ifndef TISSUE_PROPERTY_HPP
+#define TISSUE_PROPERTY_HPP
+// ============================================================================
+// Tissue_property.hpp
+//
+// This class stores the direction basis vectors at solid nodes. 
+// Direction basis includes 3 Vector_3 for each node.
+//
+// Constructed from inputs of the file basename and cpu_rank, and read using 
+// the HDF5_Reader tool.
+//
+// Date: Apr. 13th 2023
+// Author: Qingshuang Lu
+// ============================================================================
+#include "HDF5_Reader.hpp"
+
+class Tissue_property
+{
+  public:
+    Tissue_property( const std::string &fileBaseName, const int &rank );
+
+    virtual ~Tissue_property();
+
+    // ------------------------------------------------------------------------
+    // Input: nn the index of local_to_global, including local nodes and ghost nodes. 
+    //        The maxmum of nn is nlocghonode-1.
+    // Output: the Vector_3 of the corresponding direction basis vector
+    // ------------------------------------------------------------------------
+    virtual Vector_3 get_basis_r( const int &nn ) const;
+
+    virtual Vector_3 get_basis_l( const int &nn ) const;
+
+    virtual Vector_3 get_basis_c( const int &nn ) const;
+
+    // ------------------------------------------------------------------------
+    // ! Print the info for this class.
+    // ------------------------------------------------------------------------
+    virtual void print_info() const;
+
+  private:
+    // ------------------------------------------------------------------------
+    // The rank or id of the subdomain
+    // ------------------------------------------------------------------------
+    const int cpu_rank;
+
+    // ------------------------------------------------------------------------
+    // The number of local solid node
+    // ------------------------------------------------------------------------
+    int nlocghonode_s;
+
+    // ------------------------------------------------------------------------
+    // The mapping from the total local nodes to the local solid nodes.
+    // ------------------------------------------------------------------------
+    std::vector<int> node_locgho_solid;
+
+    // ------------------------------------------------------------------------
+    // The direction basis vectors. Letters r, l, and c denote radial, longitudinal,
+    // and circumferential, respectively. 
+    // ------------------------------------------------------------------------
+    std::vector< Vector_3 > basis_r, basis_l, basis_c;
+};
+
+#endif
---
+++ src/Model/Tissue_property.cpp
@@ -0,0 +1,61 @@
+#include "Tissue_property.hpp"
+
+Tissue_property::Tissue_property( const std::string &fileBaseName, const int &rank )
+:cpu_rank( rank )
+{
+  const std::string fName = SYS_T::gen_partfile_name( fileBaseName, cpu_rank );
+
+  hid_t file_id = H5Fopen( fName.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT );
+
+  HDF5_Reader * h5r = new HDF5_Reader( file_id );
+
+  nlocghonode_s = h5r -> read_intScalar("directionBasis_loc", "nlocghonode_s");
+  node_locgho_solid = h5r -> read_intVector("directionBasis_loc", "node_locgho_solid");
+  basis_r = h5r -> read_Vector3_Vector("directionBasis_loc", "loc_basis_r");
+  basis_l = h5r -> read_Vector3_Vector("directionBasis_loc", "loc_basis_l");
+  basis_c = h5r -> read_Vector3_Vector("directionBasis_loc", "loc_basis_c");
+
+  delete h5r; H5Fclose( file_id );
+}
+
+Tissue_property::~Tissue_property()
+{}
+
+Vector_3 Tissue_property::get_basis_r( const int &nn ) const
+{
+  const int index = node_locgho_solid[nn];
+  ASSERT(index > -1, "ERROR: Tissue_property::get_basis_r the input node is a fluid node.");
+  return basis_r[index];
+}
+
+Vector_3 Tissue_property::get_basis_l( const int &nn ) const
+{
+  const int index = node_locgho_solid[nn];
+  ASSERT(index > -1, "ERROR: Tissue_property::get_basis_l the input node is a fluid node.");
+  return basis_l[index];
+}
+
+Vector_3 Tissue_property::get_basis_c( const int &nn ) const
+{
+  const int index = node_locgho_solid[nn];
+  ASSERT(index > -1, "ERROR: Tissue_property::get_basis_c the input node is a fluid node.");
+  return basis_c[index];
+}
+
+void Tissue_property::print_info() const
+{
+  std::cout<<"\n -- cpu "<<cpu_rank<<" node info: \n";
+  std::cout<<"\n nlocghonode_s: "<<nlocghonode_s<<std::endl;
+  std::cout<<"\n node_locgho_solid: \n";
+  VEC_T::print(node_locgho_solid);
+  const int size = VEC_T::get_size( basis_r );
+  std::cout<<"\n basis_r: \n";
+  for(int ii=0; ii<size; ii++) basis_r[ii].print(); 
+  std::cout<<"\n basis_l: \n";
+  for(int ii=0; ii<size; ii++) basis_l[ii].print();
+  std::cout<<"\n basis_c: \n";
+  for(int ii=0; ii<size; ii++) basis_c[ii].print();
+}
+
+
+// EOF
