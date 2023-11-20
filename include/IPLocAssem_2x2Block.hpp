e7a3ce580e0bade77eeea7e6eea9de2663e6fe72
rrrabittt <12132408@mail.sustch.edu.cn>
2023-11-21 01:15:12 +0800

    change in basis class IPLocAssem_2x2Block.hpp

--- include/IPLocAssem_2x2Block.hpp
+++ include/IPLocAssem_2x2Block.hpp
@@ -170,6 +170,26 @@ class IPLocAssem_2x2Block
         const IQuadPts * const &quad )
     {SYS_T::commPrint("Warning: IPLocAssem_2x2Block::Assem_Residual is not implemented. \n");}
 
+    // Special for GOH06 material models, where node direction basis is needed.
+    virtual void Assem_Residual(
+        const double &time, const double &dt,
+        const double * const &dot_disp,
+        const double * const &dot_velo,
+        const double * const &dot_pres,
+        const double * const &disp,
+        const double * const &velo,
+        const double * const &pres,
+        FEAElement * const &element,
+        const double * const &eleCtrlPts_x,
+        const double * const &eleCtrlPts_y,
+        const double * const &eleCtrlPts_z,
+        const double * const &qua_prestress,
+        const IQuadPts * const &quad,
+	const std::vector<Vector_3> &eleBasis_r,
+        const std::vector<Vector_3> &eleBasis_l,
+        const std::vector<Vector_3> &eleBasis_c )
+    {SYS_T::commPrint("Warning: IPLocAssem_2x2Block::Assem_Residual is not implemented. \n");}
+
     // Assembly the two Residuals and the four matrice blocks.
     virtual void Assem_Tangent_Residual(
         const double &time, const double &dt,
@@ -211,6 +231,26 @@ class IPLocAssem_2x2Block
         const double * const &eleCtrlPts_z,
         const double * const &qua_prestress,
         const IQuadPts * const &quad )
+    {SYS_T::commPrint("Warning: IPLocAssem_2x2Block::Assem_Tangent_Residual is not implemented. \n");}
+
+    // Special for GOH06 material models, where node direction basis is needed.
+    virtual void Assem_Tangent_Residual(
+        const double &time, const double &dt,
+        const double * const &dot_disp,
+        const double * const &dot_velo,
+        const double * const &dot_pres,
+        const double * const &disp,
+        const double * const &velo,
+        const double * const &pres,
+        FEAElement * const &element,
+        const double * const &eleCtrlPts_x,
+        const double * const &eleCtrlPts_y,
+        const double * const &eleCtrlPts_z,
+        const double * const &qua_prestress,
+        const IQuadPts * const &quad,
+	const std::vector<Vector_3> &eleBasis_r,
+        const std::vector<Vector_3> &eleBasis_l,
+        const std::vector<Vector_3> &eleBasis_c )
     {SYS_T::commPrint("Warning: IPLocAssem_2x2Block::Assem_Tangent_Residual is not implemented. \n");}
 
     // Assembly the two Residuals and the four matrice blocks for mass matrices.
