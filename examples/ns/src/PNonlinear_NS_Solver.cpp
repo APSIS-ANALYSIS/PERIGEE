b95badbc8b81e664d1ef3f824a29d11b52060b85
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-04-28 11:18:02 +0800

    restore genA

--- examples/ns/src/PNonlinear_NS_Solver.cpp
+++ examples/ns/src/PNonlinear_NS_Solver.cpp
@@ -15,7 +15,7 @@ PNonlinear_NS_Solver::PNonlinear_NS_Solver(
   // Generate the incremental solution vector used for update 
   // the solution of the nonlinear algebraic system 
   step = new PDNSolution_NS( anode_ptr, 0, false );
-  }
+}
 
 PNonlinear_NS_Solver::PNonlinear_NS_Solver(
     const APart_Node * const &anode_ptr,
@@ -46,6 +46,235 @@ void PNonlinear_NS_Solver::print_info() const
   SYS_T::commPrint("  tangent matrix renew frequency: %d \n", nrenew_freq);
   SYS_T::commPrint("  tangent matrix renew threshold: %d \n", nrenew_threshold);
   SYS_T::commPrint("----------------------------------------------------------- \n");
+}
+
+void PNonlinear_NS_Solver::GenAlpha_Solve_NS(
+    const bool &new_tangent_flag,
+    const double &curr_time,
+    const double &dt,
+    const PDNSolution * const &sol_base,
+    const PDNSolution * const &pre_dot_sol,
+    const PDNSolution * const &pre_sol,
+    const TimeMethod_GenAlpha * const &tmga_ptr,
+    const ICVFlowRate * const flr_ptr,
+    const ALocal_Elem * const &alelem_ptr,
+    const ALocal_IEN * const &lien_ptr,
+    const FEANode * const &feanode_ptr,
+    const ALocal_NBC * const &nbc_part,
+    const ALocal_InflowBC * const &infnbc_part,
+    const ALocal_EBC * const &ebc_part,
+    const IGenBC * const &gbc,
+    const ALocal_WeakBC * const &wbc_part,
+    const Matrix_PETSc * const &bc_mat,
+    FEAElement * const &elementv,
+    FEAElement * const &elements,
+    FEAElement * const &elementvs,
+    const IQuadPts * const &quad_v,
+    const IQuadPts * const &quad_s,
+    IPLocAssem * const &lassem_ptr,
+    IPGAssem * const &gassem_ptr,
+    PLinear_Solver_PETSc * const &lsolver_ptr,
+    PDNSolution * const &dot_sol,
+    PDNSolution * const &sol,
+    bool &conv_flag, int &nl_counter ) const
+{
+#ifdef PETSC_USE_LOG
+  PetscLogEvent mat_assem_0_event, mat_assem_1_event;
+  PetscLogEvent vec_assem_0_event, vec_assem_1_event;
+  PetscLogEvent lin_solve_event;
+  PetscClassId classid_assembly;
+  PetscClassIdRegister("mat_vec_assembly", &classid_assembly);
+  PetscLogEventRegister("assembly mat 0", classid_assembly, &mat_assem_0_event);
+  PetscLogEventRegister("assembly mat 1", classid_assembly, &mat_assem_1_event);
+  PetscLogEventRegister("assembly vec 0", classid_assembly, &vec_assem_0_event);
+  PetscLogEventRegister("assembly vec 1", classid_assembly, &vec_assem_1_event);
+  PetscLogEventRegister("lin_solve", classid_assembly, &lin_solve_event);
+#endif
+
+  // Initialize the counter and error
+  nl_counter = 0;
+  double residual_norm = 0.0, initial_norm = 0.0, relative_error = 0.0;
+
+  const double gamma   = tmga_ptr->get_gamma();
+  const double alpha_m = tmga_ptr->get_alpha_m();
+  const double alpha_f = tmga_ptr->get_alpha_f();
+
+  // Same-Y predictor
+  sol->Copy(*pre_sol);
+  dot_sol->Copy(*pre_dot_sol);
+  dot_sol->ScaleValue( (gamma-1.0)/gamma );
+
+  // Define the dol_sol at alpha_m: dot_sol_alpha
+  PDNSolution dot_sol_alpha(*pre_dot_sol);
+  dot_sol_alpha.ScaleValue( 1.0 - alpha_m );
+  dot_sol_alpha.PlusAX(*dot_sol, alpha_m);
+
+  // Define the sol at alpha_f: sol_alpha
+  PDNSolution sol_alpha(*pre_sol);
+  sol_alpha.ScaleValue( 1.0 - alpha_f );
+  sol_alpha.PlusAX( *sol, alpha_f );
+
+  // ------------------------------------------------- 
+  // Update the inflow boundary values
+  rescale_inflow_value(curr_time+dt, infnbc_part, flr_ptr, sol_base, sol);
+  rescale_inflow_value(curr_time+alpha_f*dt, infnbc_part, flr_ptr, sol_base, &sol_alpha);
+  // ------------------------------------------------- 
+
+  // If new_tangent_flag == TRUE, update the tangent matrix;
+  // otherwise, use the matrix from the previous time step
+  if( new_tangent_flag )
+  {
+    gassem_ptr->Clear_KG();
+
+#ifdef PETSC_USE_LOG
+    PetscLogEventBegin(mat_assem_0_event, 0,0,0,0);
+#endif
+
+    gassem_ptr->Assem_tangent_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol, 
+        curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
+        quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, gbc, wbc_part );
+   
+#ifdef PETSC_USE_LOG
+    PetscLogEventEnd(mat_assem_0_event,0,0,0,0);
+#endif
+
+    SYS_T::commPrint("  --- M updated");
+    
+    // SetOperator will pass the tangent matrix to the linear solver and the
+    // linear solver will generate the preconditioner based on the new matrix.
+    lsolver_ptr->SetOperator( gassem_ptr->K );
+  }
+  else
+  {
+    gassem_ptr->Clear_G();
+
+#ifdef PETSC_USE_LOG
+    PetscLogEventBegin(vec_assem_0_event, 0,0,0,0);
+#endif
+
+    gassem_ptr->Assem_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
+        curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
+        quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, gbc, wbc_part );
+
+#ifdef PETSC_USE_LOG
+    PetscLogEventEnd(vec_assem_0_event,0,0,0,0);
+#endif
+  }
+
+  VecNorm(gassem_ptr->G, NORM_2, &initial_norm);
+  SYS_T::commPrint("  Init res 2-norm: %e \n", initial_norm);
+
+  // Now do consistent Newton-Raphson iteration
+  do
+  {
+#ifdef PETSC_USE_LOG
+    PetscLogEventBegin(lin_solve_event, 0,0,0,0);
+#endif
+    
+    // solve the equation K step = G
+    lsolver_ptr->Solve( gassem_ptr->G, step );
+
+#ifdef PETSC_USE_LOG
+    PetscLogEventEnd(lin_solve_event,0,0,0,0);
+#endif
+
+    bc_mat->MatMultSol( step );
+
+    nl_counter += 1;
+
+    dot_sol->PlusAX( step, -1.0 );
+    sol->PlusAX( step, (-1.0) * gamma * dt );
+
+    dot_sol_alpha.PlusAX( step, (-1.0) * alpha_m );
+    sol_alpha.PlusAX( step, (-1.0) * alpha_f * gamma * dt );
+
+    // Assembly residual (& tangent if condition satisfied) 
+    if( nl_counter % nrenew_freq == 0 || nl_counter >= nrenew_threshold )
+    {
+      gassem_ptr->Clear_KG();
+
+#ifdef PETSC_USE_LOG
+      PetscLogEventBegin(mat_assem_1_event, 0,0,0,0);
+#endif
+
+      gassem_ptr->Assem_tangent_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
+          curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
+          quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, gbc, wbc_part );
+
+#ifdef PETSC_USE_LOG
+      PetscLogEventEnd(mat_assem_1_event,0,0,0,0);
+#endif
+
+      SYS_T::commPrint("  --- M updated");
+      lsolver_ptr->SetOperator(gassem_ptr->K);
+    }
+    else
+    {
+      gassem_ptr->Clear_G();
+
+#ifdef PETSC_USE_LOG
+      PetscLogEventBegin(vec_assem_1_event, 0,0,0,0);
+#endif
+
+      gassem_ptr->Assem_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
+          curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
+          quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, gbc, wbc_part );
+
+#ifdef PETSC_USE_LOG
+      PetscLogEventEnd(vec_assem_1_event,0,0,0,0);
+#endif
+    }
+
+    VecNorm(gassem_ptr->G, NORM_2, &residual_norm);
+    
+    SYS_T::print_fatal_if( residual_norm != residual_norm, "Error: nonlinear solver residual norm is NaN. Job killed.\n" );
+    
+    SYS_T::commPrint("  --- nl_res: %e \n", residual_norm);
+
+    relative_error = residual_norm / initial_norm;
+
+    SYS_T::print_fatal_if( relative_error >= nd_tol, "Error: nonlinear solver is diverging with error %e. Job killed.\n", relative_error);
+
+  }while(nl_counter<nmaxits && relative_error > nr_tol && residual_norm > na_tol);
+
+  Print_convergence_info(nl_counter, relative_error, residual_norm);
+
+  if(relative_error <= nr_tol || residual_norm <= na_tol) conv_flag = true;
+  else conv_flag = false;
+}
+
+void PNonlinear_NS_Solver::rescale_inflow_value( const double &stime,
+    const ALocal_InflowBC * const &infbc,
+    const ICVFlowRate * const &flrate,
+    const PDNSolution * const &sol_base,
+    PDNSolution * const &sol ) const
+{
+  const int num_nbc = infbc -> get_num_nbc();
+
+  for(int nbc_id=0; nbc_id<num_nbc; ++nbc_id)
+  {
+    const int numnode = infbc -> get_Num_LD( nbc_id );
+
+    const double factor = flrate -> get_flow_rate( nbc_id, stime );
+
+    for(int ii=0; ii<numnode; ++ii)
+    {
+      const int node_index = infbc -> get_LDN( nbc_id, ii );
+      
+      const int base_idx[3] = { node_index*4+1, node_index*4+2, node_index*4+3 };
+
+      double base_vals[3];
+
+      VecGetValues(sol_base->solution, 3, base_idx, base_vals);
+
+      const double vals[3] = { base_vals[0] * factor, base_vals[1] * factor,
+          base_vals[2] * factor };
+
+      VecSetValues(sol->solution, 3, base_idx, vals, INSERT_VALUES);
+    }
+  }
+
+  sol->Assembly_GhostUpdate();
 }
 
 void PNonlinear_NS_Solver::SemiBDF1_Solve_NS(
@@ -134,236 +363,5 @@ void PNonlinear_NS_Solver::SemiBDF1_Solve_NS(
     //SYS_T::commPrint("  --- nl_res: %e \n", residual_norm);
 
 }
-
-
-void PNonlinear_NS_Solver::GenAlpha_Solve_NS(
-    const bool &new_tangent_flag,
-    const double &curr_time,
-    const double &dt,
-        const PDNSolution * const &sol_base,
-        const PDNSolution * const &pre_dot_sol,
-        const PDNSolution * const &pre_sol,
-        const TimeMethod_GenAlpha * const &tmga_ptr,
-        const ICVFlowRate * const flr_ptr,
-        const ALocal_Elem * const &alelem_ptr,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &feanode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_InflowBC * const &infnbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const ALocal_WeakBC * const &wbc_part,
-        const Matrix_PETSc * const &bc_mat,
-        FEAElement * const &elementv,
-        FEAElement * const &elements,
-        FEAElement * const &elementvs,
-        const IQuadPts * const &quad_v,
-        const IQuadPts * const &quad_s,
-        IPLocAssem * const &lassem_ptr,
-        IPGAssem * const &gassem_ptr,
-        PLinear_Solver_PETSc * const &lsolver_ptr,
-        PDNSolution * const &dot_sol,
-        PDNSolution * const &sol,
-        bool &conv_flag, int &nl_counter ) const
-{
-#ifdef PETSC_USE_LOG
-  PetscLogEvent mat_assem_0_event, mat_assem_1_event;
-  PetscLogEvent vec_assem_0_event, vec_assem_1_event;
-  PetscLogEvent lin_solve_event;
-  PetscClassId classid_assembly;
-  PetscClassIdRegister("mat_vec_assembly", &classid_assembly);
-  PetscLogEventRegister("assembly mat 0", classid_assembly, &mat_assem_0_event);
-  PetscLogEventRegister("assembly mat 1", classid_assembly, &mat_assem_1_event);
-  PetscLogEventRegister("assembly vec 0", classid_assembly, &vec_assem_0_event);
-  PetscLogEventRegister("assembly vec 1", classid_assembly, &vec_assem_1_event);
-  PetscLogEventRegister("lin_solve", classid_assembly, &lin_solve_event);
-#endif
-
-  // Initialize the counter and error
-  nl_counter = 0;
-  double residual_norm = 0.0, initial_norm = 0.0, relative_error = 0.0;
-
-  const double gamma   = tmga_ptr->get_gamma();
-  const double alpha_m = tmga_ptr->get_alpha_m();
-  const double alpha_f = tmga_ptr->get_alpha_f();
-
-  // Same-Y predictor
-  sol->Copy(*pre_sol);
-  dot_sol->Copy(*pre_dot_sol);
-  dot_sol->ScaleValue( (gamma-1.0)/gamma );
-
-  // Define the dol_sol at alpha_m: dot_sol_alpha
-  PDNSolution dot_sol_alpha(*pre_dot_sol);
-  dot_sol_alpha.ScaleValue( 1.0 - alpha_m );
-  dot_sol_alpha.PlusAX(*dot_sol, alpha_m);
-
-  // Define the sol at alpha_f: sol_alpha
-  PDNSolution sol_alpha(*pre_sol);
-  sol_alpha.ScaleValue( 1.0 - alpha_f );
-  sol_alpha.PlusAX( *sol, alpha_f );
-
-  // ------------------------------------------------- 
-  // Update the inflow boundary values
-  rescale_inflow_value(curr_time+dt, infnbc_part, flr_ptr, sol_base, sol);
-  rescale_inflow_value(curr_time+alpha_f*dt, infnbc_part, flr_ptr, sol_base, &sol_alpha);
-  // ------------------------------------------------- 
-
-  // If new_tangent_flag == TRUE, update the tangent matrix;
-  // otherwise, use the matrix from the previous time step
-  if( new_tangent_flag )
-  {
-    gassem_ptr->Clear_KG();
-
-#ifdef PETSC_USE_LOG
-    PetscLogEventBegin(mat_assem_0_event, 0,0,0,0);
-#endif
-
-    gassem_ptr->Assem_tangent_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol, 
-        curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
-        quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, wbc_part );
-   
-#ifdef PETSC_USE_LOG
-    PetscLogEventEnd(mat_assem_0_event,0,0,0,0);
-#endif
-
-    SYS_T::commPrint("  --- M updated");
-    
-    // SetOperator will pass the tangent matrix to the linear solver and the
-    // linear solver will generate the preconditioner based on the new matrix.
-    lsolver_ptr->SetOperator( gassem_ptr->K );
-  }
-  else
-  {
-    gassem_ptr->Clear_G();
-
-#ifdef PETSC_USE_LOG
-    PetscLogEventBegin(vec_assem_0_event, 0,0,0,0);
-#endif
-
-    gassem_ptr->Assem_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
-        curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
-        quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, wbc_part );
-
-#ifdef PETSC_USE_LOG
-    PetscLogEventEnd(vec_assem_0_event,0,0,0,0);
-#endif
-  }
-
-  VecNorm(gassem_ptr->G, NORM_2, &initial_norm);
-  SYS_T::commPrint("  Init res 2-norm: %e \n", initial_norm);
-
-  // Now do consistent Newton-Raphson iteration
-  do
-  {
-#ifdef PETSC_USE_LOG
-    PetscLogEventBegin(lin_solve_event, 0,0,0,0);
-#endif
-    
-    // solve the equation K step = G
-    lsolver_ptr->Solve( gassem_ptr->G, step );
-
-#ifdef PETSC_USE_LOG
-    PetscLogEventEnd(lin_solve_event,0,0,0,0);
-#endif
-
-    bc_mat->MatMultSol( step );
-
-    nl_counter += 1;
-
-    dot_sol->PlusAX( step, -1.0 );
-    sol->PlusAX( step, (-1.0) * gamma * dt );
-
-    dot_sol_alpha.PlusAX( step, (-1.0) * alpha_m );
-    sol_alpha.PlusAX( step, (-1.0) * alpha_f * gamma * dt );
-
-    // Assembly residual (& tangent if condition satisfied) 
-    if( nl_counter % nrenew_freq == 0 || nl_counter >= nrenew_threshold )
-    {
-      gassem_ptr->Clear_KG();
-
-#ifdef PETSC_USE_LOG
-      PetscLogEventBegin(mat_assem_1_event, 0,0,0,0);
-#endif
-
-      gassem_ptr->Assem_tangent_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
-          curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
-          quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part, wbc_part );
-
-#ifdef PETSC_USE_LOG
-      PetscLogEventEnd(mat_assem_1_event,0,0,0,0);
-#endif
-
-      SYS_T::commPrint("  --- M updated");
-      lsolver_ptr->SetOperator(gassem_ptr->K);
-    }
-    else
-    {
-      gassem_ptr->Clear_G();
-
-#ifdef PETSC_USE_LOG
-      PetscLogEventBegin(vec_assem_1_event, 0,0,0,0);
-#endif
-
-      gassem_ptr->Assem_residual( &dot_sol_alpha, &sol_alpha, dot_sol, sol,
-          curr_time, dt, alelem_ptr, lassem_ptr, elementv, elements, elementvs,
-          quad_v, quad_s, lien_ptr, feanode_ptr, nbc_part, ebc_part,  wbc_part );
-
-#ifdef PETSC_USE_LOG
-      PetscLogEventEnd(vec_assem_1_event,0,0,0,0);
-#endif
-    }
-
-    VecNorm(gassem_ptr->G, NORM_2, &residual_norm);
-    
-    SYS_T::print_fatal_if( residual_norm != residual_norm, "Error: nonlinear solver residual norm is NaN. Job killed.\n" );
-    
-    SYS_T::commPrint("  --- nl_res: %e \n", residual_norm);
-
-    relative_error = residual_norm / initial_norm;
-
-    SYS_T::print_fatal_if( relative_error >= nd_tol, "Error: nonlinear solver is diverging with error %e. Job killed.\n", relative_error);
-
-  }while(nl_counter<nmaxits && relative_error > nr_tol && residual_norm > na_tol);
-
-  Print_convergence_info(nl_counter, relative_error, residual_norm);
-
-  if(relative_error <= nr_tol || residual_norm <= na_tol) conv_flag = true;
-  else conv_flag = false;
-}
-
-void PNonlinear_NS_Solver::rescale_inflow_value( const double &stime,
-    const ALocal_InflowBC * const &infbc,
-    const ICVFlowRate * const &flrate,
-    const PDNSolution * const &sol_base,
-    PDNSolution * const &sol ) const
-{
-  const int num_nbc = infbc -> get_num_nbc();
-
-  for(int nbc_id=0; nbc_id<num_nbc; ++nbc_id)
-  {
-    const int numnode = infbc -> get_Num_LD( nbc_id );
-
-    const double factor = flrate -> get_flow_rate( nbc_id, stime );
-
-    for(int ii=0; ii<numnode; ++ii)
-    {
-      const int node_index = infbc -> get_LDN( nbc_id, ii );
-      
-      const int base_idx[3] = { node_index*4+1, node_index*4+2, node_index*4+3 };
-
-      double base_vals[3];
-
-      VecGetValues(sol_base->solution, 3, base_idx, base_vals);
-
-      const double vals[3] = { base_vals[0] * factor, base_vals[1] * factor,
-          base_vals[2] * factor };
-
-      VecSetValues(sol->solution, 3, base_idx, vals, INSERT_VALUES);
-    }
-  }
-
-  sol->Assembly_GhostUpdate();
-}
-
-
 
 // EOF
