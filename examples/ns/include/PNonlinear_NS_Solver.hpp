8f88be2fcb48c59bebc6066154fc0a2ffcb46ce3
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-04-28 10:58:07 +0800

    minor

--- examples/ns/include/PNonlinear_NS_Solver.hpp
+++ examples/ns/include/PNonlinear_NS_Solver.hpp
@@ -56,7 +56,7 @@ class PNonlinear_NS_Solver
         const ALocal_NBC * const &nbc_part,
         const ALocal_InflowBC * const &infnbc_part,
         const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbs,
+        const IGenBC * const &gbc,
         const ALocal_WeakBC * const &wbc_part,
         const Matrix_PETSc * const &bc_mat,
         FEAElement * const &elementv,
