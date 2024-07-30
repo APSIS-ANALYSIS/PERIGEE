fda6d7dccb77cbd14143e77c5d6be6c22f6e3ce5
SeaVegetable <1135167789@qq.com>
2024-07-30 13:16:18 +0800

    correct a mistake in wall_ps

--- examples/fsi/wall_ps_driver.cpp
+++ examples/fsi/wall_ps_driver.cpp
@@ -392,6 +392,15 @@ int main( int argc, char *argv[] )
     if( is_read_material )
     {
       std::string matmodel_file_name = "material_model_" + std::to_string(ii) + ".h5";
+      
+      hid_t model_file = H5Fopen(matmodel_file_name.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+
+      HDF5_Reader * model_h5r = new HDF5_Reader( model_file );
+
+      solid_nu[ii] = model_h5r -> read_doubleScalar("/", "nu");
+
+      delete model_h5r; H5Fclose(model_file);
+      
       if( solid_nu[ii] == 0.5 )
       {
         matmodel[ii] = new MaterialModel_GOH06_Incompressible_Mixed( matmodel_file_name.c_str() );
