8b0be81ed2164cdd4c16ee739f2623ba751918c9
Ju Liu <liujuy@gmail.com>
2024-08-05 11:14:02 +0800

    remove the test code

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -38,50 +38,6 @@
 
 int main(int argc, char *argv[])
 {
-  PetscInitialize(&argc, &argv, (char *)0, PETSC_NULLPTR);
-
-  double xx = 0;
-  double yy = 0;
-  double zz = 0;
-
-  double array_x[3];
-  double array_y[3];
-  double array_z[3];
-  
-  for(int ii=0; ii<3; ++ii)
-  {
-    array_x[ii] = MATH_T::gen_double_rand(-1.0, 1.0);
-  }
-
-  for(int ii=0; ii<3; ++ii)
-  {
-    array_y[ii] = MATH_T::gen_double_rand(-1.0, 1.0);
-  }
-
-  for(int ii=0; ii<3; ++ii)
-  {
-    array_z[ii] = MATH_T::gen_double_rand(-1.0, 1.0);
-  }
-    
-// void 类型
-  double r1 = 0;  
-  FE_T::get_tet_sphere_info(array_x[0], array_x[1], array_x[2], array_x[3],
-                            array_y[0], array_y[1], array_y[2], array_y[3],
-                            array_z[0], array_z[1], array_z[2], array_z[3],
-                            xx, yy, zz, r1);
-  std::cout << "void: r1 = " << r1 << '\n';
-
-// double 类型
-  double r2 = 0;
-  r2 = FE_T::get_tet_sphere_radius(array_x[0], array_x[1], array_x[2], array_x[3],
-                                   array_y[0], array_y[1], array_y[2], array_y[3],
-                                   array_z[0], array_z[1], array_z[2], array_z[3]);
-  std::cout << "double: r2 = " << r2 << '\n';
-
-// 误差分析
-  std::cout << std::fixed << std::setprecision(8) << "r_error = " << r1 - r2 << '\n';
-
-  PetscFinalize();
   return EXIT_SUCCESS;
 }
 
