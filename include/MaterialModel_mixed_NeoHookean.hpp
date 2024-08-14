e3756cd15fb1a2957e0f831cacbd2d829fc483f7
Ju Liu <liujuy@gmail.com>
2024-08-14 11:53:57 +0800

    lets again get the stiffness and pk stress together

--- include/MaterialModel_mixed_NeoHookean.hpp
+++ include/MaterialModel_mixed_NeoHookean.hpp
@@ -1,16 +1,16 @@
-#ifndef MATERIALMODEL_NEOHOOKEAN_HPP
-#define MATERIALMODEL_NEOHOOKEAN_HPP
+#ifndef MATERIALMODEL_MIXED_NEOHOOKEAN_HPP
+#define MATERIALMODEL_MIXED_NEOHOOKEAN_HPP
 
 #include "IMaterialModel_mixed.hpp"
 
-class MaterialModel_NeoHookean : public IMaterialModel_mixed
+class MaterialModel_mixed_NeoHookean : public IMaterialModel_mixed
 {
   public:
-    MaterialModel_NeoHookean( std::unique_ptr<IMaterialModel_vol> vmodel,
+    MaterialModel_mixed_NeoHookean( std::unique_ptr<IMaterialModel_vol> vmodel,
         const double &in_mu ) : IMaterialModel_mixed(std::move(vmodel)), 
     elastic_mu( in_mu ) {}
 
-    virtual ~MaterialModel_NeoHookean() = default;
+    virtual ~MaterialModel_mixed_NeoHookean() = default;
 
     virtual SymmTensor2_3D get_PK_2nd( const Tensor2_3D &F ) const
     {
@@ -20,18 +20,22 @@ class MaterialModel_NeoHookean : public IMaterialModel_mixed
       return elastic_mu * val * STen2::gen_symm_part(out);
     }
 
-    virtual SymmTensor4_3D get_Stiffness( const Tensor2_3D &F ) const
+    virtual SymmTensor4_3D get_PK_Stiffness( const Tensor2_3D &F,
+       Tensor2_3D &P_iso ) const
     {
       constexpr double pt67 = 2.0 / 3.0;
       const double val = elastic_mu * std::pow(F.det(), -pt67);
       const SymmTensor2_3D CC = STen2::gen_left_Cauchy_Green(F);
       const SymmTensor2_3D invCC = STen2::inverse(CC);
       
+      const SymmTensor2_3D S_iso = val * STen2::gen_symm_part( Ten4::gen_P( CC.convert_to_full() ) * Ten2::gen_id() );
+      
+      P_sio = F * S_iso;
+      
       SymmTensor4_3D out = STen4::gen_Ptilde( invCC );
       
       out *= pt67 * val * CC.tr();
 
-      const SymmTensor2_3D S_iso = val * STen2::gen_symm_part( Ten4::gen_P( CC.convert_to_full() ) * Ten2::gen_id() );
       out.add_SymmOutProduct(-pt67, invCC, S_iso);
 
       return out;
