e4b61d9d8336122162ecd8e5b88c71a0b42eb47a
SUNYujie <12231157@mail.sustech.edu.cn>
2024-08-16 20:20:14 +0800

    add a member func get_radius for Sl_rotation_info class

--- include/Sl_rotation_info.hpp
+++ include/Sl_rotation_info.hpp
@@ -17,6 +17,20 @@ class Sl_rotation_info
 
     Vector_3 get_direction_rotated() const {return direction_rotated;}
 
+    Vector_3 get_radius(const Vector_3 &coor) const
+    { 
+      // The vector from the rotation point to the input point
+      const Vector_3 point_rotated_to_coor (coor.x() - point_rotated.x(), coor.y() - point_rotated.y(), coor.z() - point_rotated.z());
+
+      const double projectd_length = Vec3::dot_product(point_rotated_to_coor, direction_rotated);
+      
+      // The projection point of the input point on the rotation axis
+      const Vector_3 point_projected (point_rotated.x() +  projectd_length * direction_rotated.x(), point_rotated.y() +  projectd_length * direction_rotated.y(), point_rotated.z() +  projectd_length * direction_rotated.z());
+      
+      // The vector from the projection point to the input point
+      return Vector_3 (coor.x()- point_projected.x(), coor.y()- point_projected.y(), coor.z()- point_projected.z());
+    } 
+
   private:
     // Info of rotation axis
     const double angular_velo;
