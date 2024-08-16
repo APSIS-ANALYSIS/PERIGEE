fdcaabf94c94fd518e58dea36cd3707911881337
SUNYujie <12231157@mail.sustech.edu.cn>
2024-08-16 20:14:08 +0800

    add Sl_rotation_info class

---
+++ include/Sl_rotation_info.hpp
@@ -0,0 +1,29 @@
+#ifndef SL_ROTATION_HPP
+#define SL_ROTATION_HPP
+
+#include "Vector_3.hpp"
+
+class Sl_rotation_info
+{
+  public:
+    Sl_rotation_info(const double &angular,const Vector_3 &point_xyz, const Vector_3 &angular_direc)
+    : angular_velo(angular), point_rotated(point_xyz), direction_rotated(angular_direc) {}
+
+    ~Sl_rotation_info() = default;
+
+    double get_angular_velo() const {return angular_velo;}
+
+    Vector_3 get_point_rotated() const {return point_rotated;}
+
+    Vector_3 get_direction_rotated() const {return direction_rotated;}
+
+  private:
+    // Info of rotation axis
+    const double angular_velo;
+
+    const Vector_3 point_rotated;
+
+    const Vector_3 direction_rotated;    
+};
+
+#endif
