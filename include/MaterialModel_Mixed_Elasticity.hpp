144a359ae081734b6836d93f11154c4cf392e0d8
Ju Liu <liujuy@gmail.com>
2024-08-16 13:24:27 +0800

    have the mixed elasticity interface ready

--- include/MaterialModel_Mixed_Elasticity.hpp
+++ include/MaterialModel_Mixed_Elasticity.hpp
@@ -9,51 +9,40 @@
 class MaterialModel_Mixed_Elasticity
 {
   public:
-    IMaterialModel_mixed( std::unique_ptr<IMaterialModel_vol> in_vmodel ) 
-      : vmodel(std::move(in_vmodel)) {};
+    MaterialModel_Mixed_Elasticity( std::unique_ptr<IMaterialModel_vol> in_vmodel,
+       std::unique_ptr<IMaterialModel_ich> in_imodel ) 
+      : vmodel(std::move(in_vmodel)), imodel(std::move(in_imodel)) {};
 
-    virtual ~IMaterialModel_mixed() = default;
+    virtual ~MaterialModel_Mixed_Elasticity() = default;
 
-    virtual void print_info() const = 0;
-
-    virtual std::string get_model_name() const
+    void print_info() const
     {
-      SYS_T::commPrint("Warning: IMaterialModel_mixed::get_model_name() is not implemented. \n");
-      return std::string("unknown");
+      SYS_T::commPrint("\t  Isochoric part: \n");
+      imodel->print_info();
+      SYS_T::commPrint("\t  Volumetric part: \n");
+      vmodel->print_info();
     }
 
-    virtual SymmTensor2_3D get_PK_2nd( const Tensor2_3D &F ) const = 0;
+    std::string get_model_name() const
+    {return imodel->get_model_name() + vmodel->get_model_name();}
 
-    virtual SymmTensor4_3D get_PK_Stiffness( const Tensor2_3D &F,
-       Tensor2_3D &P_iso ) const = 0;
+    SymmTensor2_3D get_PK_2nd( const Tensor2_3D &F ) const 
+    {return imodel->get_PK_2nd(F);}
+
+    SymmTensor4_3D get_PK_Stiffness( const Tensor2_3D &F, Tensor2_3D &P_iso ) const
+    {return imodel->get_PK_Stiffness(F, P_iso);}
 
     // P_iso := F S_iso
-    virtual Tensor2_3D get_PK_1st( const Tensor2_3D &F ) const
-    {
-      return F * get_PK_2nd(F);
-    }
+    Tensor2_3D get_PK_1st( const Tensor2_3D &F ) const
+    {return imodel->get_PK_1st(F);}
    
-    virtual SymmTensor2_3D get_Cauchy_stress( const Tensor2_3D &F ) const
-    {
-      auto out = get_PK_2nd(F);
-      out.push_forward_stress(F);
-      return (1.0/F.det()) * out;
-    }
+    SymmTensor2_3D get_Cauchy_stress( const Tensor2_3D &F ) const
+    {return imodel->get_Cauchy_stress(F);}
 
-    virtual Tensor4_3D get_PK_FFStiffness( const Tensor2_3D &F,
-       Tensor2_3D &P_iso ) const
-    {
-      auto AA = get_PK_Stiffness(F, P_iso).full();
-      AA.MatMult_1(F);
-      AA.MatMult_3(F);
-      return AA;
-    }
+    Tensor4_3D get_PK_FFStiffness( const Tensor2_3D &F, Tensor2_3D &P_iso ) const
+    {return imodel->get_PK_FFStiffness(F, P_iso);}
 
-    virtual double get_elastic_mu() const
-    {
-      SYS_T::commPrint("Warning: IMaterialModel_mixed::get_elastic_mu() is not implemented. \n");
-      return 0.0;
-    }
+    double get_elastic_mu() const {return imodel->get_elastic_mu();}
 
     double get_rho_0() const {return vmodel->get_rho_0();}
     
@@ -67,8 +56,9 @@ class MaterialModel_Mixed_Elasticity
 
     double get_elastic_kappa() const {return vmodel->get_elastic_kappa();}
 
-  protected:
-    std::unique_ptr<IMaterialModel_vol> vmodel;
+  private:
+    const std::unique_ptr<IMaterialModel_vol> vmodel;
+    const std::unique_ptr<IMaterialModel_ich> imodel;
 };
 
 #endif
