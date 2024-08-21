9934ab7aedf3cb2394c5506d05f7ce57469309db
guanjiashen <1427880824@qq.com>
2024-08-21 15:36:27 +0800

    in

--- examples/test/matmodel_test.cpp
+++ examples/test/matmodel_test.cpp
@@ -18,33 +18,33 @@ int main( int argc, char * argv[] )
 
   MaterialModel_ich_StVenant_Kirchhoff mat1( mu );
 
-  const auto S1_iso = mat1.get_PK_2nd( F );
+  const auto S1_ich = mat1.get_PK_2nd( F );
 
-  Tensor2_3D P1_iso;
+  Tensor2_3D P1_ich;
 
-  const auto CC1_iso = mat1.get_PK_Stiffness( F, P1_iso );
+  const auto CC1_ich = mat1.get_PK_Stiffness( F, P1_ich );
 
   MaterialModel_StVenant_Kirchhoff_M94_Mixed mat2( E, nu );
 
-  Tensor2_3D P2_iso, S2_iso;
-  Tensor4_3D CC2_iso;
+  Tensor2_3D P2_ich, S2_ich;
+  Tensor4_3D CC2_ich;
 
-  mat2.get_PK_Stiffness( F, P2_iso, S2_iso, CC2_iso );
+  mat2.get_PK_Stiffness( F, P2_ich, S2_ich, CC2_ich );
 
   const auto Sym_S2iso = SymmTensor2_3D( 
-    S2_iso(0, 0), S2_iso(1, 1), S2_iso(2, 2),
-    S2_iso(1, 2), S2_iso(0, 2), S2_iso(0, 1) );
+    S2_ich(0, 0), S2_ich(1, 1), S2_ich(2, 2),
+    S2_ich(1, 2), S2_ich(0, 2), S2_ich(0, 1) );
 
   std::cout << "StVenant: \n";
 
-  std::cout << "error for S_iso: \n"; 
-  ( S1_iso - Sym_S2iso ).print();
+  std::cout << "error for S_ich: \n"; 
+  ( S1_ich - Sym_S2iso ).print();
 
-  std::cout << "error for P_iso: \n"; 
-  ( P1_iso - P2_iso ).print();
+  std::cout << "error for P_ich: \n"; 
+  ( P1_ich - P2_ich ).print();
 
-  std::cout << "error for CC_iso: \n"; 
-  ( CC1_iso.full() - CC2_iso ).print_in_mat();
+  std::cout << "error for CC_ich: \n"; 
+  ( CC1_ich.full() - CC2_ich ).print_in_mat();
 
 
 
@@ -55,34 +55,34 @@ int main( int argc, char * argv[] )
   MaterialModel_ich_GOH14 mat3( mu, f1_the, f1_phi, f2_the, f2_phi,
     fk1, fk2, fkd );
 
-  const auto S3_iso = mat3.get_PK_2nd( F );
+  const auto S3_ich = mat3.get_PK_2nd( F );
 
-  Tensor2_3D P3_iso;
+  Tensor2_3D P3_ich;
 
-  const auto CC3_iso = mat3.get_PK_Stiffness( F, P3_iso );
+  const auto CC3_ich = mat3.get_PK_Stiffness( F, P3_ich );
 
   MaterialModel_GOH14_ST91_Mixed mat4( 1.0, E, nu, f1_the, f1_phi, f2_the, f2_phi,
     fk1, fk2, fkd );
 
-  Tensor2_3D P4_iso, S4_iso;
-  Tensor4_3D CC4_iso;
+  Tensor2_3D P4_ich, S4_ich;
+  Tensor4_3D CC4_ich;
 
-  mat4.get_PK_Stiffness( F, P4_iso, S4_iso, CC4_iso );
+  mat4.get_PK_Stiffness( F, P4_ich, S4_ich, CC4_ich );
 
   const auto Sym_S4iso = SymmTensor2_3D( 
-    S4_iso(0, 0), S4_iso(1, 1), S4_iso(2, 2),
-    S4_iso(1, 2), S4_iso(0, 2), S4_iso(0, 1) );
+    S4_ich(0, 0), S4_ich(1, 1), S4_ich(2, 2),
+    S4_ich(1, 2), S4_ich(0, 2), S4_ich(0, 1) );
 
-  std::cout << "\nGOH06: \n";
+  std::cout << "\nGOH14: \n";
 
-  std::cout << "error for S_iso: \n"; 
-  ( S3_iso - Sym_S4iso ).print();
+  std::cout << "error for S_ich: \n"; 
+  ( S3_ich - Sym_S4iso ).print();
 
-  std::cout << "error for P_iso: \n"; 
-  ( P3_iso - P4_iso ).print();
+  std::cout << "error for P_ich: \n"; 
+  ( P3_ich - P4_ich ).print();
 
-  std::cout << "error for CC_iso: \n"; 
-  ( CC3_iso.full() - CC4_iso ).print_in_mat();
+  std::cout << "error for CC_ich: \n"; 
+  ( CC3_ich.full() - CC4_ich ).print_in_mat();
 
 
 
