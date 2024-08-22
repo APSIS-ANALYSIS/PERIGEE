65ef4012f9f443286fbcbb73973de8116954029c
Ju Liu <liujuy@gmail.com>
2024-08-22 08:57:44 +0800

    final print error format

--- examples/test/sys_test.cpp
+++ examples/test/sys_test.cpp
@@ -74,18 +74,18 @@ int main(int argc, char *argv[])
   //oldmodel->print_info();
   
   matmodel->get_fibre_dir(0).print();
-  matmodel->get_fibre_dir(2).print();
+  matmodel->get_fibre_dir(1).print();
 
   Tensor2_3D F; F.gen_rand();
 
   if(F.det() < 0.0) F*=-1.0;
 
-  std::cout<<"F:"; 
+  std::cout<<"F: \t"; 
   F.print_in_row();
 
   const double val = std::pow(F.det(), -1.0/3.0);
 
-  std::cout<<"detF = "<<val<<"\n\n";
+  std::cout<<"detF = \t"<<val<<"\n\n";
 
   //F *= val; 
   
@@ -95,8 +95,8 @@ int main(int argc, char *argv[])
   //S_old -= S_new;
   auto S_new = matmodel->get_PK_2nd(F);
   S_old -= S_new.full();
-  std::cout<<"PK_2d diff: "<<std::sqrt(S_old.MatContraction(S_old))<<'\n';
-  std::cout<<"PK_2d valu: "<<std::sqrt(S_new.MatContraction(S_new))<<'\n';
+  std::cout<<"PK_2d diff: \t"<<std::sqrt(S_old.MatContraction(S_old))<<'\n';
+  std::cout<<"PK_2d valu: \t"<<std::sqrt(S_new.MatContraction(S_new))<<"\n\n";
 
   //S_old.print_in_row();
 
@@ -105,14 +105,14 @@ int main(int argc, char *argv[])
   
   auto sigma_new = matmodel->get_Cauchy_stress(F);
   sigma_old -= sigma_new.full();
-  std::cout<<"cauchy diff: "<<std::sqrt(sigma_old.MatContraction(sigma_old))<<'\n';
-  std::cout<<"cauchy valu: "<<std::sqrt(sigma_new.MatContraction(sigma_new))<<'\n';
+  std::cout<<"cauchy diff: \t"<<std::sqrt(sigma_old.MatContraction(sigma_old))<<'\n';
+  std::cout<<"cauchy valu: \t"<<std::sqrt(sigma_new.MatContraction(sigma_new))<<"\n\n";
 
   //P_old -= P_new
   auto P_new = matmodel->get_PK_1st(F);
   P_old -= P_new;
-  std::cout<<"PK_1t diff: "<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
-  std::cout<<"PK_1t value: "<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
+  std::cout<<"PK_1t diff: \t"<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
+  std::cout<<"PK_1t valu: \t"<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
 
   //CC_old -= CC_new
   std::cout<<"\nget_PK_Stiffness:"<<std::endl; 
@@ -121,10 +121,10 @@ int main(int argc, char *argv[])
   auto CC_new = matmodel->get_PK_Stiffness(F, P_new);
   CC_old -= CC_new.full();
   std::cout<<"CC diff:\t"<<std::sqrt(CC_old.Ten4Contraction(CC_old))<<'\n';
-  std::cout<<"CC new: \t"<<std::sqrt(CC_new.full().Ten4Contraction(CC_new.full()))<<'\n';
+  std::cout<<"CC new: \t"<<std::sqrt(CC_new.full().Ten4Contraction(CC_new.full()))<<"\n\n";
   P_old -= P_new;
-  std::cout<<"PK_1s diff:"<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
-  std::cout<<"PK_1s value:"<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
+  std::cout<<"PK_1s diff: \t"<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
+  std::cout<<"PK_1s valu: \t"<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
 
   std::cout<<"\nget_PK_FFStiffness:"<<std::endl; 
   oldmodel->get_PK_FFStiffness(F, P_old, S_old, CC_old);
@@ -133,51 +133,51 @@ int main(int argc, char *argv[])
   //CC_old.print_in_mat();
   //CC_n.print_in_mat();
   //std::cout<<std::endl; 
-  std::cout<<"CC diff:\t"<<std::sqrt(CC_old.Ten4Contraction(CC_old))<<'\n';
-  std::cout<<"CC new: \t"<<std::sqrt(CC_n.Ten4Contraction(CC_n))<<'\n';
+  std::cout<<"CC diff: \t"<<std::sqrt(CC_old.Ten4Contraction(CC_old))<<'\n';
+  std::cout<<"CC new: \t"<<std::sqrt(CC_n.Ten4Contraction(CC_n))<<"\n\n";
   P_old -= P_new;
-  std::cout<<"PK_1st diff:"<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
-  std::cout<<"PK_1st value:"<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
+  std::cout<<"PK_1st diff: \t"<<std::sqrt(P_old.MatContraction(P_old))<<'\n';
+  std::cout<<"PK_1st valu: \t"<<std::sqrt(P_new.MatContraction(P_new))<<'\n';
   std::cout<<std::endl;
   S_old -= S_new.full();
-  std::cout<<"PK_2d diff:"<<std::sqrt(S_old.MatContraction(S_old))<<'\n';
-  std::cout<<"PK_2d value:"<<std::sqrt(S_new.MatContraction(S_new))<<'\n';
+  std::cout<<"PK_2d diff: \t"<<std::sqrt(S_old.MatContraction(S_old))<<'\n';
+  std::cout<<"PK_2d valu: \t"<<std::sqrt(S_new.MatContraction(S_new))<<'\n';
 
   double p = MATH_T::gen_double_rand(-1.0,1.0);
   // rho
-  std::cout<<"\n\nget_rho: "; 
+  std::cout<<"\nget_rho: "; 
   auto rho_old = oldmodel->get_rho(p);
   auto rho_new = matmodel->get_rho(p);
   rho_old -= rho_new;
-  std::cout<<rho_old<<'\t'<<rho_new<<std::endl;
+  std::cout<<'\t'<<rho_old<<'\t'<<rho_new<<std::endl;
 
   // beta
   std::cout<<"get_beta: "; 
   auto beta_old = oldmodel->get_beta(p);
   auto beta_new = matmodel->get_beta(p);
   beta_old -= beta_new;
-  std::cout<<beta_old<<'\t'<<beta_new<<std::endl;
+  std::cout<<'\t'<<beta_old<<'\t'<<beta_new<<std::endl;
 
   // drho_dp
   std::cout<<"get_drho_dp: "; 
   auto drho_dp_old = oldmodel->get_drho_dp(p);
   auto drho_dp_new = matmodel->get_drho_dp(p);
   drho_dp_old -= drho_dp_new;
-  std::cout<<drho_dp_old<<'\t'<<drho_dp_new<<std::endl;
+  std::cout<<'\t'<<drho_dp_old<<'\t'<<drho_dp_new<<std::endl;
 
   // dbeta_dp
   std::cout<<"get_dbeta_dp: "; 
   auto dbeta_dp_old = oldmodel->get_dbeta_dp(p);
   auto dbeta_dp_new = matmodel->get_dbeta_dp(p);
   dbeta_dp_old -= dbeta_dp_new;
-  std::cout<<dbeta_dp_old<<'\t'<<dbeta_dp_new<<std::endl;
+  std::cout<<'\t'<<dbeta_dp_old<<'\t'<<dbeta_dp_new<<std::endl;
 
   // strain_energy
   std::cout<<"get_strain_energy: "; 
   auto strain_energy_old = oldmodel->get_strain_energy(F);
   auto strain_energy_new = matmodel->get_ich_energy(F);
   strain_energy_old -= strain_energy_new;
-  std::cout<<strain_energy_old<<'\t'<<strain_energy_new<<std::endl;
+  std::cout<<'\t'<<strain_energy_old<<'\t'<<strain_energy_new<<std::endl;
 
   return EXIT_SUCCESS;
 }
