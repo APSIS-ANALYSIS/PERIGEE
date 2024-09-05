48562cf5edac7a8948a929b9bb8fc9fd6c00c4c9
Ju Liu <liujuy@gmail.com>
2024-09-05 21:01:27 +0800

    better element tet4 using override to replace virtual

--- include/FEAElement_Tet4.hpp
+++ include/FEAElement_Tet4.hpp
@@ -15,66 +15,66 @@
 class FEAElement_Tet4 : public FEAElement
 {
   public:
-    FEAElement_Tet4( const int &in_nqua );
+    FEAElement_Tet4( const int &in_nqua = 5 );
 
-    virtual ~FEAElement_Tet4() = default;
+    ~FEAElement_Tet4() override = default;
  
-    virtual int get_elemDim() const {return 3;}
+    int get_elemDim() const override {return 3;}
 
     // A unique number for this element.
-    virtual int get_Type() const {return 501;}
+    int get_Type() const override {return 501;}
 
-    virtual int get_numQuapts() const {return numQuapts;}
+    int get_numQuapts() const override {return numQuapts;}
 
-    virtual int get_nLocBas() const {return 4;}
+    int get_nLocBas() const override {return 4;}
 
     // Given the quadrature points and nodal coordinates, evaluate the basis 
     // functions and their derivatives up to second order
-    virtual void buildBasis( const IQuadPts * const &quad_rule,
+    void buildBasis( const IQuadPts * const &quad_rule,
         const double * const &ctrl_x,
         const double * const &ctrl_y,
-        const double * const &ctrl_z );
+        const double * const &ctrl_z ) override;
 
     // Return the element length.
     // For the linear tet element, we calculate the DIAMETER of the
     // circumscribing sphere
-    virtual double get_h( const double * const &ctrl_x,
+    double get_h( const double * const &ctrl_x,
         const double * const &ctrl_y,
-        const double * const &ctrl_z ) const;
+        const double * const &ctrl_z ) const override;
 
     // Get functions give access to function evaluations at the quadrature point 
     // corresponding to quaindex
-    virtual void get_R( const int &quaindex, double * const &basis ) const;
+    void get_R( const int &quaindex, double * const &basis ) const override;
 
-    virtual std::vector<double> get_R( const int &quaindex ) const;
+    std::vector<double> get_R( const int &quaindex ) const override;
 
-    virtual void get_gradR( const int &quaindex, double * const &basis_x,
-        double * const &basis_y, double * const &basis_z ) const;
+    void get_gradR( const int &quaindex, double * const &basis_x,
+        double * const &basis_y, double * const &basis_z ) const override;
 
-    virtual void get_R_gradR( const int &quaindex, double * const &basis,
+    void get_R_gradR( const int &quaindex, double * const &basis,
         double * const &basis_x, double * const &basis_y,
-        double * const &basis_z ) const;
+        double * const &basis_z ) const override;
 
     // Get the Jacobian matrix dx/dr
-    virtual void get_Jacobian(const int &quaindex, double * const &jac_value) const;
+    void get_Jacobian(const int &quaindex, double * const &jac_value) const override;
 
-    virtual std::array<double,9> get_Jacobian( const int &quaindex ) const
+    std::array<double,9> get_Jacobian( const int &quaindex ) const override
     {
       ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet4::get_Jacobian function error.\n"  );
       return {{Jac[0], Jac[1], Jac[2], Jac[3], Jac[4], Jac[5], Jac[6], Jac[7], Jac[8]}};
     }
 
     // Get the inverse Jacobian matrix dr/dx
-    virtual void get_invJacobian(const int &quaindex, double * const &jac_value) const;
+    void get_invJacobian(const int &quaindex, double * const &jac_value) const override;
 
-    virtual std::array<double,9> get_invJacobian( const int &quaindex ) const
+    std::array<double,9> get_invJacobian( const int &quaindex ) const override
     {
       ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet4::get_invJacobian function error.\n"  );
       return {{Jac[9], Jac[10], Jac[11], Jac[12], Jac[13], Jac[14], Jac[15], Jac[16], Jac[17]}};
     }
 
     // Get the determinant of the Jacobian matrix
-    virtual double get_detJac(const int &quaindex) const {return detJac;}
+    double get_detJac(const int &quaindex) const override {return detJac;}
 
     // Build basis and build the boundary element
     //   Tet-Face-0 : Node 1 2 3
@@ -95,10 +95,10 @@ class FEAElement_Tet4 : public FEAElement
     //virtual Vector_3 get_2d_normal_out( const int &quaindex, double &area ) const
     //{return triangle_face->get_2d_normal_out( quaindex, area );}
 
-    virtual std::array<std::vector<double>, 3> get_face_ctrlPts( const int &face_id,
+    std::array<std::vector<double>, 3> get_face_ctrlPts( const int &face_id,
         const double * const &volctrl_x,
         const double * const &volctrl_y,
-        const double * const &volctrl_z );
+        const double * const &volctrl_z ) override;
 
   private:
     // Number of quadrature points
