04c6b8c67d24e70745a06eed51d295051aa4c8a9
Ju Liu <liujuy@gmail.com>
2024-09-08 21:29:31 +0800

    AGlobal mesh info simplified

--- include/AGlobal_Mesh_Info_FEM_3D.hpp
+++ include/AGlobal_Mesh_Info_FEM_3D.hpp
@@ -10,54 +10,74 @@
 #include "IAGlobal_Mesh_Info.hpp"
 #include "HDF5_Reader.hpp"
 
-class AGlobal_Mesh_Info_FEM_3D : public IAGlobal_Mesh_Info
+class AGlobal_Mesh_Info_FEM_3D final : public IAGlobal_Mesh_Info
 {
   public:
     AGlobal_Mesh_Info_FEM_3D( const std::string &fileBaseName,
-        const int &cpu_rank );
+        const int &cpu_rank )
+    {
+      std::string fName = SYS_T::gen_partfile_name( fileBaseName, cpu_rank );
 
-    // Construct global mesh info by enriching each cell with 
-    // additional bubble nodes
-    // Input:  num_enrich_node : the number of enriched nodes per cell
-    // Output: nFunc = original nFunc + nElem * num_enrich_node
-    //         nLocBas = nLocBas + num_enrich_node
-    //         elemType = elemType + 10
-    AGlobal_Mesh_Info_FEM_3D( const std::string &fileBaseName,
-        const int &cpu_rank, const int &num_enrich_node );
-    
-    virtual ~AGlobal_Mesh_Info_FEM_3D() = default;
+      hid_t file_id = H5Fopen( fName.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT );
+
+      HDF5_Reader * h5r = new HDF5_Reader( file_id );
+
+      std::vector<int> vdeg = h5r -> read_intVector("Global_Mesh_Info", "degree");
+
+      xdegree = vdeg[0]; ydegree = vdeg[1]; zdegree = vdeg[2];
+
+      nElem    = h5r -> read_intScalar("Global_Mesh_Info", "nElem");
+      nFunc    = h5r -> read_intScalar("Global_Mesh_Info", "nFunc");
+      nLocBas  = h5r -> read_intScalar("Global_Mesh_Info", "nLocBas");
+      probDim  = h5r -> read_intScalar("Global_Mesh_Info", "probDim");
+      elemType = h5r -> read_intScalar("Global_Mesh_Info", "elemType");
+
+      delete h5r;
+      H5Fclose( file_id );
+    }
+
+    ~AGlobal_Mesh_Info_FEM_3D() override = default;
 
     // Get the polynomial degree for the discretization method. For
     // unstructured mesh, the three function should return the same
     // value since one cannot differentiate the xyz direction in, e.g.,
     // tet mesh.
-    virtual int get_xdegree() const {return xdegree;}
-    virtual int get_ydegree() const {return ydegree;}
-    virtual int get_zdegree() const {return zdegree;}
+    int get_xdegree() const override {return xdegree;}
+    int get_ydegree() const override {return ydegree;}
+    int get_zdegree() const override {return zdegree;}
 
     // Get the total number of element of the whole mesh. 
-    virtual int get_nElem() const {return nElem;}
+    int get_nElem() const override {return nElem;}
 
     // Get the total number of nodes of the whole mesh.
-    virtual int get_nFunc() const {return nFunc;}
+    int get_nFunc() const override {return nFunc;}
 
     // Get the number of local basis functions for the element.
     // Note: this implicitly implies that we use the same type of element
     //       for the mesh.
-    virtual int get_nLocBas() const {return nLocBas;}
+    int get_nLocBas() const override {return nLocBas;}
 
     // Get the dimension of the problem.
-    virtual int get_probDim() const {return probDim;}
+    int get_probDim() const override {return probDim;}
 
     // Get an integer that indicate the element type.
-    virtual int get_elemType() const {return elemType;}
+    int get_elemType() const override {return elemType;}
 
-    virtual void print_info() const;
+    void print_info() const override
+    {
+      std::cout<<"AGlobal_Mesh_Info_FEM_3D:"<<std::endl;
+      std::cout<<"degree: "<<xdegree<<'\t'<<ydegree<<'\t'<<zdegree<<'\n';
+      std::cout<<"nElem: "<<nElem<<'\n';
+      std::cout<<"nFunc: "<<nFunc<<'\n';
+      std::cout<<"nLocBas: "<<nLocBas<<std::endl;
+      std::cout<<"probDim: "<<probDim<<std::endl;
+      std::cout<<"elemType: "<<get_elemType()<<std::endl;
+    }
 
   private:
     int xdegree, ydegree, zdegree;
     int nElem, nFunc, nLocBas, probDim, elemType;
-    
+
     AGlobal_Mesh_Info_FEM_3D() = delete;
 };
 
