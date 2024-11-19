869feadbe4355cc7c3c61df04af6fbe605ccf131
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-11-19 20:40:59 +0800

    undo a change on  inflow_rate_ptr

--- examples/ns/ns_driver.cpp
+++ examples/ns/ns_driver.cpp
@@ -291,8 +291,9 @@ int main(int argc, char *argv[])
   // if( SYS_T::file_exist( inflow_file ) )
   //   inflow_rate_ptr = new CVFlowRate_Unsteady( inflow_file.c_str() );
   // else
-  inflow_rate_ptr = new CVFlowRate_Cosine2Steady( 1, inflow_thd_time, inflow_tgt_rate, inflow_TI_perturbation );
-
+  inflow_rate_ptr = new CVFlowRate_Cosine2Steady( inflow_thd_time, inflow_TI_perturbation, 
+      inflow_file );
+  
   inflow_rate_ptr->print_info();
 
   // ===== Finite Element Container & Quadrature rules =====
