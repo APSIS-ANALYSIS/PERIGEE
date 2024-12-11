ece9495ad57bc8bf4cb40bffb4a36723ee14bb2a
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-12-11 16:22:48 +0800

    minor in ns/preprocess

--- examples/ns/preprocess_ns.cpp
+++ examples/ns/preprocess_ns.cpp
@@ -118,7 +118,7 @@ int main( int argc, char * argv[] )
   {
     if(elemType == FEType::Tet4 || elemType == FEType::Hex8)
       sur_file_out[ii] = SYS_T::gen_capfile_name( sur_file_out_base, ii, ".vtp" ); 
-    else if(elemType == FEType::Tet10 ||elemType == FEType::Hex27)
+    else if(elemType == FEType::Tet10 || elemType == FEType::Hex27)
       sur_file_out[ii] = SYS_T::gen_capfile_name( sur_file_out_base, ii, ".vtu" ); 
     else
       SYS_T::print_fatal("Error: unknown element type occurs when generating the outlet file names. \n");
