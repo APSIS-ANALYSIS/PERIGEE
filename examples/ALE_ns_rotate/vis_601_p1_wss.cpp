1870a2b51e9d01d70adc80f8d9c307e4442476a5
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-12-14 09:03:18 +0800

    update ALE/vis_hex

--- examples/ALE_ns_rotate/vis_601_p1_wss.cpp
+++ examples/ALE_ns_rotate/vis_601_p1_wss.cpp
@@ -1,10 +1,10 @@
-// ==================================================================
-// vis_601_p1_wss.cpp
+// ============================================================================
+// vis_hex8_wss.cpp
 //
 // WSS visualization for 8-node hex elements.
 //
 // Date: Oct 24 2023
-// ==================================================================
+// ============================================================================
 #include "Hex_Tools.hpp"
 #include "QuadPts_vis_quad4.hpp"
 #include "QuadPts_Gauss_Quad.hpp"
@@ -68,7 +68,9 @@ int main( int argc, char * argv[] )
   HDF5_Reader * cmd_h5r = new HDF5_Reader( prepcmd_file );
   const std::string geo_file  = cmd_h5r -> read_string("/", "geo_file");
   const std::string wall_file = cmd_h5r -> read_string("/", "sur_file_wall");
-  const int elemType = cmd_h5r -> read_intScalar("/", "elemType");
+  const std::string elemType_str = cmd_h5r -> read_string("/", "elemType");
+
+  const FEType elemType = FE_T::to_FEType(elemType_str);
 
   delete cmd_h5r; H5Fclose(prepcmd_file);
 
@@ -82,7 +84,7 @@ int main( int argc, char * argv[] )
   delete cmd_h5r; H5Fclose(prepcmd_file);
 
   // Enforce the element to be trilinear hex for now
-  if( elemType != 601 ) SYS_T::print_fatal("Error: element type should be 601 trilinear hex element.\n");
+  if( elemType != FEType::Hex8 ) SYS_T::print_fatal("Error: element type should be trilinear hex element.\n");
 
   SYS_T::GetOptionString("-sol_bname", sol_bname);
   SYS_T::GetOptionInt("-time_start", time_start);
@@ -101,7 +103,7 @@ int main( int argc, char * argv[] )
   cout<<"----------------------------------\n";
   cout<<" geo_file: "<<geo_file<<endl;
   cout<<" wall_file: "<<wall_file<<endl;
-  cout<<" elemType: "<<elemType<<endl;
+  cout<<" elemType: "<<elemType_str<<endl;
   cout<<" out_bname: "<<out_bname<<endl;
   cout<<" fl_mu: "<<fluid_mu<<endl;
   cout<<"==== Command Line Arguments ===="<<endl;
