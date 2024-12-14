efc73547903b5714931bc999bb6b872263122cd7
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-12-14 10:42:43 +0800

    remove 501 and 601 from elasto's comments

--- examples/linearPDE/elastodynamics/driver_elastodynamics.cpp
+++ examples/linearPDE/elastodynamics/driver_elastodynamics.cpp
@@ -219,7 +219,7 @@ int main(int argc, char *argv[])
     if( nqp_vol > 5 ) SYS_T::commPrint("Warning: the tet element is linear and you are using more than 5 quadrature points.\n");
     if( nqp_sur > 4 ) SYS_T::commPrint("Warning: the tri element is linear and you are using more than 4 quadrature points.\n");
 
-    elementv = new FEAElement_Tet4( nqp_vol ); // elem type 501
+    elementv = new FEAElement_Tet4( nqp_vol ); // elem type Tet4
     elements = new FEAElement_Triangle3_3D_der0( nqp_sur );
     quadv = new QuadPts_Gauss_Tet( nqp_vol );
     quads = new QuadPts_Gauss_Triangle( nqp_sur );
@@ -229,7 +229,7 @@ int main(int argc, char *argv[])
     SYS_T::print_fatal_if( nqp_vol < 29, "Error: not enough quadrature points for tets.\n" );
     SYS_T::print_fatal_if( nqp_sur < 13, "Error: not enough quadrature points for triangles.\n" );
 
-    elementv = new FEAElement_Tet10( nqp_vol ); // elem type 502
+    elementv = new FEAElement_Tet10( nqp_vol ); // elem type Tet10
     elements = new FEAElement_Triangle6_3D_der0( nqp_sur );
     quadv = new QuadPts_Gauss_Tet( nqp_vol );
     quads = new QuadPts_Gauss_Triangle( nqp_sur );
@@ -239,7 +239,7 @@ int main(int argc, char *argv[])
     SYS_T::print_fatal_if( nqp_vol_1D < 2, "Error: not enough quadrature points for hex.\n" );
     SYS_T::print_fatal_if( nqp_sur_1D < 1, "Error: not enough quadrature points for quad.\n" );
 
-    elementv = new FEAElement_Hex8( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type 601
+    elementv = new FEAElement_Hex8( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type Hex8
     elements = new FEAElement_Quad4_3D_der0( nqp_sur_1D * nqp_sur_1D );
     quadv = new QuadPts_Gauss_Hex( nqp_vol_1D );
     quads = new QuadPts_Gauss_Quad( nqp_sur_1D );
@@ -249,7 +249,7 @@ int main(int argc, char *argv[])
     SYS_T::print_fatal_if( nqp_vol_1D < 4, "Error: not enough quadrature points for hex.\n" );
     SYS_T::print_fatal_if( nqp_sur_1D < 3, "Error: not enough quadrature points for quad.\n" );
 
-    elementv = new FEAElement_Hex27( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type 602
+    elementv = new FEAElement_Hex27( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type Hex27
     elements = new FEAElement_Quad9_3D_der0( nqp_sur_1D * nqp_sur_1D );
     quadv = new QuadPts_Gauss_Hex( nqp_vol_1D );
     quads = new QuadPts_Gauss_Quad( nqp_sur_1D );
