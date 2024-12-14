6ff1a0f5c0f2f9a9cb038c605329dde37362dc92
Chi Ding <55510028+Mr-Chi-Ding@users.noreply.github.com>
2024-12-14 10:18:47 +0800

    update ALE/PLoc_Assem_WeakBC

--- examples/ALE_ns_rotate/src/PLocAssem_VMS_NS_GenAlpha_WeakBC.cpp
+++ examples/ALE_ns_rotate/src/PLocAssem_VMS_NS_GenAlpha_WeakBC.cpp
@@ -5,7 +5,7 @@ PLocAssem_VMS_NS_GenAlpha_WeakBC::PLocAssem_VMS_NS_GenAlpha_WeakBC(
         const int &in_nlocbas, const int &in_nqp,
         const int &in_snlocbas,
         const double &in_rho, const double &in_vis_mu,
-        const double &in_beta, const int &elemtype,
+        const double &in_beta, const FEType &elemtype,
         const double &angular,
         const Vector_3 &point_xyz, const Vector_3 &angular_direc,
         const double &in_ct, const double &in_ctauc,
