5a8786fa49ebcaa2e47c7adc4441a58a19cd8bac
SeaVegetable <1135167789@qq.com>
2024-12-17 14:59:32 +0800

    format

--- include/FEAElement_Hex27.hpp
+++ include/FEAElement_Hex27.hpp
@@ -171,8 +171,8 @@ class FEAElement_Hex27 final : public FEAElement
     const int numQuapts;
 
     // R : 0 <= ii < 27 x numQuapts
-    std::vector<double> R{}, dR_dx{}, dR_dy{}, dR_dz{},
-        d2R_dxx{}, d2R_dyy{}, d2R_dzz{}, d2R_dxy{}, d2R_dxz{}, d2R_dyz{};
+    std::vector<double> R {}, dR_dx {}, dR_dy {}, dR_dz {},
+        d2R_dxx {}, d2R_dyy {}, d2R_dzz {}, d2R_dxy {}, d2R_dxz {}, d2R_dyz {};
 
     // Container for
     // dx_dr : 0 <= ii < 9 numQuapts
