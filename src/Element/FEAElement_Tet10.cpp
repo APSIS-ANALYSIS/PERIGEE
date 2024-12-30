19db9f2b8b88397cc54de013e4e17997771b1aa1
Ju Liu <liujuy@gmail.com>
2024-12-30 14:13:50 +0800

    tet10 updated

--- src/Element/FEAElement_Tet10.cpp
+++ src/Element/FEAElement_Tet10.cpp
@@ -3,18 +3,18 @@
 FEAElement_Tet10::FEAElement_Tet10( const int &in_nqua ) : numQuapts( in_nqua ) ,
   triangle_face( SYS_T::make_unique<FEAElement_Triangle6_3D_der0>(numQuapts) )
 {
-  R.resize(10 * numQuapts);
+  R.resize(nLocBas * numQuapts);
 
-  dR_dx.resize(10 * numQuapts);
-  dR_dy.resize(10 * numQuapts);
-  dR_dz.resize(10 * numQuapts);
+  dR_dx.resize(nLocBas * numQuapts);
+  dR_dy.resize(nLocBas * numQuapts);
+  dR_dz.resize(nLocBas * numQuapts);
 
-  d2R_dxx.resize(10 * numQuapts);
-  d2R_dyy.resize(10 * numQuapts);
-  d2R_dzz.resize(10 * numQuapts);
-  d2R_dxy.resize(10 * numQuapts);
-  d2R_dxz.resize(10 * numQuapts);
-  d2R_dyz.resize(10 * numQuapts);
+  d2R_dxx.resize(nLocBas * numQuapts);
+  d2R_dyy.resize(nLocBas * numQuapts);
+  d2R_dzz.resize(nLocBas * numQuapts);
+  d2R_dxy.resize(nLocBas * numQuapts);
+  d2R_dxz.resize(nLocBas * numQuapts);
+  d2R_dyz.resize(nLocBas * numQuapts);
 
   dx_dr.resize(9 * numQuapts);
   dr_dx.resize(9 * numQuapts);
@@ -50,7 +50,7 @@ void FEAElement_Tet10::buildBasis( const IQuadPts * const &quad,
   double yrr = 0.0, yss = 0.0, ytt = 0.0, yrs = 0.0, yrt = 0.0, yst = 0.0;
   double zrr = 0.0, zss = 0.0, ztt = 0.0, zrs = 0.0, zrt = 0.0, zst = 0.0;
 
-  for(int ii=0; ii<10; ++ii)
+  for(int ii=0; ii<nLocBas; ++ii)
   {
     xrr += ctrl_x[ii] * d2R_drr[ii];
     xss += ctrl_x[ii] * d2R_dss[ii];
@@ -76,7 +76,7 @@ void FEAElement_Tet10::buildBasis( const IQuadPts * const &quad,
 
   for(int qua=0; qua<numQuapts; ++qua)
   {
-    const int q10 = qua * 10;
+    const int q10 = qua * nLocBas;
 
     const double qua_r = quad -> get_qp( qua, 0 );
     const double qua_s = quad -> get_qp( qua, 1 );
@@ -106,7 +106,7 @@ void FEAElement_Tet10::buildBasis( const IQuadPts * const &quad,
     double xr = 0.0, xs = 0.0, xt = 0.0;
     double yr = 0.0, ys = 0.0, yt = 0.0;
     double zr = 0.0, zs = 0.0, zt = 0.0;
-    for(int ii=0; ii<10; ++ii)
+    for(int ii=0; ii<nLocBas; ++ii)
     {
       xr += ctrl_x[ii] * dR_dr[ii];
       xs += ctrl_x[ii] * dR_ds[ii];
@@ -141,7 +141,7 @@ void FEAElement_Tet10::buildBasis( const IQuadPts * const &quad,
     dr_dx[9*qua + 7] = mdrdx(7); // dt_dy
     dr_dx[9*qua + 8] = mdrdx(8); // dt_dz
 
-    for(int ii=0; ii<10; ++ii)
+    for(int ii=0; ii<nLocBas; ++ii)
     {
       dR_dx[q10+ii] = dR_dr[ii]*mdrdx(0) + dR_ds[ii]*mdrdx(3) + dR_dt[ii]*mdrdx(6); 
       dR_dy[q10+ii] = dR_dr[ii]*mdrdx(1) + dR_ds[ii]*mdrdx(4) + dR_dt[ii]*mdrdx(7); 
@@ -154,7 +154,7 @@ void FEAElement_Tet10::buildBasis( const IQuadPts * const &quad,
     // LU factorization
     LHS.LU_fac();
 
-    for(int ii=0; ii<10; ++ii)
+    for(int ii=0; ii<nLocBas; ++ii)
     {
       const std::array<double, 6> RHS {{ d2R_drr[ii] - dR_dx[q10+ii] * xrr - dR_dy[q10+ii] * yrr - dR_dz[q10+ii] * zrr,
         d2R_drs[ii] - dR_dx[q10+ii] * xrs - dR_dy[q10+ii] * yrs - dR_dz[q10+ii] * zrs,
@@ -188,14 +188,14 @@ double FEAElement_Tet10::get_h( const double * const &ctrl_x,
 void FEAElement_Tet10::get_R( const int &quaindex, double * const &basis ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_R function error.\n" );
-  const int offset = quaindex * 10;
-  for(int ii=0; ii<10; ++ii) basis[ii] = R[offset+ii];
+  const int offset = quaindex * nLocBas;
+  for(int ii=0; ii<nLocBas; ++ii) basis[ii] = R[offset+ii];
 }
 
 std::vector<double> FEAElement_Tet10::get_R( const int &quaindex ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_R function error.\n" );
-  const int offset = quaindex * 10;
+  const int offset = quaindex * nLocBas;
   return { R[offset], R[offset+1], R[offset+2], R[offset+3],
     R[offset+4], R[offset+5], R[offset+6], R[offset+7], R[offset+8], R[offset+9] };
 }
@@ -204,8 +204,8 @@ void FEAElement_Tet10::get_gradR( const int &quaindex, double * const &basis_x,
     double * const &basis_y, double * const &basis_z ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_gradR function error.\n" );
-  const int offset = quaindex * 10;
-  for( int ii=0; ii<10; ++ii )
+  const int offset = quaindex * nLocBas;
+  for( int ii=0; ii<nLocBas; ++ii )
   {
     basis_x[ii] = dR_dx[offset + ii];
     basis_y[ii] = dR_dy[offset + ii];
@@ -218,8 +218,8 @@ void FEAElement_Tet10::get_R_gradR( const int &quaindex, double * const &basis,
     double * const &basis_z ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_R_gradR function error.\n" );
-  const int offset = quaindex * 10;
-  for( int ii=0; ii<10; ++ii )
+  const int offset = quaindex * nLocBas;
+  for( int ii=0; ii<nLocBas; ++ii )
   {
     basis[ii]   = R[offset+ ii];
     basis_x[ii] = dR_dx[offset + ii];
@@ -236,8 +236,8 @@ void FEAElement_Tet10::get_3D_R_dR_d2R( const int &quaindex,
     double * const &basis_xz, double * const &basis_yz ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_3D_R_dR_d2R function error.\n" );
-  const int offset = quaindex * 10;
-  for( int ii=0; ii<10; ++ii )
+  const int offset = quaindex * nLocBas;
+  for( int ii=0; ii<nLocBas; ++ii )
   {
     basis[ii]   = R[offset + ii];
     basis_x[ii] = dR_dx[offset + ii];
@@ -259,8 +259,8 @@ void FEAElement_Tet10::get_3D_R_gradR_LaplacianR( const int &quaindex,
     double * const &basis_zz ) const
 {
   ASSERT( quaindex >= 0 && quaindex < numQuapts, "FEAElement_Tet10::get_3D_R_gradR_LaplacianR function error.\n" );
-  const int offset = quaindex * 10;
-  for( int ii=0; ii<10; ++ii )
+  const int offset = quaindex * nLocBas;
+  for( int ii=0; ii<nLocBas; ++ii )
   {
     basis[ii]   = R[offset + ii];
     basis_x[ii] = dR_dx[offset + ii];
