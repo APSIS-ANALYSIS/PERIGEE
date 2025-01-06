0f3ca6dd030c94798040628eef1534a03e630908
Habib <habibullah350@yahoo.com>
2025-01-06 15:22:34 +0800

    Updated symmtensor2_3D include/HDF5_Writer.hpp

--- include/HDF5_Writer.hpp
+++ include/HDF5_Writer.hpp
@@ -39,7 +39,9 @@
 #include <string>
 #include <cstdlib>
 #include <climits>
-#include "Tensor2_3D.hpp"
+#include <iostream>
+#include <vector>
+#include <array>
 #include "hdf5.h"
 
 class HDF5_Writer
@@ -162,9 +164,9 @@ class HDF5_Writer
     // Tensor2_3D
     // --------------------------------------------------------------
     void write_Tensor2_3D( const hid_t &group_id, const char * const &data_name,
-        const Tensor2_3D &value ) const;
+        const std::array<double, 9> &value ) const;
 
-    void write_Tensor2_3D( const char * const &data_name, const Tensor2_3D &value ) const;
+    void write_Tensor2_3D( const char * const &data_name, const std::array<double, 9> &value ) const;
 
     // --------------------------------------------------------------
     // Matrix writer
