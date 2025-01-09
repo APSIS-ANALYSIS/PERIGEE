45c4138c7bf1c0e723c6a112af361f984f35ad3e
Ju Liu <liujuy@gmail.com>
2025-01-09 22:18:42 +0800

    clean the driver

--- examples/linearPDE/transport/driver.cpp
+++ examples/linearPDE/transport/driver.cpp
@@ -11,18 +11,6 @@
 #include "ALocal_Elem.hpp"
 #include "ALocal_EBC.hpp"
 #include "ALocal_NBC.hpp"
-#include "QuadPts_Gauss_Triangle.hpp"
-#include "QuadPts_Gauss_Quad.hpp"
-#include "QuadPts_Gauss_Tet.hpp"
-#include "QuadPts_Gauss_Hex.hpp"
-#include "FEAElement_Tet4.hpp"
-#include "FEAElement_Tet10.hpp"
-#include "FEAElement_Hex8.hpp"
-#include "FEAElement_Hex27.hpp"
-#include "FEAElement_Triangle3_3D_der0.hpp"
-#include "FEAElement_Triangle6_3D_der0.hpp"
-#include "FEAElement_Quad4_3D_der0.hpp"
-#include "FEAElement_Quad9_3D_der0.hpp"
 #include "PLocAssem_Transport_GenAlpha.hpp"
 #include "PGAssem_LinearPDE_GenAlpha.hpp"
 #include "PNonlinear_LinearPDE_Solver.hpp"
@@ -199,62 +187,6 @@ int main(int argc, char *argv[])
       "Error: Assigned CPU number does not match the partition. \n");
 
   SYS_T::commPrint("===> %d processor(s) are assigned for FEM analysis.\n", size);
-
-  // ===== Finite Element Container & Quadrature rules=====
-  SYS_T::commPrint("===> Setup element container. \n");
-  SYS_T::commPrint("===> Build quadrature rules. \n");
-  FEAElement * elementv = nullptr;
-  FEAElement * elements = nullptr;
-  IQuadPts * quadv = nullptr;
-  IQuadPts * quads = nullptr;
-
-  if( GMIptr->get_elemType() == FEType::Tet4 )
-  {
-    if( nqp_vol > 5 ) SYS_T::commPrint("Warning: the tet element is linear and you are using more than 5 quadrature points.\n");
-    if( nqp_sur > 4 ) SYS_T::commPrint("Warning: the tri element is linear and you are using more than 4 quadrature points.\n");
-
-    elementv = new FEAElement_Tet4( nqp_vol ); // elem type Tet4
-    elements = new FEAElement_Triangle3_3D_der0( nqp_sur );
-    quadv = new QuadPts_Gauss_Tet( nqp_vol );
-    quads = new QuadPts_Gauss_Triangle( nqp_sur );
-  }
-  else if( GMIptr->get_elemType() == FEType::Tet10 )
-  {
-    SYS_T::print_fatal_if( nqp_vol < 29, "Error: not enough quadrature points for tets.\n" );
-    SYS_T::print_fatal_if( nqp_sur < 13, "Error: not enough quadrature points for triangles.\n" );
-
-    elementv = new FEAElement_Tet10( nqp_vol ); // elem type Tet10
-    elements = new FEAElement_Triangle6_3D_der0( nqp_sur );
-    quadv = new QuadPts_Gauss_Tet( nqp_vol );
-    quads = new QuadPts_Gauss_Triangle( nqp_sur );
-  }
-  else if( GMIptr->get_elemType() == FEType::Hex8 )
-  {
-    SYS_T::print_fatal_if( nqp_vol_1D < 2, "Error: not enough quadrature points for hex.\n" );
-    SYS_T::print_fatal_if( nqp_sur_1D < 1, "Error: not enough quadrature points for quad.\n" );
-
-    elementv = new FEAElement_Hex8( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type Hex8
-    elements = new FEAElement_Quad4_3D_der0( nqp_sur_1D * nqp_sur_1D );
-    quadv = new QuadPts_Gauss_Hex( nqp_vol_1D );
-    quads = new QuadPts_Gauss_Quad( nqp_sur_1D );
-  }
-  else if( GMIptr->get_elemType() == FEType::Hex27 )
-  {
-    SYS_T::print_fatal_if( nqp_vol_1D < 4, "Error: not enough quadrature points for hex.\n" );
-    SYS_T::print_fatal_if( nqp_sur_1D < 3, "Error: not enough quadrature points for quad.\n" );
-
-    elementv = new FEAElement_Hex27( nqp_vol_1D * nqp_vol_1D * nqp_vol_1D ); // elem type Hex27
-    elements = new FEAElement_Quad9_3D_der0( nqp_sur_1D * nqp_sur_1D );
-    quadv = new QuadPts_Gauss_Hex( nqp_vol_1D );
-    quads = new QuadPts_Gauss_Quad( nqp_sur_1D );
-  }
-  else SYS_T::print_fatal("Error: Element type not supported.\n");
-
-  // print the information of element and quadrature rule
-  elementv->print_info();
-  elements->print_info();
-  quadv->print_info();
-  quads->print_info();
 
   // ===== Generate a sparse matrix for the enforcement of essential BCs
   Matrix_PETSc * pmat = new Matrix_PETSc(pNode, locnbc);
