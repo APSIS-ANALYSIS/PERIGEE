f3c9867842ad01545e83440e3048806dd7044999
Ju Liu <liujuy@gmail.com>
2025-01-20 09:30:18 +0800

    update test code

--- examples/test/matmodel_test.cpp
+++ examples/test/matmodel_test.cpp
@@ -1,15 +1,15 @@
-#include "APart_Basic_Info.hpp"
+#include "ANL_Tools.hpp"
 
 int main( int argc, char * argv[] )
 {
   std::string part_file("part");
 
-  int size = APart_Basic_Info::get_cpu_size(part_file, 0);
+  int size = ANL_T::get_cpu_size(part_file, 0);
 
   for(int ii=0; ii<size; ++ii)
   {
-    int rank = APart_Basic_Info::get_cpu_rank(part_file, ii);
-    size = APart_Basic_Info::get_cpu_size(part_file, ii);
+    int rank = ANL_T::get_cpu_rank(part_file, ii);
+    size = ANL_T::get_cpu_size(part_file, ii);
     std::cout<<rank<<'\t'<<size<<'\n';
   }
 
