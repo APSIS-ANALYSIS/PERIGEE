aebd7cd6543ed3e1c6b87e423379471bfed21026
Ju Liu <liujuy@gmail.com>
2025-01-26 13:52:29 +0800

    Flow rate cosine2steady converted

--- include/FlowRate_Cosine2Steady.hpp
+++ include/FlowRate_Cosine2Steady.hpp
@@ -1,23 +1,23 @@
-#ifndef CVFLOWRATE_COSINE2STEADY_HPP
-#define CVFLOWRATE_COSINE2STEADY_HPP
+#ifndef FLOWRATE_COSINE2STEADY_HPP
+#define FLOWRATE_COSINE2STEADY_HPP
 // ============================================================================
-// CVFlowRate_Cosine2Steady.hpp
+// FlowRate_Cosine2Steady.hpp
 //
-// It is similar to CVFlowRate_Linear2Steady, except using a cosine function
+// It is similar to FlowRate_Linear2Steady, except using a cosine function
 // to prescibe the starting flow rate
 //
 // Date Created: Nov. 17 2023
 // ============================================================================
 #include "Vec_Tools.hpp"
 #include "Math_Tools.hpp"
-#include "ICVFlowRate.hpp"
+#include "IFlowRate.hpp"
 
-class CVFlowRate_Cosine2Steady : public ICVFlowRate
+class FlowRate_Cosine2Steady : public IFlowRate
 {
   public:
     // This constructor will set a uniform thred_time and flrate for all inlets.
-    // Same as CVFlowRate_Linear2Steady.
-    CVFlowRate_Cosine2Steady( const int &input_num_nbc,
+    // Same as FlowRate_Linear2Steady.
+    FlowRate_Cosine2Steady( const int &input_num_nbc,
         const double &in_thred_time, const double &flrate,
         const double &in_TI_std_dev );
 
@@ -25,13 +25,14 @@ class CVFlowRate_Cosine2Steady : public ICVFlowRate
     // target flow rate are determined from the file and set to be the flow rate
     // at the initial time (i.e. time = 0.0) by setting target_flow_rate to be
     // the sum of coef_a.
-    // Same as CVFlowRate_Linear2Steady.
-    CVFlowRate_Cosine2Steady( const double &in_thred_time, 
+    // Same as FlowRate_Linear2Steady.
+    FlowRate_Cosine2Steady( const double &in_thred_time, 
         const double &in_TI_std_dev, const std::string &filename );
 
-    virtual ~CVFlowRate_Cosine2Steady() = default;
+    virtual ~FlowRate_Cosine2Steady() = default;
 
-    // From time 0 to in_time, the flow rate = 0.5 * flrate * (1 -  cos (PI * time / in_thred_time))
+    // From time 0 to in_time, the flow rate equals
+    // 0.5 * flrate * (1 -  cos (PI * time / in_thred_time))
     // From in_time to infty, flow_rate = flrate
     virtual double get_flow_rate( const int &nbc_id, const double &time ) const;
 
