e5f29897917f430b4e6c1ea68a46d625c741a698
Ju Liu <liujuy@gmail.com>
2025-02-08 18:19:24 +0800

    genbc factory tested

--- include/GenBCFactory.hpp
+++ include/GenBCFactory.hpp
@@ -19,8 +19,8 @@ class GenBCFactory
 {
   public:
     static std::unique_ptr<IGenBC> createGenBC(const std::string &lpn_file,
-        const double &initial_step = 0.1, const int &initial_index = 0,
-        const double &initial_time = 0.0)
+        const double &initial_time = 0.0, const double &initial_step = 0.1,
+        const int &initial_index = 0, const int &num_steps_0d = 1000)
     {
       // Retrieve the file type
       const int file_type = GENBC_T::get_genbc_file_type(lpn_file);
@@ -30,11 +30,11 @@ class GenBCFactory
         case 1:
           return SYS_T::make_unique<GenBC_Resistance>(lpn_file);
         case 2:
-          return SYS_T::make_unique<GenBC_RCR>(lpn_file, 1000, initial_step);
+          return SYS_T::make_unique<GenBC_RCR>(lpn_file, num_steps_0d, initial_step);
         case 3:
           return SYS_T::make_unique<GenBC_Inductance>(lpn_file);
         case 4:
-          return SYS_T::make_unique<GenBC_Coronary>(lpn_file, 1000, initial_step, initial_index);
+          return SYS_T::make_unique<GenBC_Coronary>(lpn_file, num_steps_0d, initial_step, initial_index);
         case 5:
           return SYS_T::make_unique<GenBC_Pressure>(lpn_file, initial_time);
         default:
