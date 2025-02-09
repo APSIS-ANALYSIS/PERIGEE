30dd3846a618cb8414931550c8ba8bd8fccced46
Ju Liu <liujuy@gmail.com>
2025-02-09 22:19:28 +0800

    add a new function that determine the surface nlocbas from element type

--- include/FEType.hpp
+++ include/FEType.hpp
@@ -99,6 +99,20 @@ namespace FE_T
       default: return 0;
     }
   }
+  
+  // Convert FEType to its number of local basis functions on the element
+  // surfaces. This is only defined for volumetric elements.
+  inline int to_snLocBas(const FEType &type) 
+  {
+    switch (type) 
+    {
+      case FEType::Tet4: return 3;
+      case FEType::Tet10: return 6;
+      case FEType::Hex8: return 4;
+      case FEType::Hex27: return 9;
+      default: return 0;
+    }
+  }
 }
 
 #endif
