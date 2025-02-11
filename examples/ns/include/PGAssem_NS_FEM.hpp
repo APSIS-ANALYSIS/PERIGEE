8c246aed67530d758f60123aaf200d64c74f31ea
Syj-0525 <sunyujie@LAPTOP-73KDLRBK>
2025-02-12 02:40:26 +0800

    Head file of PGAssem draft

--- examples/ns/include/PGAssem_NS_FEM.hpp
+++ examples/ns/include/PGAssem_NS_FEM.hpp
@@ -25,205 +25,272 @@ class PGAssem_NS_FEM : public IPGAssem
   public:
     // Constructor for NS equations
     PGAssem_NS_FEM( 
-        IPLocAssem * const &locassem_ptr,
-        FEAElement * const &elements,
-        const IQuadPts * const &quads,
-        const AGlobal_Mesh_Info * const &agmi_ptr,
-        const ALocal_Elem * const &alelem_ptr,
-        const ALocal_IEN * const &aien_ptr,
-        const APart_Node * const &pnode_ptr,
-        const ALocal_NBC * const &part_nbc,
-        const ALocal_EBC * const &part_ebc,
-        const IGenBC * const &gbc,
+        // IPLocAssem * const &locassem_ptr,
+        // FEAElement * const &elements,
+        // const IQuadPts * const &quads,
+        // const AGlobal_Mesh_Info * const &agmi_ptr,
+        // const ALocal_Elem * const &alelem_ptr,
+        // const ALocal_IEN * const &aien_ptr,
+        // const APart_Node * const &pnode_ptr,
+        // const ALocal_NBC * const &part_nbc,
+        // const ALocal_EBC * const &part_ebc,
+        // const IGenBC * const &gbc,
+        std::unique_ptr<ALocal_IEN> in_locien,
+        std::unique_ptr<ALocal_Elem> in_locelem,
+        std::unique_ptr<FEANode> in_fnode,
+        std::unique_ptr<APart_Node> in_pnode,
+        std::unique_ptr<ALocal_NBC> in_nbc,
+        std::unique_ptr<ALocal_InflowBC> in_infnbc,
+        std::unique_ptr<ALocal_EBC> in_ebc,
+        std::unique_ptr<ALocal_EBC> in_gbc,
+        std::unique_ptr<ALocal_WeakBC> in_wbc,
+        std::unique_ptr<IPLocAssem> in_locassem,    
         const int &in_nz_estimate=60 );
 
     // Destructor
     virtual ~PGAssem_NS_FEM();
 
     // Nonzero pattern estimate for the NS equations
-    virtual void Assem_nonzero_estimate(
-        const ALocal_Elem * const &alelem_ptr,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &elements,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const APart_Node * const &node_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc );
+    // virtual void Assem_nonzero_estimate(
+    //     const ALocal_Elem * const &alelem_ptr,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &elements,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const APart_Node * const &node_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const IGenBC * const &gbc );
+    virtual void Assem_nonzero_estimate();
 
     // Assem mass matrix and residual vector
+    // virtual void Assem_mass_residual(
+    //     const PDNSolution * const &sol_a,
+    //     const ALocal_Elem * const &alelem_ptr,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &elementv,
+    //     FEAElement * const &elements,
+    //     FEAElement * const &elementvs,
+    //     const IQuadPts * const &quad_v,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const FEANode * const &fnode_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const ALocal_WeakBC * const &wbc_part );
     virtual void Assem_mass_residual(
-        const PDNSolution * const &sol_a,
-        const ALocal_Elem * const &alelem_ptr,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &elementv,
-        FEAElement * const &elements,
-        FEAElement * const &elementvs,
-        const IQuadPts * const &quad_v,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &fnode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const ALocal_WeakBC * const &wbc_part );
+        const PDNSolution * const &sol_a );
 
     // Assembly the residual vector for the NS equations
+    // virtual void Assem_residual(
+    //     const PDNSolution * const &dot_sol,
+    //     const PDNSolution * const &sol,
+    //     const PDNSolution * const &dot_sol_np1,
+    //     const PDNSolution * const &sol_np1,
+    //     const double &curr_time,
+    //     const double &dt,
+    //     const ALocal_Elem * const &alelem_ptr,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &elementv,
+    //     FEAElement * const &elements,
+    //     FEAElement * const &elementvs,
+    //     const IQuadPts * const &quad_v,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const FEANode * const &fnode_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const IGenBC * const &gbc,
+    //     const ALocal_WeakBC * const &wbc_part );
     virtual void Assem_residual(
         const PDNSolution * const &dot_sol,
         const PDNSolution * const &sol,
         const PDNSolution * const &dot_sol_np1,
         const PDNSolution * const &sol_np1,
         const double &curr_time,
-        const double &dt,
-        const ALocal_Elem * const &alelem_ptr,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &elementv,
-        FEAElement * const &elements,
-        FEAElement * const &elementvs,
-        const IQuadPts * const &quad_v,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &fnode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc,
-        const ALocal_WeakBC * const &wbc_part );
+        const double &dt );
 
     // Assembly the residual vector and tangent matrix 
     // for the NS equations
+    // virtual void Assem_tangent_residual(
+    //     const PDNSolution * const &dot_sol,
+    //     const PDNSolution * const &sol,
+    //     const PDNSolution * const &dot_sol_np1,
+    //     const PDNSolution * const &sol_np1,
+    //     const double &curr_time,
+    //     const double &dt,
+    //     const ALocal_Elem * const &alelem_ptr,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &elementv,
+    //     FEAElement * const &elements,
+    //     FEAElement * const &elementvs,
+    //     const IQuadPts * const &quad_v,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const FEANode * const &fnode_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const IGenBC * const &gbc,
+    //     const ALocal_WeakBC * const &wbc_part );
     virtual void Assem_tangent_residual(
         const PDNSolution * const &dot_sol,
         const PDNSolution * const &sol,
         const PDNSolution * const &dot_sol_np1,
         const PDNSolution * const &sol_np1,
         const double &curr_time,
-        const double &dt,
-        const ALocal_Elem * const &alelem_ptr,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &elementv,
-        FEAElement * const &elements,
-        FEAElement * const &elementvs,
-        const IQuadPts * const &quad_v,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &fnode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc,
-        const ALocal_WeakBC * const &wbc_part );
+        const double &dt );
 
     // Assembly routine for the surface integrals of flow rate and
     // pressure
-    virtual double Assem_surface_flowrate(
+    // virtual double Assem_surface_flowrate(
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const int &ebc_id );
+    virtual double Assem_outlet_flowrate(
         const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_EBC * const &ebc_part,
         const int &ebc_id );
 
-    virtual double Assem_surface_flowrate(
+    // virtual double Assem_surface_flowrate(
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_InflowBC * const &infbc_part,
+    //     const int &nbc_id );
+    virtual double Assem_inlet_flowrate(
         const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_InflowBC * const &infbc_part,
-        const int &nbc_id );
+        const int &infnbc_id );
 
-    virtual double Assem_surface_ave_pressure(
+    // virtual double Assem_surface_ave_pressure(
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const int &ebc_id );
+    virtual double Assem_outlet_ave_pressure(
         const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_EBC * const &ebc_part,
         const int &ebc_id );
 
-    virtual double Assem_surface_ave_pressure(
+    // virtual double Assem_surface_ave_pressure(
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_InflowBC * const &infbc_part,
+    //     const int &nbc_id );
+    virtual double Assem_inlet_ave_pressure(
         const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_InflowBC * const &infbc_part,
-        const int &nbc_id );
+        const int &infnbc_id )
 
   private:
     // Private data
-    const int nLocBas, dof_sol, dof_mat, num_ebc, nlgn;
+    const std::unique_ptr<const ALocal_IEN> locien;
+    const std::unique_ptr<const ALocal_Elem> locelem;
+    const std::unique_ptr<const FEANode> fnode;
+    const std::unique_ptr<const APart_Node> pnode;
+    const std::unique_ptr<const ALocal_NBC> nbc;
+    const std::unique_ptr<const ALocal_InflowBC> in_infnbc;
+    const std::unique_ptr<const ALocal_NBC> nbc;
+    const std::unique_ptr<const ALocal_EBC> ebc;
+    const std::unique_ptr<const ALocal_EBC> gbc;
+    const std::unique_ptr<const ALocal_WeakBC> wbc;
+    const std::unique_ptr<IPLocAssem> locassem;
+
+    const int nLocBas, snLocBas, dof_sol, dof_mat, num_ebc, nlgn;
     
-    int snLocBas;
+    // int snLocBas;
 
     // Private function
     // Essential boundary condition
-    void EssBC_KG( const ALocal_NBC * const &nbc_part, const int &field );
+    // void EssBC_KG( const ALocal_NBC * const &nbc_part, const int &field );
+    void EssBC_KG( const int &field );
     
-    void EssBC_G( const ALocal_NBC * const &nbc_part, const int &field );
+    // void EssBC_G( const ALocal_NBC * const &nbc_part, const int &field );
+    void EssBC_G( const int &field );
 
     // Natural boundary condition
-    void NatBC_G( const double &curr_time, const double &dt,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part );
+    // void NatBC_G( const double &curr_time, const double &dt,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part );
+    void NatBC_G( const double &curr_time, const double &dt );
 
     // Backflow integral on outlet surfaces
-    void BackFlow_G( const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part );
+    // void BackFlow_G( const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part );
+    void BackFlow_G( const PDNSolution * const &sol );
 
+    // void BackFlow_KG( const double &dt,
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part );
     void BackFlow_KG( const double &dt,
-        const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part );
+        const PDNSolution * const &sol );
 
     // Resistance type boundary condition on outlet surfaces
+    // void NatBC_Resis_G( const double &curr_time, const double &dt,
+    //     const PDNSolution * const &dot_sol,
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const IGenBC * const &gbc );
     void NatBC_Resis_G( const double &curr_time, const double &dt,
         const PDNSolution * const &dot_sol,
-        const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc );
+        const PDNSolution * const &sol );
 
+    // void NatBC_Resis_KG( const double &curr_time, const double &dt,
+    //     const PDNSolution * const &dot_sol,
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_s,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_EBC * const &ebc_part,
+    //     const IGenBC * const &gbc );
     void NatBC_Resis_KG( const double &curr_time, const double &dt,
         const PDNSolution * const &dot_sol,
-        const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_s,
-        const IQuadPts * const &quad_s,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_EBC * const &ebc_part,
-        const IGenBC * const &gbc );
+        const PDNSolution * const &sol );
 
     // Weak imposition of no-slip boundary condition on wall
+    // void Weak_EssBC_KG( const double &curr_time, const double &dt,
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_vs,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const FEANode * const &fnode_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_WeakBC * const &wbc_part);
     void Weak_EssBC_KG( const double &curr_time, const double &dt,
-        const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_vs,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &fnode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_WeakBC * const &wbc_part);
+        const PDNSolution * const &sol );
 
+    // void Weak_EssBC_G( const double &curr_time, const double &dt,
+    //     const PDNSolution * const &sol,
+    //     IPLocAssem * const &lassem_ptr,
+    //     FEAElement * const &element_vs,
+    //     const IQuadPts * const &quad_s,
+    //     const ALocal_IEN * const &lien_ptr,
+    //     const FEANode * const &fnode_ptr,
+    //     const ALocal_NBC * const &nbc_part,
+    //     const ALocal_WeakBC * const &wbc_part);
     void Weak_EssBC_G( const double &curr_time, const double &dt,
-        const PDNSolution * const &sol,
-        IPLocAssem * const &lassem_ptr,
-        FEAElement * const &element_vs,
-        const IQuadPts * const &quad_s,
-        const ALocal_IEN * const &lien_ptr,
-        const FEANode * const &fnode_ptr,
-        const ALocal_NBC * const &nbc_part,
-        const ALocal_WeakBC * const &wbc_part);
+        const PDNSolution * const &sol );
 
     void GetLocal(const double * const &array, const int * const &IEN,
         double * const &local_array) const
