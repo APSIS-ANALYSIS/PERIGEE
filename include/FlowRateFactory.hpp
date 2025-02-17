a278f70ced2809aceb62bc6ab169b5a50eb8acb7
Ju Liu <liujuy@gmail.com>
2025-02-17 09:17:47 +0800

    add comments to flowrate factory

--- include/FlowRateFactory.hpp
+++ include/FlowRateFactory.hpp
@@ -1,6 +1,13 @@
 #ifndef FLOWRATEFACTORY_HPP
 #define FLOWRATEFACTORY_HPP
-
+// ============================================================================
+// FlowrateFactory.hpp
+//
+// This is the factory that construct a flowrate class based on the inflow file
+//
+// Author: Xuanming Huang
+// Date: Jan. 1st 2025
+// ============================================================================
 #include "FlowRate_Steady.hpp"
 #include "FlowRate_Unsteady.hpp"
 #include "FlowRate_Linear2Steady.hpp"
@@ -57,35 +64,38 @@ class FlowRateFactory
 
       reader.close();
 
+      int filetype = 0;
       // Now compare and return the corresponding type value
       if( bc_type.compare("Steady") == 0
           || bc_type.compare("steady") == 0
           || bc_type.compare("STEADY") == 0 )
       {
-        return 1;
+        filetype = 1;
       }
       else if( bc_type.compare("Unsteady") ==0
           || bc_type.compare("unsteady") == 0
           || bc_type.compare("UNSTEADY") == 0 )
       {
-        return 2;
+        filetype = 2;
       }
       else if( bc_type.compare("Linear") ==0
           || bc_type.compare("linear") == 0
           || bc_type.compare("LINEAR") == 0 )
       {
-        return 3;
+        filetype = 3;
       }
       else if( bc_type.compare("Cosine") ==0
           || bc_type.compare("cosine") == 0
           || bc_type.compare("COSINE") == 0 )
       {
-        return 4;
+        filetype = 4;
       }       
       else
       {
-        return 0;
+        filetype = 0;
       }
+
+      return filetype;
     }
 };
 
