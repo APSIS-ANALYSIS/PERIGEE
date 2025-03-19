5e6ac74d4d37e61a1be797b14321cb0d05bf1cec
SUNYujie <12231157@mail.sustech.edu.cn>
2025-03-19 19:39:02 +0800

    Matrix_free and PCSHELL update

--- examples/ns/include/Matrix_Free_Tools.hpp
+++ examples/ns/include/Matrix_Free_Tools.hpp
@@ -2,9 +2,18 @@
 #define MATRIX_FREE_TOOLS_HPP
 
 #include "PGAssem_Block_NS_FEM_HERK.hpp"
+#include "PLinear_Solver_PETSc.hpp"
 
 namespace MF_T
 {
+
+  typedef struct 
+  {
+    PGAssem_Block_NS_FEM_HERK *gloAssem;
+    std::unique_ptr<PLinear_Solver_PETSc> lsolver_A;
+    std::unique_ptr<PLinear_Solver_PETSc> lsolver_S;
+  } SolverContext;
+
   PetscErrorCode MF_MatMult(Mat shell, Vec x, Vec y)
   {
     void *ptr;
@@ -14,8 +23,6 @@ namespace MF_T
 
     Vec x1, x2, y1, y2;
     PetscInt m1_local, m2_local;
-    const PetscScalar *x_array, *y1_array, *y2_array;
-    PetscScalar *y_array;
 
     const PetscScalar coef = user->Get_tangent_alpha_RK();
 
@@ -37,59 +44,213 @@ namespace MF_T
     VecSetUp(y1);
     VecSetUp(y2);
 
-    // Split x into x1, x2
-    VecGetArrayRead(x, &x_array);
-    VecPlaceArray(x1, x_array);
-    VecPlaceArray(x2, x_array + m1_local);
+    // Split x into x1, x2  
+    PetscInt start1, start2;
+    IS is1, is2;
 
-    // Allocate temporary vectors for x1 and x2
-    Vec x1_tmp, x2_tmp;
-    VecDuplicate(x1, &x1_tmp);
-    VecDuplicate(x2, &x2_tmp);
+    VecGetOwnershipRange(x, &start1, NULL);
+    start2 = start1 + m1_local; 
 
-    // y1 = (A3 + A4) * x1 + A2 * x2
-    // MatMult(user->subK[3], x1, y1);
-    // MatMultAdd(user->subK[4], x1, y1, y1);
-    // MatMultAdd(user->subK[2], x2, y1, y1);
+    ISCreateStride(PETSC_COMM_WORLD, m1_local, start1, 1, &is1);
+    ISCreateStride(PETSC_COMM_WORLD, m2_local, start2, 1, &is2);
 
-    MatMult(user->subK[3], x1, y1);       // y1 = A3 * x1
-    MatMult(user->subK[4], x1, x1_tmp);   // x1 = A4 * x1
-    VecAXPY(y1, coef, x1_tmp);            // y1 = A3 * x1 + coef * A4 * x1
-    MatMult(user->subK[2], x2, x1_tmp);   // x1 = A2 * x2
-    VecAXPY(y1, coef, x1_tmp);            // y1 = (A3 + coef * A4) * x1 + coef * A2 * x2
-    
-    // y2 = A1 * x1 + A0 * x2
-    // MatMult(user->subK[1], x1, y2);
-    // MatMultAdd(user->subK[0], x2, y2, y2);
+    VecGetSubVector(x, is1, &x1);
+    VecGetSubVector(x, is2, &x2);  
 
-    MatMult(user->subK[1], x1, y2);      // y2 = A1 * x1
-    MatMult(user->subK[0], x2, x2_tmp);  // x2 = A0 * x2
-    VecAXPY(y2, coef, x2_tmp);           // y2 = A1 * x1 + coef * A0 * x2
-    
+    MatMult(user->subK[3], x1, y1);      // y1 = A3 * x1
+    MatMult(user->subK[4], x1, x1);      // x1 = A4 * x1
+    VecAXPY(y1, coef, x1);               // y1 = A3 * x1 + coef * A4 * x1
+    MatMult(user->subK[2], x2, x1);      // x1 = A2 * x2
+    VecAXPY(y1, coef, x1);               // y1 = (A3 + coef * A4) * x1 + coef * A2 * x2
+
     // Combine y1, y2 to y
-    VecGetArrayRead(y1, &y1_array);
-    VecGetArrayRead(y2, &y2_array);
-    VecGetArray(y, &y_array);
-
-    PetscMemcpy(y_array, y1_array, m1_local * sizeof(PetscScalar));
-    PetscMemcpy(y_array + m1_local, y2_array, m2_local * sizeof(PetscScalar));
-
-    // Release
-    VecRestoreArrayRead(y1, &y1_array);
-    VecRestoreArrayRead(y2, &y2_array);
-    VecRestoreArray(y, &y_array);
-
-    VecResetArray(x1);
-    VecResetArray(x2);
-    VecRestoreArrayRead(x, &x_array);
-
+    VecGetSubVector(y, is1, &y1);
+    VecGetSubVector(y, is2, &y2);     
+    MatMult(user->subK[1], x1, y2);      // y2 = A1 * x1
+    MatMult(user->subK[0], x2, x2);      // x2 = A0 * x2
+    VecAXPY(y2, coef, x2);               // y2 = A1 * x1 + coef * A0 * x2
+    
     // Destruction of sub vectors
     VecDestroy(&x1);
     VecDestroy(&x2);
     VecDestroy(&y1);
     VecDestroy(&y2);
-    VecDestroy(&x1_tmp);
-    VecDestroy(&x2_tmp);
+    ISDestroy(&is1);
+    ISDestroy(&is2);
+
+    return 0;
+  }
+
+  PetscErrorCode SetupApproxSchur(PGAssem_Block_NS_FEM_HERK *const user, Mat &S_approx)
+  {
+    // Schur complement approximation: S = D - C inv(DIAGFORM(A)) B
+    Vec diag;
+    PetscInt mA_local;
+
+    MatGetLocalSize(user->subK[3], &mA_local, NULL);
+
+    // inverse of diagonal of A
+    VecCreate(PETSC_COMM_WORLD, &diag);   
+    VecSetSizes(diag, mA_local, PETSC_DETERMINE);
+    VecSetType(diag, VECMPI);
+    MatGetDiagonal(user->subK[3], diag);
+    VecReciprocal(diag);
+
+    MatDiagonalScale(user->subK[2], diag, NULL); // overwrites B = subK[2]) 
+    MatMatMult(user->subK[1], user->subK[2], MAT_INITIAL_MATRIX, PETSC_DETERMINE, &S_approx);
+
+    MatScale(S_approx, -1.0);
+    MatAXPY(S_approx, 1.0, user->subK[0], DIFFERENT_NONZERO_PATTERN);  // S_approx = D - S_approx
+
+    // restore B = subK[2]
+    MatGetDiagonal(user->subK[3], diag);
+    MatDiagonalScale(user->subK[2], diag, NULL);
+
+    VecDestroy(&diag);
+
+    return 0;
+  }
+
+  // PetscErrorCode MatMult_S(Mat S, Vec x, Vec y)
+  // {
+  //   void *ptr;
+  //   PGAssem_Block_NS_FEM_HERK *user;   // 这里应该传一个包含PGA和ksp_A的结构体进去
+  //   MatShellGetContext(S, &ptr);
+  //   user = (PGAssem_Block_NS_FEM_HERK*) ptr;
+
+  //   Mat B, C, D;
+  //   Vec temp, Ax;
+
+  //   B = user->subK[2];
+  //   C = user->subK[1];
+  //   D = user->subK[0];
+
+  //   VecDuplicate(x, &temp);
+  //   VecDuplicate(x, &Ax);
+
+  //   // A^{-1} B x
+  //   MatMult(B, x, temp);     // temp = B * x
+  //   KSPSolve(user->ksp_A, temp, Ax);  // Ax = A^{-1} * (B x)
+
+  //   // C * A^{-1} B x
+  //   MatMult(C, Ax, temp);  // temp = C * Ax
+
+  //   // y = D x - temp
+  //   MatMult(D, x, y);
+  //   VecAXPY(y, -1.0, temp);
+
+  //   VecDestroy(&temp);
+  //   VecDestroy(&Ax);
+
+  //   return 0;
+  // }
+
+  // PetscErrorCode S_PCSetUp(PC pc, PGAssem_Block_NS_FEM_HERK *user)
+  // {
+  //   Mat S;
+
+  //   PetscInt mD_local;
+
+  //   MatGetLocalSize(user->subK[0], &mD_local, NULL);
+
+  //   MatCreateShell(PETSC_COMM_WORLD, mD_local, mD_local, PETSC_DETERMINE, PETSC_DETERMINE, user, &S);
+  //   MatShellSetOperation(S, MATOP_MULT, (void(*)(void))MatMult_S);
+
+  //   lsolver_S->Setoperator(S); 
+
+  //   return 0;
+  // }
+
+  // PetscErrorCode MyPCSetUp(PC pc)
+  // {
+  //   PGAssem_Block_NS_FEM_HERK *user;
+  //   PetscCall(PCShellGetContext(pc, (void**)&user));
+
+  //   Mat A = user->subK[3];
+  //   lsolver_A->Setoperator(A);  
+
+  //   Mat S;
+
+  //   PetscInt mD_local;
+
+  //   MatGetLocalSize(user->subK[0], &mD_local, NULL);
+
+  //   MatCreateShell(PETSC_COMM_WORLD, mD_local, mD_local, PETSC_DETERMINE, PETSC_DETERMINE, user, &S);
+  //   MatShellSetOperation(S, MATOP_MULT, (void(*)(void))MatMult_S);
+
+  //   lsolver_S->Setoperator(S);
+
+  //   return 0;
+  // }
+
+  
+  PetscErrorCode MF_PCSchurApply(PC pc, Vec x, Vec y)
+  {
+    void *ptr;
+    SolverContext *ctx;
+    PCShellGetContext(pc, &ptr);
+    ctx = (SolverContext*) ptr;    
+
+    Mat A = ctx->gloAssem->subK[3], B = ctx->gloAssem->subK[2], C = ctx->gloAssem->subK[1], D = ctx->gloAssem->subK[0];
+
+    Vec x1, x2, y1, y2, z1, z2, w1, w2, temp;
+    IS is1, is2;
+    PetscInt m1_local, m2_local;
+
+    MatGetLocalSize(A, &m1_local, NULL);
+    MatGetLocalSize(D, &m2_local, NULL);
+
+    PetscInt start1, start2;
+    VecGetOwnershipRange(x, &start1, NULL);
+    start2 = start1 + m1_local;
+
+    ISCreateStride(PETSC_COMM_WORLD, m1_local, start1, 1, &is1);
+    ISCreateStride(PETSC_COMM_WORLD, m2_local, start2, 1, &is2);
+
+    VecGetSubVector(x, is1, &x1);
+    VecGetSubVector(x, is2, &x2);
+
+    VecDuplicate(x1, &z1);
+    VecDuplicate(x2, &z2);
+    VecDuplicate(x1, &w1);
+    VecDuplicate(x2, &w2);
+    VecDuplicate(x2, &temp);
+
+    // Step 1: Compute z1 = A^{-1} x1
+    ctx->lsolver_A->Solve(x1, z1);
+
+    // Step 2: Compute z2 = x2 - C * z1
+    MatMult(C, z1, temp);
+    VecWAXPY(z2, -1.0, temp, x2);
+
+    // Step 3: Compute w2 = S^{-1} z2
+    ctx->lsolver_S->Solve(z2, w2);
+
+    // Step 4: Compute w1 = A^{-1}(z1 - B w2)
+    MatMult(B, w2, temp);
+    VecWAXPY(z1, -1.0, temp, z1);
+    ctx->lsolver_A->Solve(z1, w1); 
+  
+    // Combine w1 and w2 into y
+    VecGetSubVector(y, is1, &y1);
+    VecGetSubVector(y, is2, &y2);
+
+    VecCopy(w1, y1);
+    VecCopy(w2, y2);
+
+    // Restore and clean up
+    VecRestoreSubVector(x, is1, &x1);
+    VecRestoreSubVector(x, is2, &x2);
+    VecRestoreSubVector(y, is1, &y1);
+    VecRestoreSubVector(y, is2, &y2);
+
+    ISDestroy(&is1);
+    ISDestroy(&is2);
+    VecDestroy(&z1);
+    VecDestroy(&z2);
+    VecDestroy(&w1);
+    VecDestroy(&w2);
+    VecDestroy(&temp);
 
     return 0;
   }
