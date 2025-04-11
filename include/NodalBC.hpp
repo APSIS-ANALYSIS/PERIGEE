048cde75d330560adc9a76223b0391bce2c6943c
SUNYujie <12231157@mail.sustech.edu.cn>
2025-04-11 10:01:26 +0800

    support to read the slave surface in NodalBC

--- include/NodalBC.hpp
+++ include/NodalBC.hpp
@@ -45,6 +45,11 @@ class NodalBC : public INodalBC
     NodalBC( const std::vector<std::string> &vtkfileList, const int &nFunc );
 
     // --------------------------------------------------------------
+    // The list of vtp files specifies the Dirichlet nodes and periodical type BC nodes.
+    // --------------------------------------------------------------
+    NodalBC( const std::vector<std::string> &vtkfileList, const std::vector<std::string> &slafileList, const int &nFunc );
+
+    // --------------------------------------------------------------
     // The list of vtp files specifies the rotated Dirichlet nodes. 
     // No periodical type BC nodes.
     // --------------------------------------------------------------
--- src/Mesh/NodalBC.cpp
+++ src/Mesh/NodalBC.cpp
@@ -49,6 +49,67 @@ NodalBC::NodalBC( const std::vector<std::string> &vtkfileList,
 }
 
 NodalBC::NodalBC( const std::vector<std::string> &vtkfileList,
+  const std::vector<std::string> &slafileList,
+  const int &nFunc )
+{
+dir_nodes.clear();
+per_slave_nodes.clear();
+per_master_nodes.clear();
+num_per_nodes = 0;
+
+for( int ii{0}; ii < VEC_T::get_size(vtkfileList); ++ii )
+{
+  const auto vtkfile = vtkfileList[ii];
+  SYS_T::file_check( vtkfile );
+
+  const auto gnode = VTK_T::read_int_PointData(vtkfile, "GlobalNodeID");
+
+  for(unsigned int jj=0; jj<gnode.size(); ++jj)
+  {
+    SYS_T::print_fatal_if( gnode[jj]<0 || gnode[jj]>=nFunc, "Error: the nodal index %d is not in the range [0, %d)! \n", gnode[jj], nFunc);
+
+    dir_nodes.push_back( static_cast<unsigned int>( gnode[jj]) );
+  }
+}
+
+VEC_T::sort_unique_resize(dir_nodes);
+
+num_dir_nodes = dir_nodes.size();
+
+for( int ii{0}; ii < VEC_T::get_size(slafileList); ++ii )
+{
+  const auto slafile = slafileList[ii];
+  SYS_T::file_check( slafile );
+
+  const auto sla_gnode = VTK_T::read_int_PointData(slafile, "GlobalNodeID");
+  const auto mas_gnode = VTK_T::read_int_PointData(slafile, "MasterNodeID");
+
+  for(unsigned int jj=0; jj<sla_gnode.size(); ++jj)
+  {
+    SYS_T::print_fatal_if( sla_gnode[jj]<0 || sla_gnode[jj]>=nFunc, "Error: the slave nodal index %d is not in the range [0, %d)! \n", sla_gnode[jj], nFunc);
+    SYS_T::print_fatal_if( mas_gnode[jj]<0 || mas_gnode[jj]>=nFunc, "Error: the master nodal index %d is not in the range [0, %d)! \n", mas_gnode[jj], nFunc);
+
+    per_slave_nodes.push_back( static_cast<unsigned int>( sla_gnode[jj]) );
+    per_master_nodes.push_back( static_cast<unsigned int>( mas_gnode[jj]) );
+  }
+}
+
+num_per_nodes = per_slave_nodes.size();
+
+Create_ID( nFunc );
+
+std::cout<<"===> NodalBC specified by \n";
+for( const auto &vtkfile : vtkfileList )
+  std::cout<<"     "<<vtkfile<<"\n";
+std::cout<<"     is generated."<<std::endl;
+
+std::cout<<"===> Periodic NodalBC specified by \n";
+for( const auto &slavtkfile : slafileList )
+  std::cout<<"     "<<slavtkfile<<"\n";
+std::cout<<"     is generated."<<std::endl;
+}
+
+NodalBC::NodalBC( const std::vector<std::string> &vtkfileList,
     const std::string &rotated_file, 
     const std::string &fixed_file,    
     const int &nFunc )
