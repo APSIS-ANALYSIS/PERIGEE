4727f0e0e1b40efa5215ea0fc0ed6d99ea4e3530
Ju Liu <liujuy@gmail.com>
2025-04-24 14:37:26 +0800

    clean the name for sine2zero

--- include/FlowRateDot_Sine2Zero.hpp
+++ include/FlowRateDot_Sine2Zero.hpp
@@ -1,10 +1,10 @@
-#ifndef FLOWRATEDOT_SINE2ZERO_HPP
-#define FLOWRATEDOT_SINE2ZERO_HPP
+#ifndef FLOWRATE_SINE2ZERO_HPP
+#define FLOWRATE_SINE2ZERO_HPP
 // ============================================================================
-// FlowRateDot_Sine2Zero.hpp
+// FlowRate_Sine2Zero.hpp
 //
 // It is designed to match FlowRate_Cosine2Steady, using a sine function
-// to prescibe the starting dot flow rate
+// to prescibe the starting dot flow rate.
 //
 // Date Created: Nov. 04 2024
 // ============================================================================
@@ -12,7 +12,7 @@
 #include "Math_Tools.hpp"
 #include "IFlowRate.hpp"
 
-class FlowRateDot_Sine2Zero : public IFlowRate
+class FlowRate_Sine2Zero : public IFlowRate
 {
   public:
     // This constructor will set a uniform thred_time for all inlets, and the
@@ -20,21 +20,25 @@ class FlowRateDot_Sine2Zero : public IFlowRate
     // at the initial time (i.e. time = 0.0) by setting target_flow_rate to be
     // the sum of coef_a.
     // Same as FlowRate_Cosine2Steady.
-    FlowRateDot_Sine2Zero( const std::string &filename );
+    FlowRate_Sine2Zero( const std::string &filename );
 
-    virtual ~FlowRateDot_Sine2Zero() = default;
+    virtual ~FlowRate_Sine2Zero() = default;
 
     // From time 0 to in_thred_time,
-    // flow_rate =  start_rate + 0.5 * (target_rate - start_rate) * (1 -  cos (PI * time / in_thred_time)),
-    // dot_flow_rate = 0.5 * (target_rate - start_rate) * PI / in_thred_time * sin (PI * time / in_thred_time),
+    // flow_rate =  start_rate + 0.5 * (target_rate - start_rate) 
+    //                               * (1 -  cos (PI * time / in_thred_time)),
+    // dot_flow_rate = 0.5 * (target_rate - start_rate) 
+    //                 * PI / in_thred_time * sin (PI * time / in_thred_time),
     // From in_thred_time to infty, flow_rate = target_rate, dot_flow_rate = 0.0.
     virtual double get_flow_rate( const int &nbc_id, const double &time ) const;
 
     // Get the turbulance intensity
-    virtual double get_flow_TI_std_dev( const int &nbc_id ) const { return TI_std_dev[nbc_id]; }
+    virtual double get_flow_TI_std_dev( const int &nbc_id ) const 
+    { return TI_std_dev[nbc_id]; }
 
     // Get the start rate if the users want to now it
-    virtual double get_flow_start_rate( const int &nbc_id ) const { return start_flow_rate[nbc_id]; }
+    virtual double get_flow_start_rate( const int &nbc_id ) const 
+    { return start_flow_rate[nbc_id]; }
 
     virtual int get_num_nbc() const { return num_nbc; }
 
