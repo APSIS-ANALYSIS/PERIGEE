dff4b928b22337f7ab5fdf1c26e8820f47100910
Ju Liu <liujuy@gmail.com>
2026-04-21 17:59:25 +0800

    Refactor ElemBC int params to pass-by-value

--- include/ElemBC_3D.hpp
+++ include/ElemBC_3D.hpp
@@ -22,28 +22,28 @@ class ElemBC_3D : public ElemBC
 
     virtual int get_num_ebc() const {return num_ebc;}
 
-    virtual int get_num_node(const int &ebc_id) const {return num_node[ebc_id];}
+    virtual int get_num_node(int ebc_id) const {return num_node[ebc_id];}
 
-    virtual int get_num_cell(const int &ebc_id) const {return num_cell[ebc_id];}
+    virtual int get_num_cell(int ebc_id) const {return num_cell[ebc_id];}
 
-    virtual int get_cell_nLocBas(const int &ebc_id) const {return cell_nLocBas[ebc_id];}
+    virtual int get_cell_nLocBas(int ebc_id) const {return cell_nLocBas[ebc_id];}
 
-    virtual double get_pt_xyz(const int &ebc_id, const int &node, const int &dir) const
+    virtual double get_pt_xyz(int ebc_id, int node, int dir) const
     {return pt_xyz[ebc_id][3*node+dir];}
 
-    virtual int get_ien(const int &ebc_id, const int &cell, const int &lnode) const
+    virtual int get_ien(int ebc_id, int cell, int lnode) const
     {return sur_ien[ebc_id][ cell_nLocBas[ebc_id] * cell + lnode ];}
 
-    virtual int get_global_node(const int &ebc_id, const int &node_index) const
+    virtual int get_global_node(int ebc_id, int node_index) const
     {return global_node[ebc_id][node_index];}
 
-    virtual void get_global_node(const int &ebc_id, std::vector<int> &out) const
+    virtual void get_global_node(int ebc_id, std::vector<int> &out) const
     {out = global_node[ebc_id];}
 
-    virtual std::vector<int> get_global_node(const int &ebc_id) const
+    virtual std::vector<int> get_global_node(int ebc_id) const
     {return global_node[ebc_id];}
 
-    virtual int get_global_cell(const int &ebc_id, const int &cell_index) const
+    virtual int get_global_cell(int ebc_id, int cell_index) const
     {return global_cell[ebc_id][cell_index];}
 
     virtual void print_info() const;
@@ -75,11 +75,11 @@ class ElemBC_3D : public ElemBC
     virtual void resetSurIEN_outwardnormal( const IIEN * const &VIEN );
 
     // Access the data in ElemBC_3D_outflow, outward normal vector
-    virtual Vector_3 get_normal_vec( const int &ebc_id ) const
+    virtual Vector_3 get_normal_vec( int ebc_id ) const
     {SYS_T::commPrint("Warning: get_normal_vec is not implemented. \n"); return Vector_3();}
 
     // Access the data in ElemBC_3D_outflow, basis surface integration
-    virtual std::vector<double> get_intNA( const int &ebc_id ) const
+    virtual std::vector<double> get_intNA( int ebc_id ) const
     {SYS_T::commPrint("Warning: get_intNA is not implemented.\n"); return {};}
 
     // Access the data in ElemBC_3D_wall_turbulence, wall model type
@@ -87,7 +87,7 @@ class ElemBC_3D : public ElemBC
     {SYS_T::commPrint("Warning: get_wall_model_type is not implemented. \n"); return -1;}
 
     // Access the data in ElemBC_3D_wall_turbulence, face id of volume element
-    virtual int get_faceID( const int &cell_index ) const
+    virtual int get_faceID( int cell_index ) const
     {SYS_T::commPrint("Warning: get_faceID is not implemented. \n"); return {};}
 
   protected:
