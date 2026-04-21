cd7363db2193a4861b09ae34a1568b87cd593175
Ju Liu <liujuy@gmail.com>
2026-04-21 08:50:30 +0800

    refactor(ns): apply smart-pointer ownership in prepost

--- examples/ns/prepost.cpp
+++ examples/ns/prepost.cpp
@@ -12,6 +12,7 @@
 #include "Global_Part_Serial.hpp"
 #include "Part_FEM.hpp"
 #include "yaml-cpp/yaml.h"
+#include <memory>
 
 int main( int argc, char * argv[] )
 {
@@ -25,15 +26,13 @@ int main( int argc, char * argv[] )
 
   // Read preprocessor command-line arguements recorded in the .h5 file
 
-  HDF5_Reader * cmd_h5r = new HDF5_Reader( "preprocessor_cmd.h5" );
+  auto cmd_h5r = SYS_T::make_unique<HDF5_Reader>( "preprocessor_cmd.h5" );
 
   std::string geo_file = cmd_h5r -> read_string("/", "geo_file");
   const std::string elemType_str = cmd_h5r -> read_string("/","elemType");
   const int dofNum = cmd_h5r -> read_intScalar("/","dofNum");
   int in_ncommon = cmd_h5r -> read_intScalar("/","in_ncommon");
   const FEType elemType = FE_T::to_FEType(elemType_str);
-
-  delete cmd_h5r;
 
   // The user can specify the new mesh partition options from the yaml file
   const std::string yaml_file("ns_prepost.yml");
@@ -68,41 +67,39 @@ int main( int argc, char * argv[] )
 
   VTK_T::read_vtu_grid(geo_file, nFunc, nElem, ctrlPts, vecIEN);
   
-  IIEN * IEN = new IEN_FEM(nElem, vecIEN);
+  auto IEN = SYS_T::make_unique<IEN_FEM>(nElem, vecIEN);
   VEC_T::clean( vecIEN ); // clean the vector
 
   const int nLocBas = FE_T::to_nLocBas(elemType);
   
   SYS_T::print_fatal_if( IEN->get_nLocBas() != nLocBas, "Error: the nLocBas from the Mesh %d and the IEN %d classes do not match. \n", nLocBas, IEN->get_nLocBas());
 
-  IGlobal_Part * global_part = nullptr;
+  std::unique_ptr<IGlobal_Part> global_part = nullptr;
   if(cpu_size > 1)
-    global_part = new Global_Part_METIS( cpu_size, in_ncommon,
-        isDualGraph, nElem, nFunc, nLocBas, IEN, "post_epart", "post_npart" );
+    global_part = SYS_T::make_unique<Global_Part_METIS>( cpu_size, in_ncommon,
+        isDualGraph, nElem, nFunc, nLocBas, IEN.get(), "post_epart", "post_npart" );
   else if(cpu_size == 1)
-    global_part = new Global_Part_Serial( nElem, nFunc, "post_epart", "post_npart" );
+    global_part = SYS_T::make_unique<Global_Part_Serial>( nElem, nFunc, "post_epart", "post_npart" );
   else SYS_T::print_fatal("ERROR: wrong cpu_size: %d \n", cpu_size);
 
-  Map_Node_Index * mnindex = new Map_Node_Index(global_part, cpu_size, nFunc);
+  auto mnindex = SYS_T::make_unique<Map_Node_Index>(global_part.get(), cpu_size, nFunc);
   mnindex->write_hdf5("post_node_mapping");
 
   cout<<"=== Start Partition ... \n";
   int proc_size = cpu_size;
-  SYS_T::Timer * mytimer = new SYS_T::Timer();
+  auto mytimer = SYS_T::make_unique<SYS_T::Timer>();
   for(int proc_rank = 0; proc_rank < proc_size; ++proc_rank)
   {
     mytimer->Reset(); mytimer->Start();
-    IPart * part = new Part_FEM( nElem, nFunc, nLocBas, global_part, mnindex, IEN,
+    auto part = SYS_T::make_unique<Part_FEM>( nElem, nFunc, nLocBas, global_part.get(), mnindex.get(), IEN.get(),
         ctrlPts, proc_rank, proc_size, elemType, {0, dofNum, true, "NS"} );
     part->write(part_file.c_str());
     mytimer->Stop();
     cout<<"-- proc "<<proc_rank<<" Time taken: "<<mytimer->get_sec()<<" sec. \n";
-    delete part;
   }
 
   // Clean memory
   cout<<"=== Clean memory. \n";
-  delete mnindex; delete global_part; delete IEN; delete mytimer;
   return EXIT_SUCCESS;
 }
 
