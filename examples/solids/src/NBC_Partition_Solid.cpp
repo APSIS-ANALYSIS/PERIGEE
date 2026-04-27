8b8132cc6641027a092bcd57eac8da7d0fd2ed41
Ju Liu <liujuy@gmail.com>
2026-04-27 09:33:00 +0800

    Feature/elasticity 生成一个基于稳定化有限元的固体求解器 (#250)
    * use vector for mem data
    
    * use raii for mem control
    
    * remove an unneeded fun
    
    * tested in ns proj only, interpolater is becoming a class without mem data
    
    * converted interpolater into a namespace
    
    * update fun call
    
    * use data fun to reload
    
    * ns proj updated with new interpolater
    
    * we do not need those trivial overloading
    
    * setted up a solid project
    
    * preprocess basically done
    
    * enable ebc nbc part write
    
    * have the local assembly for elasticity from fsi project copied directly into solids example folder
    
    * in
    
    * prepost in
    
    * driver init
    
    * update PLocAssem
    
    * solids/PGAssem in
    
    * solids/PNonlinear_Solver in
    
    * solids/PTime_Solver in
    
    * solids/driver in
    
    * solids/PDNSolution in
    
    * solids/PLocAssem Incompressible in
    
    * solids/VTK VisData in
    
    * minor
    
    * visualize driver in
    
    * minor
    
    * add new ALocal_NBC_Solid for disp-driven case
    
    * add tag is_disp_driven for NBC
    
    * add Apply_Dirichlet_BC for disp-driven case
    
    * recover no traction for EBC
    
    * use Apply_Dirichlet_BC from PGAssem for disp-driven case
    
    * updated to disp-driven case
    
    * change names of binary files
    
    * fix bugs
    
    * in
    
    * change disp_driven tag into bool
    
    ---------
    
    Co-authored-by: ZhaoChongran <imchongran@gmail.com>

---
+++ examples/solids/src/NBC_Partition_Solid.cpp
@@ -0,0 +1,127 @@
+#include "NBC_Partition_Solid.hpp"
+#include "HDF5_Writer.hpp"
+
+#include <algorithm>
+#include <unordered_set>
+
+NBC_Partition_Solid::NBC_Partition_Solid( const IPart * const &part,
+    const Map_Node_Index * const &mnindex,
+    const std::vector<NodalBC_Solid *> &solid_nbc_list_x,
+    const std::vector<NodalBC_Solid *> &solid_nbc_list_y,
+    const std::vector<NodalBC_Solid *> &solid_nbc_list_z,
+    const int &dof,
+    const int &nFunc )
+: cpu_rank( part->get_cpu_rank() )
+{
+  std::vector<std::vector<unsigned int>> dir_nodes(dof);
+  std::vector<std::vector<unsigned int>> disp_nodes(dof);
+  std::vector<std::unordered_set<unsigned int>> dir_node_sets(dof);
+
+  const std::vector<std::vector<NodalBC_Solid *>> solid_nbc_lists
+  {
+    {},
+    solid_nbc_list_x,
+    solid_nbc_list_y,
+    solid_nbc_list_z
+  };
+
+  SYS_T::print_fatal_if( dof != 4,
+      "Error: NBC_Partition_Solid assumes dof = 4, but received %d.\n", dof );
+
+  for(int field=1; field<dof; ++field)
+  {
+    for(const auto * const nbc : solid_nbc_lists[field])
+    {
+      for(unsigned int ii=0; ii<nbc->get_num_dir_nodes(); ++ii)
+      {
+        const unsigned int node = nbc->get_dir_nodes(ii);
+        dir_nodes[field].push_back(node);
+        dir_node_sets[field].insert(node);
+        if(nbc->get_is_disp_driven()) disp_nodes[field].push_back(node);
+      }
+    }
+  }
+
+  for(int field=1; field<dof; ++field)
+  {
+    std::sort(dir_nodes[field].begin(), dir_nodes[field].end());
+    dir_nodes[field].erase(
+        std::unique(dir_nodes[field].begin(), dir_nodes[field].end()),
+        dir_nodes[field].end());
+
+    std::sort(disp_nodes[field].begin(), disp_nodes[field].end());
+    disp_nodes[field].erase(
+        std::unique(disp_nodes[field].begin(), disp_nodes[field].end()),
+        disp_nodes[field].end());
+  }
+
+  LID.clear(); LDN.clear(); LPSN.clear(); LPMN.clear();
+  LocalMaster.clear(); LocalMasterSlave.clear();
+  Num_LD.assign(dof, 0);
+  Num_LPS.assign(dof, 0);
+  Num_LPM.assign(dof, 0);
+  LDN_is_disp_driven.clear();
+
+  for(int field=0; field<dof; ++field)
+  {
+    std::unordered_set<unsigned int> disp_node_set(
+        disp_nodes[field].begin(), disp_nodes[field].end());
+
+    for(const auto node_old : dir_nodes[field])
+    {
+      const unsigned int node_new = mnindex->get_old2new(node_old);
+
+      if(part->isNodeInPart(node_new))
+      {
+        LDN.push_back(node_new);
+        LDN_is_disp_driven.push_back(disp_node_set.count(node_old) > 0 ? 1 : 0);
+        Num_LD[field] += 1;
+      }
+    }
+  }
+
+  LDN.shrink_to_fit();
+  LDN_is_disp_driven.shrink_to_fit();
+
+  const int totnode = part->get_nlocghonode();
+  LID.resize(totnode * dof);
+
+  PERIGEE_OMP_PARALLEL_FOR
+  for(int field=0; field<dof; ++field)
+  {
+    for(int jj=0; jj<totnode; ++jj)
+    {
+      const int new_index = part->get_local_to_global(jj);
+      const int old_index = mnindex->get_new2old(new_index);
+
+      if(dir_node_sets[field].count(static_cast<unsigned int>(old_index)) > 0)
+        LID[field * totnode + jj] = -1;
+      else
+        LID[field * totnode + jj] = new_index;
+    }
+  }
+
+  LID.shrink_to_fit();
+}
+
+void NBC_Partition_Solid::write_hdf5( const std::string &FileName,
+    const std::string &GroupName ) const
+{
+  const std::string fName = SYS_T::gen_partfile_name( FileName, cpu_rank );
+  hid_t file_id = H5Fopen(fName.c_str(), H5F_ACC_RDWR, H5P_DEFAULT);
+  hid_t g_id = H5Gcreate(file_id, GroupName.c_str(),
+      H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+  HDF5_Writer h5w(file_id);
+  h5w.write_intVector( g_id, "LID", LID );
+  if( LDN.size() > 0 ) h5w.write_intVector( g_id, "LDN", LDN );
+  h5w.write_intVector( g_id, "Num_LD", Num_LD );
+  h5w.write_intVector( g_id, "Num_LPS", Num_LPS );
+  h5w.write_intVector( g_id, "Num_LPM", Num_LPM );
+  h5w.write_intVector( g_id, "LDN_is_disp_driven", LDN_is_disp_driven );
+
+  H5Gclose(g_id);
+  H5Fclose(file_id);
+}
+
+// EOF
