06db9349da56db55707653fbffb0d4d0845c34bc
SUNYujie <12231157@mail.sustech.edu.cn>
2026-06-08 09:32:48 +0800

    clean

--- examples/ns/include/LoadData.hpp
+++ examples/ns/include/LoadData.hpp
@@ -129,57 +129,6 @@ namespace LoadData
     dot_sol->Assembly_GhostUpdate();
   }
 
-  // --------------------------------------------------------------------------
-  // rescale_dot_inflow_velo
-  //   Rescale the baseline dot_inflow velocity profile using time-dependent
-  //   dot-flow-rate factors and optional turbulence-intensity perturbations.
-  //
-  //   This routine updates a 3-DOF dot velocity vector (dot_u,dot_v,dot_w), whereas
-  //   rescale_dot_inflow_value() updates the dot velocity components embedded in a
-  //   4-DOF Navier-Stokes dot solution vector (dot_p,dot_u,dot_v,dot_w).
-  // -------------------------------------------------------------------------- 
-  inline void rescale_dot_inflow_velo( const double &stime,
-      const ALocal_InflowBC * const &infnbc,
-      const IFlowRate * const &flrate,
-      const PDNSolution * const &sol_base,
-      PDNSolution * const &dot_velo )
-  {
-    const int num_nbc = infnbc -> get_num_nbc();
-
-    for(int nbc_id=0; nbc_id<num_nbc; ++nbc_id)
-    {
-      const int numnode = infnbc -> get_Num_LD( nbc_id );
-
-      const double factor  = flrate -> get_dot_flow_rate( nbc_id, stime );
-      const double std_dev = flrate -> get_flow_TI_std_dev( nbc_id );
-
-      for(int ii=0; ii<numnode; ++ii)
-      {
-        const int node_index = infnbc -> get_LDN( nbc_id, ii );
-
-        const int base_idx[3] = { node_index*4+1, node_index*4+2, node_index*4+3 };
-
-        double base_vals[3];
-
-        VecGetValues(sol_base->solution, 3, base_idx, base_vals);
-
-        const double perturb_x = MATH_T::gen_double_rand_normal(0, std_dev);
-        const double perturb_y = MATH_T::gen_double_rand_normal(0, std_dev);
-        const double perturb_z = MATH_T::gen_double_rand_normal(0, std_dev);
-
-        const double vals[3] = { base_vals[0] * factor * (1.0 + perturb_x), 
-          base_vals[1] * factor * (1.0 + perturb_y),
-          base_vals[2] * factor * (1.0 + perturb_z) };
-
-        const int dot_velo_idx[3] = { node_index*3, node_index*3+1, node_index*3+2 };
-
-        VecSetValues(dot_velo->solution, 3, dot_velo_idx, vals, INSERT_VALUES);
-      }
-    }
-
-    dot_velo->Assembly_GhostUpdate();
-  }
-
 }
 
 #endif
